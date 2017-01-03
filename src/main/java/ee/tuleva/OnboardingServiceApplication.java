87e0208e2aad32d62da86af389f79a97799acabb
jordan.valdma <jordan.valdma@gmail.com>
2017-01-03 09:14:55 +0200

    Initial commit

---
+++ src/main/java/ee/tuleva/OnboardingServiceApplication.java
@@ -0,0 +1,12 @@
+package ee.tuleva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingServiceApplication.class, args);
+	}
+}

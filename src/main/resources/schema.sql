0b0237b6e868e72c54ac5aa29ff8582cd8030355
jordan.valdma <jordan.valdma@gmail.com>
2017-01-30 16:05:00 +0200

    Adds oauth schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,30 @@
+create table oauth_client_details (
+  client_id VARCHAR(256) PRIMARY KEY,
+  resource_ids VARCHAR(256),
+  client_secret VARCHAR(256),
+  scope VARCHAR(256),
+  authorized_grant_types VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities VARCHAR(256),
+  access_token_validity INTEGER,
+  refresh_token_validity INTEGER,
+  additional_information VARCHAR(4096),
+  autoapprove VARCHAR(256)
+);
+
+
+create table oauth_access_token (
+  token_id VARCHAR(256) PRIMARY KEY,
+  token bytea,
+  authentication_id VARCHAR(256),
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
+  authentication bytea,
+  refresh_token VARCHAR(256)
+);
+
+create table oauth_refresh_token (
+  token_id VARCHAR(256) REFERENCES oauth_access_token,
+  token bytea,
+  authentication bytea
+);

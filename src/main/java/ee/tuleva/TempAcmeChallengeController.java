2fa15566935c1f0172b00278184a5933b5cc4ab3
Siim Käba <siim.kaba@zerotech.ee>
2017-01-31 12:45:13 +0200

    refreshing tokens in temporary acme challenge for tls cert

--- src/main/java/ee/tuleva/TempAcmeChallengeController.java
+++ src/main/java/ee/tuleva/TempAcmeChallengeController.java
@@ -9,12 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class TempAcmeChallengeController {
 
-    @RequestMapping(value = "/.well-known/acme-challenge/8cOWlAHexK7H8RE-gmXWByhZkZzhFuAS0XzjzcH3obk", produces = "text/plain")
+    @RequestMapping(value = "/.well-known/acme-challenge/a7XKHlcPF3EZdo0VYOMFxgB5gVmc1hkkP4qGuDHhJvI", produces = "text/plain")
     @ResponseBody
     public String challenge(HttpServletResponse response) {
         response.setContentType("text/plain");
         response.setCharacterEncoding("UTF-8");
-        return "8cOWlAHexK7H8RE-gmXWByhZkZzhFuAS0XzjzcH3obk.WmtYasmHj0-9Rw5MJjwC0wMmsYFHPXTJMhCCgqBxzTo";
+        return "a7XKHlcPF3EZdo0VYOMFxgB5gVmc1hkkP4qGuDHhJvI.WmtYasmHj0-9Rw5MJjwC0wMmsYFHPXTJMhCCgqBxzTo";
     }
 
 }

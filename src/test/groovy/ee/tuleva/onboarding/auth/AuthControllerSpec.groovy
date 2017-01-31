950b242e6c78628df8e1bda4545c121de297685f
jordan.valdma <jordan.valdma@gmail.com>
2017-01-31 17:15:20 +0200

    Extracts boilerplate mockmvc into baseControllerSpec

--- src/test/groovy/ee/tuleva/onboarding/auth/AuthControllerSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/auth/AuthControllerSpec.groovy
@@ -1,20 +1,14 @@
 package ee.tuleva.onboarding.auth
 
 import com.codeborne.security.mobileid.MobileIDSession
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 
-class AuthControllerSpec extends Specification {
-
-    private final static ObjectMapper mapper = new ObjectMapper()
+class AuthControllerSpec extends BaseControllerSpec {
 
     MobileIdAuthService mobileIdAuthService = Mock(MobileIdAuthService)
     MobileIdSessionStore mobileIdSessionStore = Mock(MobileIdSessionStore)
@@ -43,20 +37,6 @@ class AuthControllerSpec extends Specification {
         [
                 phoneNumber: MobileIdFixture.samplePhoneNumber
         ]
-    }
-
-    private MockMvc getMockMvc(Object... controllers) {
-        MappingJackson2HttpMessageConverter converter = setupJacksonConverter()
-        return MockMvcBuilders.standaloneSetup(controllers)
-                .setMessageConverters(converter)
-                .build()
-    }
-
-    private static MappingJackson2HttpMessageConverter setupJacksonConverter() {
-        ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setObjectMapper(objectMapper);
-        return converter
     }
 
 }
---
+++ src/test/groovy/ee/tuleva/onboarding/auth/BaseControllerSpec.groovy
@@ -0,0 +1,27 @@
+package ee.tuleva.onboarding.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import spock.lang.Specification;
+
+public class BaseControllerSpec extends Specification {
+
+    protected final static ObjectMapper mapper = new ObjectMapper()
+
+    protected MockMvc getMockMvc(Object... controllers) {
+        MappingJackson2HttpMessageConverter converter = setupJacksonConverter()
+        return MockMvcBuilders.standaloneSetup(controllers)
+                .setMessageConverters(converter)
+                .build()
+    }
+
+    protected static MappingJackson2HttpMessageConverter setupJacksonConverter() {
+        ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        return converter
+    }
+
+}

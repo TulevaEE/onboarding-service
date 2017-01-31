b5166aca469cb3007267d108c458768019e5f49b
Laura Ruusmann <ruusmannlaura@gmail.com>
2017-01-31 15:06:56 +0200

    Implemented comparison endpoint

---
+++ src/main/java/ee/tuleva/comparisons/ComparisonController.java
@@ -0,0 +1,27 @@
+package ee.tuleva.comparisons;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ComparisonController {
+
+    @Autowired
+    private ComparisonService comparisonService;
+
+    @RequestMapping(path = "v1/comparisons/", method = RequestMethod.GET)
+    public Map<String, String> comparison(@RequestParam(value = "totalCapital") Double totalCapital, @RequestParam(value = "age") Integer age,
+                                          @RequestParam(value = "isin") String isin, @RequestParam(value = "monthlyWage") Double monthlyWage){
+
+
+        return comparisonService.comparedResults(totalCapital, age, isin, monthlyWage);
+
+    }
+
+}
---
+++ src/main/java/ee/tuleva/comparisons/EstonianFeeFinderService.java
@@ -0,0 +1,80 @@
+package ee.tuleva.comparisons;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+@Component
+public class EstonianFeeFinderService {
+
+    private static final String feeURLString = "https://www.pensionikeskus.ee/ii-sammas/fondid/fonditasude-vordlused/";
+
+    @Autowired
+    ComparisonDAO comparisonDAO;
+
+    @Scheduled(cron = "0 0 0 1 */3 ?")
+    public void updateFeesFromPensionSystem() throws IOException, ParseException {
+
+        Document doc = Jsoup.connect(feeURLString).get();
+
+        Element table = doc.select("table").get(0);
+        Elements allRows = table.select("tr");
+        Elements rows = new Elements();
+
+        for (Element e : allRows) {
+            Attributes rowAttributes = e.attributes();
+            for (Attribute attribute : rowAttributes) {
+                if (attribute.getValue().equals("data")) {
+                    rows.add(e);
+                }
+            }
+        }
+
+        for (Element e : rows) {
+
+            String hrefToPensionFundDetails = e.select("a").first().attr("href");
+            String[] partsOfPath = hrefToPensionFundDetails.split("/");
+            int fundCodeInPensionSystem = Integer.parseInt(partsOfPath[partsOfPath.length - 1]);
+            String isin = comparisonDAO.getIsin(fundCodeInPensionSystem);
+
+            Elements cols = e.select("td");
+            cols.remove(0);
+            String tableColValues = cols.select("td").text();
+            String[] allFeeValues = tableColValues.split("[^\\d]?%[^\\d]?");
+
+            double sumFee = 0;
+            for (String fee:allFeeValues){
+                double f;
+                if (fee.contains(",")){
+                    NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+                    Number number = format.parse(fee);
+                    f = number.doubleValue();
+                }
+                else {
+                    f = Double.parseDouble(fee);
+                }
+                sumFee += f;
+            }
+
+            comparisonDAO.addFee(isin, sumFee);
+
+        }
+
+    }
+
+}

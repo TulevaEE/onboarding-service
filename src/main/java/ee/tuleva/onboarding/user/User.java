67f9d90ca61dbc3c6a14efe8352225ed7cb2b2f2
Erko Risthein <erko.risthein@transferwise.com>
2017-01-31 11:17:06 +0200

    User domain work in progress

--- src/main/java/ee/tuleva/onboarding/user/User.java
+++ src/main/java/ee/tuleva/onboarding/user/User.java
@@ -3,9 +3,23 @@ package ee.tuleva.onboarding.user;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
 @Data
 @Builder
-class User {
+@Entity
+class User implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	private String username;
+
+	private String password;
 
 }

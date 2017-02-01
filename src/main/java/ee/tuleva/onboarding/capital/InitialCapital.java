10dfae0c8368e9a7df313dfdd1716a39de2bb3e1
jordan.valdma <jordan.valdma@gmail.com>
2017-02-01 17:07:29 +0200

    Adds InitialCapital json View

--- src/main/java/ee/tuleva/onboarding/capital/InitialCapital.java
+++ src/main/java/ee/tuleva/onboarding/capital/InitialCapital.java
@@ -1,5 +1,6 @@
 package ee.tuleva.onboarding.capital;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import ee.tuleva.onboarding.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,13 +23,16 @@ public class InitialCapital {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(InitialCapitalView.SkipUserField.class)
     private Long id;
 
     @NotNull
+    @JsonView(InitialCapitalView.SkipUserField.class)
     BigDecimal amount;
 
     @NotBlank
     @Size(min = 3, max = 3)
+    @JsonView(InitialCapitalView.SkipUserField.class)
     String currency;
 
     @OneToOne
--- src/main/java/ee/tuleva/onboarding/capital/InitialCapitalController.java
+++ src/main/java/ee/tuleva/onboarding/capital/InitialCapitalController.java
@@ -1,8 +1,9 @@
 package ee.tuleva.onboarding.capital;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import ee.tuleva.onboarding.user.User;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +12,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 @RequestMapping("/v1")
+@RequiredArgsConstructor
 public class InitialCapitalController {
 
-    InitialCapitalRepository initialCapitalRepository;
-
-    @Autowired
-    InitialCapitalController(InitialCapitalRepository initialCapitalRepository) {
-        this.initialCapitalRepository = initialCapitalRepository;
-    }
+    private final InitialCapitalRepository initialCapitalRepository;
 
     @ApiOperation(value = "Get info about current user initial capital")
     @RequestMapping(method = GET, value = "/initial-capital")
+    @JsonView(InitialCapitalView.SkipUserField.class)
     public InitialCapital initialCapital(@AuthenticationPrincipal User user) {
         return initialCapitalRepository.findByUser(user);
     }

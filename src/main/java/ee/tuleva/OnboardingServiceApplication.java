f943644c0789aa06f685d04b0cd7c18c040741fd
jordan.valdma <jordan.valdma@gmail.com>
2017-02-01 12:03:15 +0200

    Add correct main class name

--- src/main/java/ee/tuleva/OnboardingServiceApplication.java
+++ src/main/java/ee/tuleva/OnboardingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
-public class Application {
+public class OnboardingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(OnboardingServiceApplication.class, args);
     }
 
 }

4f5a39240836d8f58f30c5d1b96cb075d4fd9386
jordan.valdma <jordan.valdma@gmail.com>
2017-02-03 09:17:53 +0200

    mandate service

--- src/main/java/ee/tuleva/onboarding/mandate/MandateService.java
+++ src/main/java/ee/tuleva/onboarding/mandate/MandateService.java
@@ -1,12 +1,31 @@
 package ee.tuleva.onboarding.mandate;
 
+import ee.tuleva.domain.fund.Fund;
+import ee.tuleva.domain.fund.FundRepository;
+import ee.tuleva.onboarding.user.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MandateService {
 
-    public Mandate save(CreateMandateCommand createMandateCommand) {
+    FundRepository fundRepository;
 
+    public Mandate save(User user, CreateMandateCommand createMandateCommand) {
+/*
+        createMandateCommand.fundTransferExchanges.stream().forEach( fte -> {
+
+        });
+
+        FundTransferExchange fundTransferExchange = FundTransferExchange.builder()
+                .
+
+        Fund futureContributionFund = fundRepository.findOne(createMandateCommand.futureContributionFundId);
+
+        Mandate.builder()
+                .user(user)
+                .futureContributionFund(futureContributionFund)
+                .fundTransferExchanges()
+*/
         return null;
     }
 

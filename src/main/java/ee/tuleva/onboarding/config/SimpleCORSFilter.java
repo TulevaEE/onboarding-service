e6d76ed8c6c84d49364a3cf19c76154150eedce3
jordan.valdma <jordan.valdma@gmail.com>
2017-02-03 14:32:56 +0200

    Enable CORS

---
+++ src/main/java/ee/tuleva/onboarding/config/SimpleCORSFilter.java
@@ -0,0 +1,27 @@
+package ee.tuleva.onboarding.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class SimpleCORSFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000, https://tuleva.ee, https://pension.tuleva.ee");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization");
+        chain.doFilter(req, res);
+    }
+}

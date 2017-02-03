df5b99dea310a678541421638fffa565dec25455
jordan.valdma <jordan.valdma@gmail.com>
2017-02-03 15:17:45 +0200

    Fix cors

--- src/main/java/ee/tuleva/onboarding/config/SimpleCORSFilter.java
+++ src/main/java/ee/tuleva/onboarding/config/SimpleCORSFilter.java
@@ -18,7 +18,7 @@ public class SimpleCORSFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000, https://tuleva.ee, https://pension.tuleva.ee");
+        response.setHeader("Access-Control-Allow-Origin", "https://pension.tuleva.ee");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Authorization");

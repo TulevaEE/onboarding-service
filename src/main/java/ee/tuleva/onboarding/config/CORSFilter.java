bfab18fc02c20f37828fe636d45ed2ae8b9f7bf9
jordan.valdma <jordan.valdma@gmail.com>
2017-02-04 13:28:05 +0200

    Update cors filters to allow header types

--- src/main/java/ee/tuleva/onboarding/config/CORSFilter.java
+++ src/main/java/ee/tuleva/onboarding/config/CORSFilter.java
@@ -1,6 +1,8 @@
 package ee.tuleva.onboarding.config;
 
+import com.sun.deploy.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -10,10 +12,12 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Component
-public class SimpleCORSFilter extends GenericFilterBean {
+public class CORSFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
@@ -23,6 +27,12 @@ public class SimpleCORSFilter extends GenericFilterBean {
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Authorization");
         response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        List<String> allowedHeaders = Arrays.asList(
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.AUTHORIZATION);
+        response.setHeader("Access-Control-Allow-Headers", StringUtils.join(allowedHeaders, ", "));
+
         chain.doFilter(req, res);
     }
 }

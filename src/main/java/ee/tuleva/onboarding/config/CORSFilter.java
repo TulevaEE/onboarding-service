1ad5eacf1f65da0ab3e9814041258a184d1eeded
jordan.valdma <jordan.valdma@gmail.com>
2017-02-04 15:11:17 +0200

    Enable CORS OPTIONS for ALL

--- src/main/java/ee/tuleva/onboarding/config/CORSFilter.java
+++ src/main/java/ee/tuleva/onboarding/config/CORSFilter.java
@@ -43,9 +43,7 @@ public class CORSFilter extends GenericFilterBean {
 
         HttpServletRequest request = ((HttpServletRequest) req);
 
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())
-                && request.getRequestURI().startsWith("/oauth/token")
-                ) {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
             chain.doFilter(req, res);

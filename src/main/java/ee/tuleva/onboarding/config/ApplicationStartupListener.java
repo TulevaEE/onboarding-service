c0316ed7b8b921c7d5e6cf12d334830acc9ccf76
Erko Risthein <erko.risthein@transferwise.com>
2017-03-07 11:22:03 +0200

    Added application startup listener for nginx

---
+++ src/main/java/ee/tuleva/onboarding/config/ApplicationStartupListener.java
@@ -0,0 +1,26 @@
+package ee.tuleva.onboarding.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.google.common.io.Files.touch;
+
+@Component
+@Slf4j
+public class ApplicationStartupListener implements ApplicationListener<ApplicationReadyEvent> {
+
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		try {
+			touch(new File("/tmp/app-initialized"));
+		} catch (IOException e) {
+			log.error("Could not indicate NGIX that it can start accepting traffic");
+		}
+	}
+
+}

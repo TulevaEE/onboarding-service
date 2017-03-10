d8a459bce0ecfd5e0997a5ee700189c662c836a4
Erko Risthein <erko.risthein@transferwise.com>
2017-03-10 12:08:12 +0200

    Fixed SecurityConfig, removed anonymous access

--- src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
@@ -1,27 +1,23 @@
 package ee.tuleva.onboarding.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableOAuth2Client
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
-        .anonymous().and()
                 .authorizeRequests()
-                .regexMatchers("/").permitAll()
+                .regexMatchers("/", "/swagger-ui.html", "/authenticate", "/idLogin", "/oauth/token").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .csrf().ignoringAntMatchers("/v1/**", "/authenticate");
-
+                .csrf().ignoringAntMatchers("/v1/**", "/authenticate", "/idLogin", "/oauth/token");
     }
 }

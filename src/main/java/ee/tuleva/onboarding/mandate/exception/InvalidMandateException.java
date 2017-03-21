8cf0246a5bf9ab3dfbe45d78eea3263734827fb7
jordan.valdma <jordan.valdma@gmail.com>
2017-03-21 19:30:20 +0200

    Correct mandate exception description

--- src/main/java/ee/tuleva/onboarding/mandate/exception/InvalidMandateException.java
+++ src/main/java/ee/tuleva/onboarding/mandate/exception/InvalidMandateException.java
@@ -3,7 +3,7 @@ package ee.tuleva.onboarding.mandate.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.UNPROCESSABLE_ENTITY, reason="Mandate not found")
+@ResponseStatus(value= HttpStatus.UNPROCESSABLE_ENTITY, reason="Invalid mandate. Can't transfer more than 100% from source fund.")
 public class InvalidMandateException extends RuntimeException {
 
     public InvalidMandateException() {

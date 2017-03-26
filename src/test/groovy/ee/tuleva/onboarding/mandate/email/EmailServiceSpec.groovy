67f0005621b5a941f697f49e077a24582fbf7622
jordan.valdma <jordan.valdma@gmail.com>
2017-03-26 18:21:13 +0300

    email service spec mock configuration

--- src/test/groovy/ee/tuleva/onboarding/mandate/email/EmailServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/email/EmailServiceSpec.groovy
@@ -1,19 +1,32 @@
 package ee.tuleva.onboarding.mandate.email
 
+import ee.tuleva.onboarding.auth.UserFixture
+import ee.tuleva.onboarding.config.MandateEmailConfiguration
 import spock.lang.Ignore
 import spock.lang.Specification
 
 class EmailServiceSpec extends Specification {
 
-    EmailService service = new EmailService();
+    MandateEmailConfiguration mandateEmailConfiguration = Mock(MandateEmailConfiguration);
+    EmailService service = new EmailService(mandateEmailConfiguration);
 
-    @Ignore
+    def setup() {
+        mandateEmailConfiguration.to >> "jordanvaldma@gmail.com"
+        mandateEmailConfiguration.from >> "avaldused@tuleva.ee"
+        mandateEmailConfiguration.bcc >> "avaldused@tuleva.ee"
+        mandateEmailConfiguration.mandrillKey >> "ffuAx8sTNIXaMP2YRN-F-w"
+
+        service.initialize()
+
+    }
+
+//    @Ignore
     def "Send"() {
         given:
         String senderSignatureName = "Jordan Valdma";
 
         when:
-        service.send(senderSignatureName)
+        service.send(UserFixture.sampleUser(), 123, "file".bytes)
 
         then:
         true

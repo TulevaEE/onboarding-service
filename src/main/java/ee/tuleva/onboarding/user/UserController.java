e943c1e928cb329401e1098b3d61bb804583132a
jordan.valdma <jordan.valdma@gmail.com>
2017-04-18 16:07:24 +0300

    Renames AuthenticatedPErsonResponse to UserResponse

--- src/main/java/ee/tuleva/onboarding/user/UserController.java
+++ src/main/java/ee/tuleva/onboarding/user/UserController.java
@@ -1,7 +1,7 @@
 package ee.tuleva.onboarding.user;
 
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson;
-import ee.tuleva.onboarding.user.response.AuthenticatedPersonResponse;
+import ee.tuleva.onboarding.user.response.UserResponse;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,8 +20,8 @@ public class UserController {
 
 	@ApiOperation(value = "Get info about the current user")
 	@RequestMapping(method = GET, value = "/me")
-	public AuthenticatedPersonResponse me(@ApiIgnore @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson) {
-		return AuthenticatedPersonResponse.fromAuthenticatedPerson(authenticatedPerson);
+	public UserResponse me(@ApiIgnore @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson) {
+		return UserResponse.fromAuthenticatedPerson(authenticatedPerson);
 	}
 
 	@ApiOperation(value = "Get info about the current user preferences from CSD")
--- src/main/java/ee/tuleva/onboarding/user/response/UserResponse.java
+++ src/main/java/ee/tuleva/onboarding/user/response/UserResponse.java
@@ -9,19 +9,20 @@ import lombok.Setter;
 @Builder
 @Getter
 @Setter
-public class AuthenticatedPersonResponse {
+public class UserResponse {
 
+    private Long id;
     private String personalCode;
     private String firstName;
     private String lastName;
-
 
     public int getAge() {
         return PersonalCode.getAge(this.getPersonalCode());
     }
 
-    public static AuthenticatedPersonResponse fromAuthenticatedPerson(AuthenticatedPerson authenticatedPerson) {
-        return AuthenticatedPersonResponse.builder()
+    public static UserResponse fromAuthenticatedPerson(AuthenticatedPerson authenticatedPerson) {
+        return UserResponse.builder()
+                .id(authenticatedPerson.getUser().getId())
                 .firstName(authenticatedPerson.getFirstName())
                 .lastName(authenticatedPerson.getLastName())
                 .personalCode(authenticatedPerson.getPersonalCode())

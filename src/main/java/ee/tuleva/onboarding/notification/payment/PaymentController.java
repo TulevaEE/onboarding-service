82c1365e6b6dc81c9f68f46a4c494c5fa5ee8225
Erko Risthein <erko.risthein@transferwise.com>
2017-05-10 17:29:27 +0300

    Added payment amount validation

--- src/main/java/ee/tuleva/onboarding/notification/payment/PaymentController.java
+++ src/main/java/ee/tuleva/onboarding/notification/payment/PaymentController.java
@@ -1,14 +1,18 @@
 package ee.tuleva.onboarding.notification.payment;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ee.tuleva.onboarding.error.ValidationErrorsException;
 import ee.tuleva.onboarding.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.Errors;
+import org.springframework.validation.SmartValidator;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -24,17 +28,24 @@ public class PaymentController {
 
   private final ObjectMapper mapper;
   private final UserService userService;
+  private final SmartValidator validator;
 
   @Value("${frontend.url}")
   private String frontendUrl;
 
   @PostMapping("/payments")
   public void incomingPayment(@ModelAttribute @Valid IncomingPayment incomingPayment,
-                              HttpServletResponse response) throws IOException {
+                              @ApiIgnore HttpServletResponse response,
+                              @ApiIgnore Errors errors) throws IOException, ValidationErrorsException {
 
     log.info("Incoming payment: {}", incomingPayment);
 
     Payment payment = mapper.readValue(incomingPayment.getJson(), Payment.class);
+
+    validator.validate(payment, errors);
+    if (errors != null && errors.hasErrors()) {
+      throw new ValidationErrorsException(errors);
+    }
 
     Long userId = payment.getReference();
 

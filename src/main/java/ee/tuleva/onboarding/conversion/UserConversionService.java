63a33ba4c8e1ffa56af817fa0cf8859e6089eb34
jordan.valdma <jordan.valdma@gmail.com>
2017-05-23 15:34:46 +0300

    Use pending mandates from EPIS service

--- src/main/java/ee/tuleva/onboarding/conversion/UserConversionService.java
+++ src/main/java/ee/tuleva/onboarding/conversion/UserConversionService.java
@@ -3,12 +3,18 @@ package ee.tuleva.onboarding.conversion;
 import ee.tuleva.onboarding.account.AccountStatementService;
 import ee.tuleva.onboarding.account.FundBalance;
 import ee.tuleva.onboarding.auth.principal.Person;
+import ee.tuleva.onboarding.fund.FundRepository;
+import ee.tuleva.onboarding.mandate.processor.implementation.EpisService;
+import ee.tuleva.onboarding.mandate.processor.implementation.MandateApplication.TransferApplicationDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static ee.tuleva.onboarding.mandate.processor.implementation.MandateApplication.MandateApplicationStatus.PENDING;
 
 @Service
 @Slf4j
@@ -16,6 +22,8 @@ import java.util.UUID;
 public class UserConversionService {
 
     private final AccountStatementService accountStatementService;
+    private final EpisService episService;
+    private final FundRepository fundRepository;
 
     private static final String CONVERTED_FUND_MANAGER_NAME = "Tuleva";
 
@@ -43,14 +51,41 @@ public class UserConversionService {
     }
 
     boolean isTransfersComplete(List<FundBalance> fundBalances) {
-        return !fundBalances.stream()
-                .anyMatch(
+        return getIsinsOfPendingTransfersToConvertedFundManager()
+                .containsAll(unConvertedIsins(fundBalances));
+  }
+
+    List<String> getIsinsOfPendingTransfersToConvertedFundManager() {
+        return getPendingTransfers().stream().filter(transferApplicationDTO ->
+                fundRepository
+                        .findByIsin(transferApplicationDTO.getTargetFundIsin())
+                        .getFundManager()
+                        .getName()
+                        .equalsIgnoreCase(CONVERTED_FUND_MANAGER_NAME)
+
+        ).map(transferApplicationDTO -> transferApplicationDTO.getSourceFundIsin())
+                .collect(Collectors.toList());
+    }
+
+    List<TransferApplicationDTO> getPendingTransfers() {
+        List<TransferApplicationDTO> transferApplications = episService.getFundTransferExchanges();
+
+        return transferApplications.stream().filter(transferApplicationDTO ->
+                transferApplicationDTO.getStatus().equals(PENDING)
+        ).collect(Collectors.toList());
+    }
+
+    List<String> unConvertedIsins(List<FundBalance> fundBalances) {
+        return fundBalances.stream()
+                .filter(
                         fundBalance ->
                                 !fundBalance.getFund()
                                         .getFundManager()
                                         .getName()
                                         .equalsIgnoreCase(CONVERTED_FUND_MANAGER_NAME)
-                );
+                )
+                .map(fundBalance -> fundBalance.getFund().getIsin())
+                .collect(Collectors.toList());
     }
 
 }

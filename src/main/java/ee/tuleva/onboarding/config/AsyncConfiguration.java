38ffe44c651a70d958a70b713657f2646efa9c26
Erko Risthein <erko.risthein@transferwise.com>
2017-06-28 15:19:48 +0300

    Addded an AsyncExceptionHandler

--- src/main/java/ee/tuleva/onboarding/config/AsyncConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/config/AsyncConfiguration.java
@@ -1,7 +1,8 @@
 package ee.tuleva.onboarding.config;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -9,7 +10,7 @@ import java.util.concurrent.Executor;
 
 @EnableAsync
 @Configuration
-public class AsyncConfiguration extends AsyncConfigurerSupport {
+public class AsyncConfiguration implements AsyncConfigurer {
 
     @Override
     public Executor getAsyncExecutor() {
@@ -20,6 +21,11 @@ public class AsyncConfiguration extends AsyncConfigurerSupport {
         executor.setThreadNamePrefix("AsyncProcess-");
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
     }
 
 }

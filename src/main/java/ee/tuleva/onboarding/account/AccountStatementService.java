a3abce237ca4368d4a34e881cca45cd4f3d5c063
Jordan Valdma <jordan.valdma@gmail.com>
2017-07-14 13:50:44 +0300

    Handle error on second pillar fund missing

--- src/main/java/ee/tuleva/onboarding/account/AccountStatementService.java
+++ src/main/java/ee/tuleva/onboarding/account/AccountStatementService.java
@@ -14,6 +14,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -57,10 +58,14 @@ public class AccountStatementService {
     }
 
     private List<FundBalance> convertXRoadResponse(PensionAccountBalanceResponseType response) {
-        return
-                response.getUnits().getBalance().stream()
-                        .map(kprUnitsOsakudToFundBalanceConverter::convert)
-                        .collect(Collectors.toList());
+        try {
+            return response.getUnits().getBalance().stream()
+                    .map(kprUnitsOsakudToFundBalanceConverter::convert)
+                    .collect(Collectors.toList());
+        } catch (NullPointerException e) {
+            log.info("Person didn't have second pillar pension funds");
+            return Arrays.asList();
+        }
     }
 
     private String getActiveFundName(Person person) {

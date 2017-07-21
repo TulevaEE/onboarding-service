a7834bb4be8ebbf0bc9b052170cc06f5039a2dcc
Erko Risthein <erko@risthein.ee>
2017-07-21 13:21:47 +0300

    Fixed oauth config for the funds endpoint

--- src/main/java/ee/tuleva/onboarding/config/OAuthConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/config/OAuthConfiguration.java
@@ -61,7 +61,7 @@ public class OAuthConfiguration {
                             "/v1" + INITIAL_CAPITAL_URI,
                             "/v1" + MANDATES_URI + ".*"
                             ).hasAuthority(Authority.MEMBER)
-                    .regexMatchers("/v1/funds").hasAnyAuthority(Authority.USER, Authority.ROLE_CLIENT)
+                    .regexMatchers(HttpMethod.GET, "/v1/funds.*").hasAnyAuthority(Authority.USER, Authority.ROLE_CLIENT)
                     .regexMatchers(HttpMethod.POST, "/v1/users").hasAuthority(Authority.ROLE_CLIENT)
                     .regexMatchers(HttpMethod.HEAD, "/v1/members").hasAuthority(Authority.ROLE_CLIENT)
                     .regexMatchers("/v1/.*").hasAuthority(Authority.USER)

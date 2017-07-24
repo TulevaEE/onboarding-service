5072b67857271eacee514e971843f0d7e05e34a9
Jordan Valdma <jordan.valdma@gmail.com>
2017-07-24 13:11:15 +0300

    Update user name on login

---
+++ src/test/groovy/ee/tuleva/onboarding/user/UserNameUpdaterSpec.groovy
@@ -0,0 +1,42 @@
+package ee.tuleva.onboarding.user
+
+import ee.tuleva.onboarding.auth.BeforeTokenGrantedEvent
+import ee.tuleva.onboarding.auth.PersonFixture
+import ee.tuleva.onboarding.auth.principal.Person
+import org.springframework.security.oauth2.provider.OAuth2Authentication
+import spock.lang.Specification
+
+class UserNameUpdaterSpec extends Specification {
+
+    UserService userService = Mock(UserService)
+    UserRepository userRepository = Mock(UserRepository)
+
+    UserNameUpdater service = new UserNameUpdater(userService, userRepository)
+
+    def "OnBeforeTokenGrantedEvent: Update user name on before token granted event"() {
+        given:
+
+        Person samplePerson = PersonFixture.samplePerson()
+
+        OAuth2Authentication oAuth2Authentication = Mock({
+            getPrincipal() >> samplePerson
+        })
+
+        BeforeTokenGrantedEvent beforeTokenGrantedEvent = new BeforeTokenGrantedEvent(this, oAuth2Authentication)
+
+        when:
+        service.onBeforeTokenGrantedEvent(beforeTokenGrantedEvent)
+
+        then:
+        1 * userService.findByPersonalCode(samplePerson.personalCode) >> Optional.of(
+                User.builder()
+                .firstName("this will change")
+                .lastName("this will also change")
+                .build()
+        )
+
+        1 * userRepository.save({ User user ->
+            user.firstName == samplePerson.firstName && user.lastName == samplePerson.lastName})
+
+    }
+}

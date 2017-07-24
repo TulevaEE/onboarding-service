3720122b2dee70990579a2529fb71139135d17c8
Jordan Valdma <jordan.valdma@gmail.com>
2017-07-24 13:08:52 +0300

    Intent the file correctly

--- src/main/java/ee/tuleva/onboarding/user/UserService.java
+++ src/main/java/ee/tuleva/onboarding/user/UserService.java
@@ -21,111 +21,111 @@ import static org.apache.commons.lang3.text.WordUtils.capitalizeFully;
 @Transactional
 public class UserService {
 
-  private final UserRepository userRepository;
-  private final MemberRepository memberRepository;
-  private final MailChimpService mailChimpService;
+    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
+    private final MailChimpService mailChimpService;
 
-  public User getById(Long userId) {
-    return userRepository.findOne(userId);
-  }
-
-  public Optional<User> findByPersonalCode(String personalCode) {
-    return userRepository.findByPersonalCode(personalCode);
-  }
-
-  public User createNewUser(User user) {
-    return userRepository.save(user);
-  }
-
-  public User updateUser(String personalCode, String email, String phoneNumber) {
-    User user = findByPersonalCode(personalCode).map(existingUser -> {
-      existingUser.setEmail(email);
-      existingUser.setPhoneNumber(phoneNumber);
-      return existingUser;
-    }).orElseThrow(() -> new RuntimeException("User does not exist"));
-
-    return updateUser(user);
-  }
-
-  public User registerAsMember(Long userId) {
-    User user = userRepository.findOne(userId);
-
-    if (user.getMember().isPresent()) {
-      throw new UserAlreadyAMemberException("User is already a member!");
+    public User getById(Long userId) {
+        return userRepository.findOne(userId);
     }
 
-    Member newMember = Member.builder()
-            .user(user)
-            .memberNumber(memberRepository.getNextMemberNumber())
-            .build();
-
-    log.info("Registering user as new member #{}: {}", newMember.getMemberNumber(), user);
-
-    user.setMember(newMember);
-    return updateUser(user);
-  }
-
-  public boolean isAMember(Long userId) {
-    Optional<User> user = Optional.ofNullable(userRepository.findOne(userId));
-    return user.map(u -> u.getMember().isPresent()).orElse(false);
-  }
-
-  private User updateUser(User user) {
-    User savedUser = userRepository.save(user);
-    mailChimpService.createOrUpdateMember(savedUser);
-    return savedUser;
-  }
-
-  public User updateNameIfMissing(User user, String fullName) {
-    if (!user.hasName()) {
-      String firstName = substringBeforeLast(fullName, " ");
-      String lastName = substringAfterLast(fullName, " ");
-      user.setFirstName(capitalizeFully(firstName));
-      user.setLastName(capitalizeFully(lastName));
-      return userRepository.save(user);
-    }
-    return user;
-  }
-
-  public User createOrUpdateUser(String personalCode, String email, String phoneNumber) {
-    if (isAMember(personalCode, email)) {
-      throw new UserAlreadyAMemberException("This user is already a member");
+    public Optional<User> findByPersonalCode(String personalCode) {
+        return userRepository.findByPersonalCode(personalCode);
     }
 
-    User user = userRepository.findByPersonalCode(personalCode)
-            .map(u -> {
-              u.setEmail(email);
-              u.setPhoneNumber(phoneNumber);
-              return u;
-            }).orElse(
-                    userRepository.findByEmail(email)
-                            .map(u -> {
-                              u.setPersonalCode(personalCode);
-                              u.setPhoneNumber(phoneNumber);
-                              return u;
-                            }).orElse(User.builder()
-                            .personalCode(personalCode)
-                            .email(email)
-                            .phoneNumber(phoneNumber)
-                            .active(true)
-                            .build())
-            );
+    public User createNewUser(User user) {
+        return userRepository.save(user);
+    }
 
-    return userRepository.save(user);
-  }
+    public User updateUser(String personalCode, String email, String phoneNumber) {
+        User user = findByPersonalCode(personalCode).map(existingUser -> {
+            existingUser.setEmail(email);
+            existingUser.setPhoneNumber(phoneNumber);
+            return existingUser;
+        }).orElseThrow(() -> new RuntimeException("User does not exist"));
 
-  private boolean isAMember(String personalCode, String email) {
-    return isAMemberByPersonalCode(personalCode) || isAMemberByEmail(email);
-  }
+        return updateUser(user);
+    }
 
-  private boolean isAMemberByPersonalCode(String personalCode) {
-    Optional<User> user = userRepository.findByPersonalCode(personalCode);
-    return user.map(u -> u.getMember().isPresent()).orElse(false);
-  }
+    public User registerAsMember(Long userId) {
+        User user = userRepository.findOne(userId);
 
-  private boolean isAMemberByEmail(String email) {
-    Optional<User> user = userRepository.findByEmail(email);
-    return user.map(u -> u.getMember().isPresent()).orElse(false);
-  }
+        if (user.getMember().isPresent()) {
+            throw new UserAlreadyAMemberException("User is already a member!");
+        }
+
+        Member newMember = Member.builder()
+                .user(user)
+                .memberNumber(memberRepository.getNextMemberNumber())
+                .build();
+
+        log.info("Registering user as new member #{}: {}", newMember.getMemberNumber(), user);
+
+        user.setMember(newMember);
+        return updateUser(user);
+    }
+
+    public boolean isAMember(Long userId) {
+        Optional<User> user = Optional.ofNullable(userRepository.findOne(userId));
+        return user.map(u -> u.getMember().isPresent()).orElse(false);
+    }
+
+    private User updateUser(User user) {
+        User savedUser = userRepository.save(user);
+        mailChimpService.createOrUpdateMember(savedUser);
+        return savedUser;
+    }
+
+    public User updateNameIfMissing(User user, String fullName) {
+        if (!user.hasName()) {
+            String firstName = substringBeforeLast(fullName, " ");
+            String lastName = substringAfterLast(fullName, " ");
+            user.setFirstName(capitalizeFully(firstName));
+            user.setLastName(capitalizeFully(lastName));
+            return userRepository.save(user);
+        }
+        return user;
+    }
+
+    public User createOrUpdateUser(String personalCode, String email, String phoneNumber) {
+        if (isAMember(personalCode, email)) {
+            throw new UserAlreadyAMemberException("This user is already a member");
+        }
+
+        User user = userRepository.findByPersonalCode(personalCode)
+                .map(u -> {
+                    u.setEmail(email);
+                    u.setPhoneNumber(phoneNumber);
+                    return u;
+                }).orElse(
+                        userRepository.findByEmail(email)
+                                .map(u -> {
+                                    u.setPersonalCode(personalCode);
+                                    u.setPhoneNumber(phoneNumber);
+                                    return u;
+                                }).orElse(User.builder()
+                                .personalCode(personalCode)
+                                .email(email)
+                                .phoneNumber(phoneNumber)
+                                .active(true)
+                                .build())
+                );
+
+        return userRepository.save(user);
+    }
+
+    private boolean isAMember(String personalCode, String email) {
+        return isAMemberByPersonalCode(personalCode) || isAMemberByEmail(email);
+    }
+
+    private boolean isAMemberByPersonalCode(String personalCode) {
+        Optional<User> user = userRepository.findByPersonalCode(personalCode);
+        return user.map(u -> u.getMember().isPresent()).orElse(false);
+    }
+
+    private boolean isAMemberByEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        return user.map(u -> u.getMember().isPresent()).orElse(false);
+    }
 
 }

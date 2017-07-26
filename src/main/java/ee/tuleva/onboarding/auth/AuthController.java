1f852a592ff16fa8111697cd9144a19313c26466
Erko Risthein <erko@risthein.ee>
2017-07-26 18:54:42 +0300

    Added ID card login GET method with redirect

--- src/main/java/ee/tuleva/onboarding/auth/AuthController.java
+++ src/main/java/ee/tuleva/onboarding/auth/AuthController.java
@@ -11,15 +11,20 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.common.exceptions.UnauthorizedClientException;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.Objects;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
@@ -34,6 +39,9 @@ public class AuthController {
     @Value("${id-card.secret.token:Bearer ${random.uuid}}")
     private String idCardSecretToken;
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @ApiOperation(value = "Initiate authentication")
     @RequestMapping(
             method = POST,
@@ -46,11 +54,13 @@ public class AuthController {
     }
 
     @ApiOperation(value = "ID card login")
-    @RequestMapping(method = POST, value = "/idLogin")
+    @RequestMapping(method = {GET, POST}, value = "/idLogin")
     @ResponseBody
     public IdCardLoginResponse idLogin(@RequestHeader(value = "ssl_client_verify") String clientCertificateVerification,
                                        @RequestHeader(value = "ssl_client_cert") String clientCertificate,
-                                       @RequestHeader(value = "x-authorization") String crossAuthorizationToken) {
+                                       @RequestHeader(value = "x-authorization") String crossAuthorizationToken,
+                                       @ApiIgnore HttpServletResponse response,
+                                       @ApiIgnore HttpMethod httpMethod) throws IOException {
         if (!Objects.equals(crossAuthorizationToken, idCardSecretToken)) {
             throw new UnauthorizedClientException("Invalid X-Authorization");
         }
@@ -59,6 +69,9 @@ public class AuthController {
         }
         idCardAuthService.checkCertificate(clientCertificate);
 
+        if (httpMethod.equals(HttpMethod.GET)) {
+            response.sendRedirect(frontendUrl + "?login=idCard");
+        }
         return IdCardLoginResponse.success();
     }
 

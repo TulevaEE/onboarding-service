5b072007b10d64fdb0a1b413119846d27c1c46b6
Erko Risthein <erko@risthein.ee>
2017-11-23 13:27:18 +0200

    Fix epis cache eviction

--- src/main/java/ee/tuleva/onboarding/epis/EpisService.java
+++ src/main/java/ee/tuleva/onboarding/epis/EpisService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.http.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
@@ -26,110 +27,115 @@ import static java.util.Arrays.asList;
 @RequiredArgsConstructor
 public class EpisService {
 
-   private final String TRANSFER_APPLICATIONS_CACHE_NAME = "transferApplications";
-   private final String CONTACT_DETAILS_CACHE_NAME = "contactDetails";
-   private final String ACCOUNT_STATEMENT_CACHE_NAME = "accountStatement";
+  private final String TRANSFER_APPLICATIONS_CACHE_NAME = "transferApplications";
+  private final String CONTACT_DETAILS_CACHE_NAME = "contactDetails";
+  private final String ACCOUNT_STATEMENT_CACHE_NAME = "accountStatement";
 
-   private final RestTemplate restTemplate;
+  private final RestTemplate restTemplate;
 
-   @Value("${epis.service.url}")
-   String episServiceUrl;
+  @Value("${epis.service.url}")
+  String episServiceUrl;
 
-   public void process(List<MandateXmlMessage> messages) {
+  public void process(List<MandateXmlMessage> messages) {
 
-      String url = episServiceUrl + "/processing";
+    String url = episServiceUrl + "/processing";
 
-      log.info("Submitting process to {} " + url);
+    log.info("Submitting process to {} " + url);
 
-      CreateProcessingCommand command = new CreateProcessingCommand(messages);
+    CreateProcessingCommand command = new CreateProcessingCommand(messages);
 
-      try {
-         restTemplate.postForObject(url, getProcessingRequest(command), CreateProcessingCommand.class);
-      } catch (HttpStatusCodeException e) {
-         throw new EpisServiceException("Error processing mandate messages through epis-service", e);
-      }
-   }
+    try {
+      restTemplate.postForObject(url, getProcessingRequest(command), CreateProcessingCommand.class);
+    } catch (HttpStatusCodeException e) {
+      throw new EpisServiceException("Error processing mandate messages through epis-service", e);
+    }
+  }
 
-   @Cacheable(value=TRANSFER_APPLICATIONS_CACHE_NAME, key="#person.personalCode")
-   public List<TransferExchangeDTO> getTransferApplications(Person person) {
-      String url = episServiceUrl + "/exchanges";
+  @Cacheable(value = TRANSFER_APPLICATIONS_CACHE_NAME, key = "#person.personalCode")
+  public List<TransferExchangeDTO> getTransferApplications(Person person) {
+    String url = episServiceUrl + "/exchanges";
 
-      log.info("Getting exchanges from {} for {} {}",
-              url, person.getFirstName(), person.getLastName());
+    log.info("Getting exchanges from {} for {} {}",
+        url, person.getFirstName(), person.getLastName());
 
-      ResponseEntity<TransferExchangeDTO[]> response = restTemplate.exchange(
-              url, HttpMethod.GET, new HttpEntity(getHeaders()), TransferExchangeDTO[].class);
+    ResponseEntity<TransferExchangeDTO[]> response = restTemplate.exchange(
+        url, HttpMethod.GET, new HttpEntity(getHeaders()), TransferExchangeDTO[].class);
 
-      return asList(response.getBody());
-   }
+    return asList(response.getBody());
+  }
 
-   public void clearCache(Person person) {
-      clearTransferApplicationsCache(person);
-      clearContactDetailsCache(person);
-      clearAccountStatementCache(person);
-   }
+  @Caching(evict = {
+      @CacheEvict(value = TRANSFER_APPLICATIONS_CACHE_NAME, key = "#person.personalCode"),
+      @CacheEvict(value = CONTACT_DETAILS_CACHE_NAME, key = "#person.personalCode"),
+      @CacheEvict(value = ACCOUNT_STATEMENT_CACHE_NAME, key = "#person.personalCode")
+  })
+  public void clearCache(Person person) {
+    clearTransferApplicationsCache(person);
+    clearContactDetailsCache(person);
+    clearAccountStatementCache(person);
+  }
 
-   @CacheEvict(value=TRANSFER_APPLICATIONS_CACHE_NAME, key="#person.personalCode")
-   public void clearTransferApplicationsCache(Person person) {
-      log.info("Clearing exchanges cache for {} {}",
-          person.getFirstName(), person.getLastName());
-   }
+  @CacheEvict(value = TRANSFER_APPLICATIONS_CACHE_NAME, key = "#person.personalCode")
+  public void clearTransferApplicationsCache(Person person) {
+    log.info("Clearing exchanges cache for {} {}",
+        person.getFirstName(), person.getLastName());
+  }
 
-   @Cacheable(value=CONTACT_DETAILS_CACHE_NAME, key="#person.personalCode")
-   public UserPreferences getContactDetails(Person person) {
-      String url = episServiceUrl + "/contact-details";
+  @Cacheable(value = CONTACT_DETAILS_CACHE_NAME, key = "#person.personalCode")
+  public UserPreferences getContactDetails(Person person) {
+    String url = episServiceUrl + "/contact-details";
 
-      log.info("Getting contact details from {} for {} {}",
-          url, person.getFirstName(), person.getLastName());
+    log.info("Getting contact details from {} for {} {}",
+        url, person.getFirstName(), person.getLastName());
 
-      ResponseEntity<UserPreferences> response = restTemplate.exchange(
-          url, HttpMethod.GET, new HttpEntity(getHeaders()), UserPreferences.class);
+    ResponseEntity<UserPreferences> response = restTemplate.exchange(
+        url, HttpMethod.GET, new HttpEntity(getHeaders()), UserPreferences.class);
 
-      return response.getBody();
-   }
+    return response.getBody();
+  }
 
-   @CacheEvict(value=CONTACT_DETAILS_CACHE_NAME, key="#person.personalCode")
-   public void clearContactDetailsCache(Person person) {
-      log.info("Clearing contact cache for {} {}",
-          person.getFirstName(), person.getLastName());
-   }
+  @CacheEvict(value = CONTACT_DETAILS_CACHE_NAME, key = "#person.personalCode")
+  public void clearContactDetailsCache(Person person) {
+    log.info("Clearing contact cache for {} {}",
+        person.getFirstName(), person.getLastName());
+  }
 
-   private HttpEntity getProcessingRequest(CreateProcessingCommand command) {
-      return new HttpEntity<>(command, getHeaders());
-   }
+  private HttpEntity getProcessingRequest(CreateProcessingCommand command) {
+    return new HttpEntity<>(command, getHeaders());
+  }
 
-   private HttpHeaders getHeaders() {
-      HttpHeaders headers = new HttpHeaders();
-      headers.setContentType(MediaType.APPLICATION_JSON);
-      headers.add("authorization", "Bearer " + getToken());
+  private HttpHeaders getHeaders() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.add("authorization", "Bearer " + getToken());
 
-      return headers;
-   }
+    return headers;
+  }
 
-   private String getToken() {
-      OAuth2AuthenticationDetails details =
-              (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
+  private String getToken() {
+    OAuth2AuthenticationDetails details =
+        (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
 
-      return details.getTokenValue();
-   }
+    return details.getTokenValue();
+  }
 
-   @Cacheable(value= ACCOUNT_STATEMENT_CACHE_NAME, key="#person.personalCode")
-   public List<FundBalanceDto> getAccountStatement(Person person) {
-      String url = episServiceUrl + "/account-statement";
+  @Cacheable(value = ACCOUNT_STATEMENT_CACHE_NAME, key = "#person.personalCode")
+  public List<FundBalanceDto> getAccountStatement(Person person) {
+    String url = episServiceUrl + "/account-statement";
 
-      log.info("Getting account statement from {} for {} {}",
-          url, person.getFirstName(), person.getLastName());
+    log.info("Getting account statement from {} for {} {}",
+        url, person.getFirstName(), person.getLastName());
 
-      ResponseEntity<FundBalanceDto[]> response = restTemplate.exchange(
-          url, HttpMethod.GET, new HttpEntity(getHeaders()), FundBalanceDto[].class);
+    ResponseEntity<FundBalanceDto[]> response = restTemplate.exchange(
+        url, HttpMethod.GET, new HttpEntity(getHeaders()), FundBalanceDto[].class);
 
-      return asList(response.getBody());
-   }
+    return asList(response.getBody());
+  }
 
-   @CacheEvict(value= ACCOUNT_STATEMENT_CACHE_NAME, key="#person.personalCode")
-   public void clearAccountStatementCache(Person person) {
-      log.info("Clearing account statement cache for {} {}",
-          person.getFirstName(), person.getLastName());
-   }
+  @CacheEvict(value = ACCOUNT_STATEMENT_CACHE_NAME, key = "#person.personalCode")
+  public void clearAccountStatementCache(Person person) {
+    log.info("Clearing account statement cache for {} {}",
+        person.getFirstName(), person.getLastName());
+  }
 
 }

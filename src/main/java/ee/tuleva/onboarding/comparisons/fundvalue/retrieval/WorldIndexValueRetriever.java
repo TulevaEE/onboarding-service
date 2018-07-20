0d8c0699f26d5a91d1d5bbf2c9ca6113c599bce1
Rauno Viskus <Rauno56@users.noreply.github.com>
2018-07-20 11:30:21 +0300

    Retrieve world index fund values from Google Drive (#94)
    * Retrieve world index fund values from Google Drive
    * Use MARKET instead of WORLD_INDEX as comparisonFund
    * Fix typo in test name

---
+++ src/main/java/ee/tuleva/onboarding/comparisons/fundvalue/retrieval/WorldIndexValueRetriever.java
@@ -0,0 +1,145 @@
+package ee.tuleva.onboarding.comparisons.fundvalue.retrieval;
+
+import com.rollbar.Rollbar;
+import ee.tuleva.onboarding.comparisons.fundvalue.ComparisonFund;
+import ee.tuleva.onboarding.comparisons.fundvalue.FundValue;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class WorldIndexValueRetriever implements FundValueRetriever {
+
+    private final Environment environment;
+    private final RestTemplate restTemplate;
+
+    @Value("${logging.rollbar.accessToken:#{null}}")
+    private String accessToken;
+
+    private static final String SOURCE_URL = "https://docs.google.com/spreadsheets/d/125aXusxnf-Mij-4D4W5Qnneb8H4chjebnIMpyrAfRkI/gviz/tq?tqx=out:csv&gid=619370394";
+
+    @Override
+    public ComparisonFund getRetrievalFund() {
+        return ComparisonFund.MARKET;
+    }
+
+    @Override
+    public List<FundValue> retrieveValuesForRange(Instant startDate, Instant endDate) {
+        return restTemplate.execute(SOURCE_URL, HttpMethod.GET, getRequestCallback(), getResponseExtractor(startDate, endDate));
+    }
+
+    private ResponseExtractor<List<FundValue>> getResponseExtractor(Instant startDate, Instant endDate) {
+        return response -> {
+            if (response.getStatusCode() == HttpStatus.OK) {
+                InputStream body = response.getBody();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(body, StandardCharsets.UTF_8));
+                Stream<String> lines = reader.lines().skip(3);
+                return parseLines(lines, startDate, endDate);
+            } else {
+                warn("Failed to fetch World Index values");
+                return Collections.emptyList();
+            }
+        };
+    }
+
+    private List<FundValue> parseLines(Stream<String> lines, Instant startDate, Instant endDate) {
+        return lines
+            .map(this::parseLine)
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .filter((FundValue value) -> {
+                Instant time = value.getTime();
+                return (startDate.isBefore(time) || startDate.equals(time)) && (endDate.isAfter(time) || endDate.equals(time));
+            })
+            .collect(Collectors.toList());
+    }
+
+    private Optional<FundValue> parseLine(String line) {
+        log.info("Parsing line: [{}].", line);
+        String[] parts = line.split("\",\"");
+        if (parts.length < 6) {
+            warn("Invalid line: Less than 6 columns.");
+            return Optional.empty();
+        }
+        parts[0] = parts[0].replaceFirst("^\"", "");
+        parts[parts.length-1] = parts[parts.length-1].replaceFirst("\"$", "");
+
+        log.debug("Parts({}): {}", parts.length, parts);
+
+        Optional<Instant> time = parseTime(parts[0]);
+        Optional<BigDecimal> value = parseAmount(parts[5]);
+
+        if (!time.isPresent() || !value.isPresent()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(FundValue.builder()
+            .comparisonFund(ComparisonFund.MARKET)
+            .time(time.get())
+            .value(value.get())
+            .build());
+    }
+
+    private Optional<Instant> parseTime(String time) {
+        SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy");
+        try {
+            return Optional.of(format.parse(time).toInstant());
+        } catch (ParseException e) {
+            warn("Failed to parse time");
+            return Optional.empty();
+        }
+    }
+
+    private Optional<BigDecimal> parseAmount(String amount) {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setDecimalSeparator('.');
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.0#", symbols);
+        decimalFormat.setParseBigDecimal(true);
+        try {
+            return Optional.of((BigDecimal) decimalFormat.parse(amount));
+        } catch (ParseException e) {
+            warn("Failed to parse amount");
+            return Optional.empty();
+        }
+    }
+
+    private RequestCallback getRequestCallback() {
+        return request -> request
+                .getHeaders()
+                .setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM, MediaType.TEXT_PLAIN));
+    }
+
+    private void warn(String message) {
+        log.warn(message);
+        new Rollbar(accessToken, environment.getActiveProfiles()[0])
+            .platform(System.getProperty("java.version"))
+            .warning(message);
+    }
+}
---
+++ src/test/groovy/ee/tuleva/onboarding/comparisons/fundvalue/retrieval/WorldIndexValueRetrieverSpec.groovy
@@ -0,0 +1,145 @@
+package ee.tuleva.onboarding.comparisons.fundvalue.retrieval
+
+import ee.tuleva.onboarding.comparisons.fundvalue.ComparisonFund
+import ee.tuleva.onboarding.comparisons.fundvalue.FundValue
+import org.springframework.core.env.Environment
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.client.ClientHttpResponse
+import org.springframework.web.client.RequestCallback
+import org.springframework.web.client.ResponseExtractor
+import org.springframework.web.client.RestTemplate
+import spock.lang.Specification
+
+import java.nio.charset.StandardCharsets
+import java.text.SimpleDateFormat
+import java.time.Instant
+
+class WorldIndexValueRetrieverSpec extends Specification {
+
+    RestTemplate restTemplate
+    Environment environment
+    WorldIndexValueRetriever retriever
+
+    void setup() {
+        restTemplate = Mock(RestTemplate)
+        environment = Mock(Environment)
+        environment.getActiveProfiles() >> ["test"]
+        retriever = new WorldIndexValueRetriever(environment, restTemplate)
+        retriever.accessToken = "test"
+    }
+
+    def "it is configured for the right fund"() {
+        when:
+        ComparisonFund retrievalFund = retriever.getRetrievalFund()
+
+        then:
+        retrievalFund == ComparisonFund.MARKET
+    }
+
+    def "it successfully parses a valid fund values"() {
+        given:
+        String responseBody = """"Indeks Components ","ISIN","Share","Last price nav","date","expense ratio","",""
+"","","70%","24.1800","","0.25%","",""
+"","","30%","223.8900","","0.20%","",""
+"18-Jul-2018","24.18","223.89","8.1931","0.3617","279.09","",""
+"17-Jul-2018","24.05","224.01","8.1931","0.3617","278.07","",""
+"""
+        ClientHttpResponse response = createResponse(HttpStatus.OK, responseBody)
+        List<FundValue> expectedValues = [
+                FundValue.builder().comparisonFund(ComparisonFund.MARKET).value(279.09).time(parseInstant("2018-07-18")).build(),
+                FundValue.builder().comparisonFund(ComparisonFund.MARKET).value(278.07).time(parseInstant("2018-07-17")).build(),
+        ]
+
+        when:
+        List<FundValue> values = retriever.retrieveValuesForRange(parseInstant("2018-01-01"), parseInstant("2019-01-01"))
+
+        then:
+        1 * restTemplate.execute(_, _, _, _, _) >> {
+            String url, HttpMethod method, RequestCallback callback, ResponseExtractor<List<FundValue>> handler, Object[] uriVariables ->
+                handler.extractData(response)
+        }
+        values == expectedValues
+    }
+
+    def "it filters out lines with incorrect dates"() {
+        given:
+        Instant startTime = parseInstant("2018-07-16")
+        Instant endTime = parseInstant("2018-07-17")
+        String responseBody = """"Indeks Components ","ISIN","Share","Last price nav","date","expense ratio","","","","","","","","","","","","","","","","","","","",""
+"","","70%","24.1800","","0.25%","","","","","","","","","","","","","","","","","","","",""
+"","","30%","223.8900","","0.20%","","","","","","","","","","","","","","","","","","","",""
+"18-Jul-2018","24.18","223.89","6.7372","0.2974","229.49","","","","","","","","","","","","","","","","","","","",""
+"17-Jul-2018","24.05","224.01","6.7372","0.2974","228.65","","","","","","","","","","","","","","","","","","","",""
+"16-Jul-2018","23.94","223.43","6.7372","0.2974","227.74","","","","","","","","","","","","","","","","","","","",""
+"13-Jul-2018","24.04","223.77","6.7372","0.2974","228.52","","","","","","","","","","","","","","","","","","","",""
+"""
+        ClientHttpResponse response = createResponse(HttpStatus.OK, responseBody)
+        List<FundValue> expectedValues = [
+                FundValue.builder().comparisonFund(ComparisonFund.MARKET).value(228.65).time(parseInstant("2018-07-17")).build(),
+                FundValue.builder().comparisonFund(ComparisonFund.MARKET).value(227.74).time(parseInstant("2018-07-16")).build(),
+        ]
+
+        when:
+        List<FundValue> values = retriever.retrieveValuesForRange(startTime, endTime)
+
+        then:
+        1 * restTemplate.execute(_, _, _, _, _) >> {
+            String url, HttpMethod method, RequestCallback callback, ResponseExtractor<List<FundValue>> handler, Object[] uriVariables ->
+                handler.extractData(response)
+        }
+        values == expectedValues
+
+    }
+
+    def "when a row is misformed it is ignored"() {
+        given:
+        ClientHttpResponse response = createResponse(HttpStatus.OK, """i"Indeks Components ","ISIN","Share","Last price nav","date","expense ratio","",""
+broken
+"","","70%","24.1800","","0.25%","",""
+"","","30%","223.8900","","0.20%","",""
+"18-Jul-2018","24.18","223.89","8.1931","0.3617","279.09","",""
+"17-Jul-2018","24.05","224.01","8.1931","0.3617","278.07","",""
+""")
+        List<FundValue> expectedValues = [
+            FundValue.builder().comparisonFund(ComparisonFund.MARKET).value(279.09).time(parseInstant("2018-07-18")).build(),
+            FundValue.builder().comparisonFund(ComparisonFund.MARKET).value(278.07).time(parseInstant("2018-07-17")).build(),
+        ]
+
+        when:
+        List<FundValue> values = retriever.retrieveValuesForRange(parseInstant("2018-01-01"), parseInstant("2019-01-01"))
+
+        then:
+        1 * restTemplate.execute(_, _, _, _, _) >> {
+            String url, HttpMethod method, RequestCallback callback, ResponseExtractor<List<FundValue>> handler, Object[] uriVariables ->
+                handler.extractData(response)
+        }
+        values == expectedValues
+    }
+
+    def "when an invalid response is received, an empty list is returned"() {
+        given:
+        ClientHttpResponse response = createResponse(HttpStatus.BAD_REQUEST, "")
+
+        when:
+        List<FundValue> values = retriever.retrieveValuesForRange(Instant.now(), Instant.now())
+
+        then:
+        1 * restTemplate.execute(_, _, _, _, _) >> {
+            String url, HttpMethod method, RequestCallback callback, ResponseExtractor<List<FundValue>> handler, Object[] uriVariables ->
+                handler.extractData(response)
+        }
+        values.empty
+    }
+
+    private ClientHttpResponse createResponse(HttpStatus status, String csvBody) {
+        ClientHttpResponse response = Mock(ClientHttpResponse)
+        response.getStatusCode() >> status
+        response.getBody() >> new ByteArrayInputStream(csvBody.getBytes(StandardCharsets.UTF_8))
+        return response;
+    }
+
+    private static Instant parseInstant(String format) {
+        return new SimpleDateFormat("yyyy-MM-dd").parse(format).toInstant()
+    }
+}

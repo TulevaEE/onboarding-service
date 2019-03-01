6ded5f485218c1e95a4e050d31183b89d11592a1
Erko Risthein <erko@risthein.ee>
2019-03-01 15:31:38 +0200

    Fix mandate error

--- src/main/java/ee/tuleva/onboarding/epis/contact/UserPreferences.java
+++ src/main/java/ee/tuleva/onboarding/epis/contact/UserPreferences.java
@@ -1,12 +1,12 @@
 package ee.tuleva.onboarding.epis.contact;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 // TODO: rename to ContactDetails
 public class UserPreferences {
 

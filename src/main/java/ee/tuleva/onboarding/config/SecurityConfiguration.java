8dc78c9984f18a9022d8a9abda8188e7374414f0
Maido Käära <m@maido.io>
2019-04-27 15:30:05 +0300

    Password encoder

--- src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
@@ -1,10 +1,15 @@
 package ee.tuleva.onboarding.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @EnableWebSecurity
@@ -15,13 +20,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .requestMatchers(EndpointRequest.to("health")).permitAll()
-                .regexMatchers("/", "/health", "/swagger-ui.html", "/authenticate", "/idLogin", "/oauth/token",
-                        "/notifications/payments").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().ignoringAntMatchers("/v1/**", "/authenticate", "/idLogin", "/oauth/token",
-                "/notifications/payments");
+            .authorizeRequests()
+            .requestMatchers(EndpointRequest.to("health")).permitAll()
+            .regexMatchers("/", "/health", "/swagger-ui.html", "/authenticate", "/idLogin", "/oauth/token",
+                "/notifications/payments").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .csrf().ignoringAntMatchers("/v1/**", "/authenticate", "/idLogin", "/oauth/token",
+            "/notifications/payments");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        DelegatingPasswordEncoder encoder = (DelegatingPasswordEncoder)
+            PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        encoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
+        return encoder;
     }
 }

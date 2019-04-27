42372f7c47c7d739d76ad9a5c187e497af04848e
Maido Käära <m@maido.io>
2019-04-27 12:33:24 +0300

    Redirect /health to /actuator/health

--- src/main/java/ee/tuleva/onboarding/IndexController.java
+++ src/main/java/ee/tuleva/onboarding/IndexController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "redirect:/swagger-ui.html";
-	}
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/swagger-ui.html";
+    }
+
+    // Temporary fix for health endpoint change
+    @RequestMapping("/health")
+    public String health() {
+        return "forward:/actuator/health";
+    }
 
 }
--- src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package ee.tuleva.onboarding.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,11 +16,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .regexMatchers("/", "/swagger-ui.html", "/authenticate", "/idLogin", "/oauth/token",
-                    "/notifications/payments").permitAll()
+                .requestMatchers(EndpointRequest.to("health")).permitAll()
+                .regexMatchers("/", "/health", "/swagger-ui.html", "/authenticate", "/idLogin", "/oauth/token",
+                        "/notifications/payments").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().ignoringAntMatchers("/v1/**", "/authenticate", "/idLogin", "/oauth/token",
-                    "/notifications/payments");
+                "/notifications/payments");
     }
 }

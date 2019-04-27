e424e23f91ed7fcb570691435c8c97e3ca29d5e9
Maido Käära <m@maido.io>
2019-04-27 15:44:47 +0300

    Default is NOOP

--- src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder() {
         DelegatingPasswordEncoder encoder = (DelegatingPasswordEncoder)
             PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        encoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
+        encoder.setDefaultPasswordEncoderForMatches(NoOpPasswordEncoder.getInstance());
         return encoder;
     }
 }

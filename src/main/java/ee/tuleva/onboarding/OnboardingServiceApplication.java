fe01c9a8320bba814610c06e27a85cadf6393e29
Maido Käära <m@maido.io>
2019-05-06 17:49:35 +0300

    Track release in Sentry

--- src/main/java/ee/tuleva/onboarding/OnboardingServiceApplication.java
+++ src/main/java/ee/tuleva/onboarding/OnboardingServiceApplication.java
@@ -1,15 +1,37 @@
 package ee.tuleva.onboarding;
 
+import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.GitProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Profiles;
 
 @SpringBootApplication
+@Slf4j
 public class OnboardingServiceApplication {
 
     public static void main(String[] args) {
         // CloudFlare is not a fan of Java user agents
         System.setProperty("http.agent", "HTTPie/1.0.2");
-        SpringApplication.run(OnboardingServiceApplication.class, args);
+        val context = SpringApplication.run(OnboardingServiceApplication.class, args);
+        trackRelease(context);
+    }
+
+    private static void trackRelease(ApplicationContext context) {
+        try {
+            val gitProperties = context.getBean(GitProperties.class);
+            Sentry.getStoredClient().setRelease(gitProperties.getShortCommitId());
+            val isProduction = context.getEnvironment().acceptsProfiles(Profiles.of("production"));
+            if (isProduction) {
+                Sentry.getStoredClient().setEnvironment("production");
+            }
+        } catch (NoSuchBeanDefinitionException e) {
+            log.info("No git properties found, continuing without release info");
+        }
     }
 
 }

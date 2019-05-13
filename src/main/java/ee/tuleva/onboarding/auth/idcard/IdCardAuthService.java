eea78c1f074df3998800fad809a56d3c05d1988a
Maido Käära <m@maido.io>
2019-05-13 17:06:29 +0300

    Determine ID document type

--- src/main/java/ee/tuleva/onboarding/auth/idcard/IdCardAuthService.java
+++ src/main/java/ee/tuleva/onboarding/auth/idcard/IdCardAuthService.java
@@ -5,26 +5,66 @@ import com.codeborne.security.mobileid.MobileIDAuthenticator;
 import ee.tuleva.onboarding.auth.session.GenericSessionStore;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.bouncycastle.asn1.DLSequence;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Objects;
+
+import static org.apache.commons.io.IOUtils.toInputStream;
+import static org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils.parseExtensionValue;
 
 @Service
 @Slf4j
 @AllArgsConstructor
 public class IdCardAuthService {
 
-	private final MobileIDAuthenticator authenticator;
-	private final GenericSessionStore sessionStore;
+    private static final String OID = "2.5.29.32";
+    private static final String AUTHENTICATION_POLICY_ID = "0.4.0.2042.1.2";
+    private static final int POLICY_NO_1 = 0;
+    private static final int POLICY_NO_2 = 1;
+    private final MobileIDAuthenticator authenticator;
+    private final GenericSessionStore sessionStore;
 
-	public IdCardSession checkCertificate(String certificate) {
-		log.info("Checking ID card certificate");
-		CheckCertificateResponse response = authenticator.checkCertificate(certificate);
-		IdCardSession session = IdCardSession.builder()
-				.firstName(response.firstName)
-				.lastName(response.lastName)
-				.personalCode(response.personalCode)
-				.build();
-		sessionStore.save(session);
-		return session;
-	}
+    public IdCardSession checkCertificate(String certificate) {
+        log.info("Checking ID card certificate");
+        CheckCertificateResponse response = authenticator.checkCertificate(certificate);
+        IdCardSession session = IdCardSession.builder()
+            .firstName(response.firstName)
+            .lastName(response.lastName)
+            .personalCode(response.personalCode)
+            .documentType(getDocumentTypeFromCertificate(certificate))
+            .build();
+        sessionStore.save(session);
+        return session;
+    }
+
+    private IdDocumentType getDocumentTypeFromCertificate(String certificate) {
+        try {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            val certStream = toInputStream(certificate, "UTF8");
+            X509Certificate cert = (X509Certificate) cf.generateCertificate(certStream);
+            byte[] encodedExtensionValue = cert.getExtensionValue(OID);
+            if (encodedExtensionValue != null) {
+                DLSequence extensionValue = (DLSequence) parseExtensionValue(encodedExtensionValue);
+                DLSequence first = (DLSequence) extensionValue.getObjectAt(POLICY_NO_1);
+                DLSequence second = (DLSequence) extensionValue.getObjectAt(POLICY_NO_2);
+                if (Objects.equals(second.getObjectAt(0).toString(), AUTHENTICATION_POLICY_ID)) {
+                    return IdDocumentType.findByIdentifier(first.getObjectAt(POLICY_NO_1).toString());
+                } else {
+                    log.warn("Unknown identifier {}", second.getObjectAt(0));
+                }
+            } else {
+                log.warn("Extension missing!");
+            }
+        } catch (CertificateException | IOException e) {
+            log.warn("Could not parse certificate");
+        }
+        return IdDocumentType.UNKNOWN;
+    }
 
 }

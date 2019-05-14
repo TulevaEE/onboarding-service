934c849f1fbb9b5953e605a41fa74952153faf6c
Alar <register@wbgrupp.net>
2019-05-14 15:44:13 +0300

    Move schema and structure migrations for dev profile under FlyWay

---
+++ src/main/resources/db/dev/V1_20__insert_3rd_pillar_test_funds.sql
@@ -0,0 +1,51 @@
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600109419', 'LHV Pensionifond Indeks Pluss', 3, 'ACTIVE', 1, 0.0034, 0.0085)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600010294', 'LHV Täiendav Pensionifond', 3, 'ACTIVE', 1, 0.01, 0.018)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600098422', 'Luminor Aktsiad 100 Pensionifond', 3, 'ACTIVE', 4, 0.015, 0.0166)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600109369', 'Luminor Intress Pluss Pensionifond', 3, 'ACTIVE', 4, 0.015, 0.0153)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600074076', 'SEB Aktiivne Pensionifond', 3, 'ACTIVE', 3, 0.015, 0.0183)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600008934', 'SEB Tasakaalukas Pensionifond', 3, 'ACTIVE', 3, 0.01, 0.0131)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600109484', 'Swedbank Pensionifond V100 indeks (väljumine piiratud)', 3, 'ACTIVE', 2, 0.0049, 0.009)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600007530', 'Swedbank Pensionifond V1 (Tasakaalustatud strateegia)', 3, 'ACTIVE', 2, 0.012, 0.0148)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600071031', 'Swedbank Pensionifond V2 (Kasvustrateegia)', 3, 'ACTIVE', 2, 0.013, 0.016)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600071049', 'Swedbank Pensionifond V3 (Aktsiastrateegia)', 3, 'ACTIVE', 2, 0.014, 0.0175)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600103248', 'Swedbank Pensionifond X (Aktsiastrateegia)', 3, 'ACTIVE', 2, 0.014, 0.0175)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600019725', 'Swedbank Pensionifond Y (Aktsiastrateegia)', 3, 'ACTIVE', 2, 0.014, 0.0175)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO fund(isin, name, pillar, status, fund_manager_id, management_fee_rate, ongoing_charges_figure)
+VALUES ('EE3600103297', 'Swedbank Pensionifond Z (Aktsiastrateegia)', 3, 'ACTIVE', 2, 0.014, 0.0175)
+ON CONFLICT DO NOTHING;

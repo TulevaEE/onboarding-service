205e13fb72ff22393d685c22e3fba27ebe9224a3
Mati Vait <mativait@gmail.com>
2019-05-14 08:39:22 +0300

    Added attributes to fund:  * pillar  * ongoing_charges_figure

--- src/main/resources/db/migration/V1_17__add_ongoing_charge_figure_to_fund.sql
+++ src/main/resources/db/migration/V1_17__add_ongoing_charge_figure_to_fund.sql
@@ -1,2 +1,3 @@
-ALTER TABLE fund
-  ADD COLUMN ongoing_charges_figure NUMERIC(10, 8) NOT NULL DEFAULT 0;
+ALTER TABLE fund ADD COLUMN ongoing_charges_figure NUMERIC(10, 8);
+UPDATE fund SET ongoing_charges_figure = 0.0 where ongoing_charges_figure is null;
+ALTER TABLE fund ALTER COLUMN ongoing_charges_figure SET NOT NULL;
--- src/main/resources/db/migration/V1_18__add_pillar_attribute_to_fund.sql
+++ src/main/resources/db/migration/V1_18__add_pillar_attribute_to_fund.sql
@@ -1,2 +1,3 @@
 ALTER TABLE fund ADD COLUMN pillar SMALLINT;
 UPDATE fund SET pillar = 2 where pillar is null;
+ALTER TABLE fund ALTER COLUMN pillar SET NOT NULL;

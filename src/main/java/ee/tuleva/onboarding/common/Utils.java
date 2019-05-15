48f2c1afe6a447b1a68ed7baea7619e0ee6cff52
Alar <register@wbgrupp.net>
2019-05-15 15:17:57 +0300

    Fixed Util method

--- src/main/java/ee/tuleva/onboarding/common/Utils.java
+++ src/main/java/ee/tuleva/onboarding/common/Utils.java
@@ -1,17 +1,21 @@
 package ee.tuleva.onboarding.common;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.security.InvalidParameterException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 
+@Slf4j
 public class Utils {
 
-    public static Instant parseInstant(String format) {
+    public static Instant parseInstant(String inputDate) {
         try {
-            return new SimpleDateFormat("yyyy-MM-dd").parse(format).toInstant();
+            return new SimpleDateFormat("yyyy-MM-dd").parse(inputDate).toInstant();
         } catch (ParseException e) {
-            e.printStackTrace();
-            return Instant.now();
+            log.error("Error parsing date from "+inputDate, e);
+            throw  new InvalidParameterException("Error parsing date from: "+inputDate);
         }
     }
 }

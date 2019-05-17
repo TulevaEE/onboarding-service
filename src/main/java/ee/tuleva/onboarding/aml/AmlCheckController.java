86c0aa95026e7f42701b8851623d7bb0fb071241
Maido Käära <m@maido.io>
2019-05-17 18:49:56 +0300

    Frontend doesn't like empty responses

--- src/main/java/ee/tuleva/onboarding/aml/AmlCheckController.java
+++ src/main/java/ee/tuleva/onboarding/aml/AmlCheckController.java
@@ -3,7 +3,10 @@ package ee.tuleva.onboarding.aml;
 import ee.tuleva.onboarding.aml.command.AmlCheckAddCommand;
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson;
 import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -18,20 +21,29 @@ import java.util.List;
 @RequiredArgsConstructor
 class AmlCheckController {
 
-  private final AmlCheckService amlCheckService;
+    private final AmlCheckService amlCheckService;
 
-  @GetMapping
-  @ApiOperation(value = "Get missing AML checks")
-  public List<AmlCheckType> getMissing(@ApiIgnore @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson) {
-    Long userId = authenticatedPerson.getUserId();
-    return amlCheckService.getMissingChecks(userId);
-  }
+    @GetMapping
+    @ApiOperation(value = "Get missing AML checks")
+    public List<AmlCheckType> getMissing(@ApiIgnore @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson) {
+        Long userId = authenticatedPerson.getUserId();
+        return amlCheckService.getMissingChecks(userId);
+    }
 
-  @PostMapping
-  @ApiOperation(value = "Add manual AML check")
-  public void addCheck(@ApiIgnore @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson,
-                       @Valid @RequestBody AmlCheckAddCommand command) {
-    Long userId = authenticatedPerson.getUserId();
-    amlCheckService.addCheckIfMissing(userId, command.getType(), command.isSuccess());
-  }
+    @PostMapping
+    @ApiOperation(value = "Add manual AML check")
+    public AmlCheckCreatedResponse addCheck(@ApiIgnore @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson,
+                                            @Valid @RequestBody AmlCheckAddCommand command) {
+        Long userId = authenticatedPerson.getUserId();
+        amlCheckService.addCheckIfMissing(userId, command.getType(), command.isSuccess());
+        return new AmlCheckCreatedResponse(command.getType(), command.isSuccess());
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    static class AmlCheckCreatedResponse {
+        private AmlCheckType type;
+        private boolean success;
+    }
 }

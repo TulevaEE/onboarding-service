1e0810b0ee21342b4afd9e9e6cdf979bb0ceb7cd
Jordan Valdma <jordan.valdma@gmail.com>
2019-05-17 15:11:07 +0300

    Remove accidental commit

--- src/main/java/ee/tuleva/onboarding/conversion/ConversionController.java
+++ src/main/java/ee/tuleva/onboarding/conversion/ConversionController.java
@@ -19,11 +19,7 @@ public class ConversionController {
     @ApiOperation(value = "Get info about the current user conversion")
     @GetMapping("/me/conversion")
     public ConversionResponse conversion(@ApiIgnore @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson) {
-        return ConversionResponse.builder()
-            .transfersComplete(true)
-            .selectionComplete(true)
-            .build();
-//        return userConversionService.getConversion(authenticatedPerson);
+        return userConversionService.getConversion(authenticatedPerson);
     }
 
 }

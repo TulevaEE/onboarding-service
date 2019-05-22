325452f7aa450db4cbc6cea655e861e3dd33ba94
Erko Risthein <erko@risthein.ee>
2019-05-22 10:55:25 +0300

    Fix capital rounding bug

--- src/main/java/ee/tuleva/onboarding/capital/CapitalService.java
+++ src/main/java/ee/tuleva/onboarding/capital/CapitalService.java
@@ -5,64 +5,54 @@ import ee.tuleva.onboarding.capital.event.AggregatedCapitalEventRepository;
 import ee.tuleva.onboarding.capital.event.member.MemberCapitalEvent;
 import ee.tuleva.onboarding.capital.event.member.MemberCapitalEventRepository;
 import ee.tuleva.onboarding.capital.event.member.MemberCapitalEventType;
-import ee.tuleva.onboarding.capital.event.organisation.OrganisationCapitalEventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.time.LocalDate;
 import java.util.List;
+
+import static ee.tuleva.onboarding.capital.event.member.MemberCapitalEventType.*;
+import static java.math.BigDecimal.ROUND_HALF_DOWN;
 
 @Service
 @RequiredArgsConstructor
 public class CapitalService {
 
     private final MemberCapitalEventRepository memberCapitalEventRepository;
-    private final OrganisationCapitalEventRepository organisationCapitalEventRepository;
     private final AggregatedCapitalEventRepository aggregatedCapitalEventRepository;
 
     CapitalStatement getCapitalStatement(Long memberId) {
         List<MemberCapitalEvent> events = memberCapitalEventRepository.findAllByMemberId(memberId);
 
-        BigDecimal capitalPaymentAmount =
-            getCapitalAmount(events, MemberCapitalEventType.CAPITAL_PAYMENT);
-
-        BigDecimal membershipBonusAmount =
-            getCapitalAmount(events, MemberCapitalEventType.MEMBERSHIP_BONUS);
-
-        BigDecimal unvestedWorkCompensationAmount =
-            getCapitalAmount(events, MemberCapitalEventType.UNVESTED_WORK_COMPENSATION);
-
-        BigDecimal workCompensationAmount =
-            getCapitalAmount(events, MemberCapitalEventType.WORK_COMPENSATION);
-
-
-       return new CapitalStatement(
-           membershipBonusAmount, capitalPaymentAmount,
-           unvestedWorkCompensationAmount, workCompensationAmount,
-           getProfit(events)
-       );
+        return new CapitalStatement(
+            getCapitalAmount(events, MEMBERSHIP_BONUS),
+            getCapitalAmount(events, CAPITAL_PAYMENT),
+            getCapitalAmount(events, UNVESTED_WORK_COMPENSATION),
+            getCapitalAmount(events, WORK_COMPENSATION),
+            getProfit(events)
+        );
     }
 
     @NotNull
     private BigDecimal getCapitalAmount(List<MemberCapitalEvent> events, MemberCapitalEventType capitalPayment) {
         return events.stream().filter(event -> event.getType() == capitalPayment)
-            .filter(event -> event.getAccountingDate().compareTo(LocalDate.now()) < 1)
+            .filter(event -> event.getAccountingDate().compareTo(LocalDate.now()) <= 0)
             .map(MemberCapitalEvent::getFiatValue)
-            .reduce(BigDecimal.ZERO, BigDecimal::add).round(new MathContext(2));
+            .reduce(BigDecimal.ZERO, BigDecimal::add)
+            .setScale(2, ROUND_HALF_DOWN);
     }
 
     private BigDecimal getProfit(List<MemberCapitalEvent> events) {
 
         BigDecimal totalFiatValue = events.stream()
-            .filter(event -> event.getAccountingDate().compareTo(LocalDate.now()) < 1)
+            .filter(event -> event.getAccountingDate().compareTo(LocalDate.now()) <= 0)
             .map(MemberCapitalEvent::getFiatValue)
             .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         BigDecimal totalOwnershipUnitAmount = events.stream()
-            .filter(event -> event.getAccountingDate().compareTo(LocalDate.now()) < 1)
+            .filter(event -> event.getAccountingDate().compareTo(LocalDate.now()) <= 0)
             .map(MemberCapitalEvent::getOwnershipUnitAmount)
             .reduce(BigDecimal.ZERO, BigDecimal::add);
 
@@ -72,6 +62,6 @@ public class CapitalService {
         BigDecimal investmentFiatValue =
             latestAggregatedCapitalEvent.getOwnershipUnitPrice().multiply(totalOwnershipUnitAmount);
 
-        return investmentFiatValue.subtract(totalFiatValue).round(new MathContext(2));
+        return investmentFiatValue.subtract(totalFiatValue).setScale(2, ROUND_HALF_DOWN);
     }
 }

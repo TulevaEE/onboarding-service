85960c6fa82bb1e963679cc206acacfebf9a5b93
Erko Risthein <erko@risthein.ee>
2019-08-29 21:31:05 +0300

    Simplify the Oauth2RestTemplateConfiguration

--- src/main/java/ee/tuleva/onboarding/config/Oauth2RestTemplateConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/config/Oauth2RestTemplateConfiguration.java
@@ -3,14 +3,11 @@ package ee.tuleva.onboarding.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @Configuration
-@EnableOAuth2Client
 public class Oauth2RestTemplateConfiguration {
 
     @Bean
@@ -20,7 +17,7 @@ public class Oauth2RestTemplateConfiguration {
     }
 
     @Bean
-    public OAuth2RestOperations oauth2RestTemplate(ClientCredentialsResourceDetails resource, OAuth2ClientContext context) {
-        return new OAuth2RestTemplate(resource, context);
+    public OAuth2RestOperations oauth2RestTemplate(ClientCredentialsResourceDetails resourceDetails) {
+        return new OAuth2RestTemplate(resourceDetails);
     }
 }

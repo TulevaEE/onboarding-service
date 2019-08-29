4dfe6421304793502bfb55c090a45dbfcfc2be0c
Erko Risthein <erko@risthein.ee>
2019-08-29 17:17:01 +0300

    Add support for filtering returns by specifying the keys

--- src/main/java/ee/tuleva/onboarding/comparisons/returns/ReturnsService.java
+++ src/main/java/ee/tuleva/onboarding/comparisons/returns/ReturnsService.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.util.Collections;
 import java.util.List;
 
+import static ee.tuleva.onboarding.comparisons.returns.provider.PersonalReturnProvider.THIRD_PILLAR;
 import static java.util.stream.Collectors.toList;
 
 @Service
@@ -19,11 +21,12 @@ public class ReturnsService {
 
     private final List<ReturnProvider> returnProviders;
 
-    public Returns get(Person person, LocalDate fromDate) {
-        int pillar = 2; // TODO
+    public Returns get(Person person, LocalDate fromDate, List<String> keys) {
+        int pillar = getPillar(keys);
         Instant fromTime = fromDate.atStartOfDay().toInstant(ZoneOffset.UTC);
 
         List<Return> returns = returnProviders.stream()
+            .filter(returnProvider -> keys == null || !Collections.disjoint(keys, returnProvider.getKeys()))
             .map(returnProvider -> returnProvider.getReturns(person, fromTime, pillar).getReturns())
             .flatMap(List::stream)
             .collect(toList());
@@ -32,5 +35,13 @@ public class ReturnsService {
             .from(fromDate)
             .returns(returns)
             .build();
+    }
+
+    private Integer getPillar(List<String> keys) {
+        if (keys != null && keys.contains(THIRD_PILLAR)) {
+            return 3;
+        } else {
+            return 2;
+        }
     }
 }

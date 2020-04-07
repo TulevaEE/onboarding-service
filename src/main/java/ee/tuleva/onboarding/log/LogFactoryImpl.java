2e3034979af25f319f5644a27dab79e6e47c6369
risto.saar <risto.saar@gmail.com>
2020-04-07 13:29:10 +0900

    new mobile ID signing implementation

--- src/main/java/ee/tuleva/onboarding/log/LogFactoryImpl.java
+++ src/main/java/ee/tuleva/onboarding/log/LogFactoryImpl.java
@@ -1,37 +1,5 @@
 package ee.tuleva.onboarding.log;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogConfigurationException;
 import org.apache.commons.logging.LogFactory;
 
-public class LogFactoryImpl extends LogFactory {
-
-  @Override
-  public Object getAttribute(String name) {
-    return null;
-  }
-
-  @Override
-  public String[] getAttributeNames() {
-    return new String[0];
-  }
-
-  @Override
-  public Log getInstance(Class clazz) throws LogConfigurationException {
-    return null;
-  }
-
-  @Override
-  public Log getInstance(String name) throws LogConfigurationException {
-    return null;
-  }
-
-  @Override
-  public void release() {}
-
-  @Override
-  public void removeAttribute(String name) {}
-
-  @Override
-  public void setAttribute(String name, Object value) {}
-}
+public class LogFactoryImpl extends LogFactory {}

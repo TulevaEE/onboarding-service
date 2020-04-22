a026f43239e019046145fd05bbb9a7aaff145c00
Erko Risthein <erko@risthein.ee>
2020-04-22 22:56:09 +0300

    Add missing tests

--- src/test/groovy/ee/tuleva/onboarding/mandate/signature/SignatureServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/signature/SignatureServiceSpec.groovy
@@ -6,8 +6,6 @@ import com.codeborne.security.mobileid.MobileIdSignatureSession
 import com.codeborne.security.mobileid.SignatureFile
 import spock.lang.Specification
 
-import static java.util.Collections.singletonList
-
 class SignatureServiceSpec extends Specification {
 
     def smartIdSigner = Mock(SmartIdSigner)
@@ -16,15 +14,15 @@ class SignatureServiceSpec extends Specification {
 
     def "startSign() works for mobile id"() {
         given:
-        List<SignatureFile> files = Arrays.asList(
-                new SignatureFile("test1.txt", "text/plain", "Test1".bytes),
-                new SignatureFile("test2.txt", "text/plain", "Test2".bytes)
-        )
+        List<SignatureFile> files = [
+            new SignatureFile("test1.txt", "text/plain", "Test1".bytes),
+            new SignatureFile("test2.txt", "text/plain", "Test2".bytes)
+        ]
 
         signer.startSign(files, "38501010002", "55555555") >> new MobileIdSignatureSession(1, "1234")
 
         when:
-        MobileIdSignatureSession session = service.startSign(files as List<SignatureFile>, "38501010002", "55555555")
+        MobileIdSignatureSession session = service.startSign(files, "38501010002", "55555555")
 
         then:
         session.challenge == "1234"
@@ -46,7 +44,7 @@ class SignatureServiceSpec extends Specification {
     def "startSign() works with id card"() {
         given:
         def expectedSession = new IdCardSignatureSession(1, "sigId", "hash")
-        def files = singletonList(new SignatureFile("file.txt", "text/plain", new byte[1]))
+        def files = [new SignatureFile("file.txt", "text/plain", new byte[1])]
         signer.startSign(files, "signCert") >> expectedSession
 
         when:
@@ -64,6 +62,32 @@ class SignatureServiceSpec extends Specification {
 
         when:
         def file = service.getSignedFile(session, "signedHash")
+
+        then:
+        file == expectedFile
+    }
+
+    def "startSign() works with smart id"() {
+        given:
+        def expectedSession = new SmartIdSignatureSession("certSessionId", "personalCode", [])
+        def files = [new SignatureFile("file.txt", "text/plain", new byte[1])]
+        smartIdSigner.startSign(files, "personalCode") >> expectedSession
+
+        when:
+        def session = service.startSmartIdSign(files, "personalCode")
+
+        then:
+        session == expectedSession
+    }
+
+    def "getSignedFile() works with smart id"() {
+        given:
+        def session = new SmartIdSignatureSession("certSessionId", "personalCode", [])
+        def expectedFile = new byte[1]
+        smartIdSigner.getSignedFile(session) >> expectedFile
+
+        when:
+        def file = service.getSignedFile(session)
 
         then:
         file == expectedFile

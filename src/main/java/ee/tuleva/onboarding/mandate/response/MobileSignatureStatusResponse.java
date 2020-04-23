1ac4104bb77f74eca7ced546df264c2b8f46d4a1
Erko Risthein <erko@risthein.ee>
2020-04-23 08:28:35 +0300

    Fix indentation

--- src/main/java/ee/tuleva/onboarding/mandate/response/MobileSignatureStatusResponse.java
+++ src/main/java/ee/tuleva/onboarding/mandate/response/MobileSignatureStatusResponse.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class MobileSignatureStatusResponse {
 
-	private final String statusCode;
+    private final String statusCode;
     private final String challengeCode;
 
 }

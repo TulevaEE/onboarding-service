7c4501082aa7979d8feaf4638271993650408a87
aleksandraisabel <43557218+aleksandraisabel@users.noreply.github.com>
2020-08-03 11:10:00 +0300

    Delete mailchimp and its dependencies (#260)
    * Delete mailchimp and its dependencies
    
    * Delete gradle dependency

--- src/main/java/ee/tuleva/onboarding/notification/mailchimp/MailChimpException.java
+++
@@ -1,9 +0,0 @@
-package ee.tuleva.onboarding.notification.mailchimp;
-
-public class MailChimpException extends RuntimeException {
-
-  public MailChimpException(Throwable cause) {
-    super(cause);
-  }
-
-}

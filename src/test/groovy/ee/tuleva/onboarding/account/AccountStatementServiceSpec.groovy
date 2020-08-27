3f1f84eb032713928f154b338989a30f49821c81
Aleksandra Isabel Pillmann <isabelpillmann@gmail.com>
2020-08-27 15:05:34 +0300

    Remove portrayed non-active funds from account statement

--- src/test/groovy/ee/tuleva/onboarding/account/AccountStatementServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/account/AccountStatementServiceSpec.groovy
@@ -1,12 +1,15 @@
 package ee.tuleva.onboarding.account
 
-
+import ee.tuleva.onboarding.auth.principal.Person
 import ee.tuleva.onboarding.epis.EpisService
 import ee.tuleva.onboarding.epis.account.FundBalanceDto
+import ee.tuleva.onboarding.fund.Fund
 import spock.lang.Specification
 
 import static ee.tuleva.onboarding.account.AccountStatementFixture.activeTuleva2ndPillarFundBalance
 import static ee.tuleva.onboarding.auth.PersonFixture.samplePerson
+import static java.math.BigDecimal.ONE
+import static java.math.BigDecimal.ZERO
 
 class AccountStatementServiceSpec extends Specification {
 
@@ -43,6 +46,35 @@ class AccountStatementServiceSpec extends Specification {
         then:
             accountStatement.isEmpty()
             0 * fundBalanceConverter.convert(fundBalanceDto, person)
+    }
+
+    def "filters non-active out zero balance funds"() {
+        given:
+        def person = samplePerson()
+        def nonActiveZeroFund = FundBalanceDto.builder().isin("1").value(ZERO).activeContributions(false).build()
+        def nonActiveNonZeroFund = FundBalanceDto.builder().isin("2").value(ONE).activeContributions(false).build()
+        def activeZeroFund = FundBalanceDto.builder().isin("3").value(ZERO).activeContributions(true).build()
+        def activeNonZeroFund = FundBalanceDto.builder().isin("4").value(ONE).activeContributions(true).build()
+
+        episService.getAccountStatement(person) >> [nonActiveZeroFund, nonActiveNonZeroFund, activeZeroFund, activeNonZeroFund]
+        fundBalanceConverter.convert(_, person) >> { FundBalanceDto fundBalanceDto, _ ->
+            FundBalance.builder().fund(Fund.builder().isin(fundBalanceDto.isin).build()).build()
+        }
+
+        when:
+        List<FundBalance> accountStatement = service.getAccountStatement(person)
+
+        then:
+        with(accountStatement.get(0)) {
+            isin == nonActiveNonZeroFund.isin
+        }
+        with(accountStatement.get(1)) {
+            isin == activeZeroFund.isin
+        }
+        with(accountStatement.get(2)) {
+            isin == activeNonZeroFund.isin
+        }
+        accountStatement.size() == 3
     }
 
     def "handles fundBalanceDto to fundBalance conversion exceptions"() {

d1b8cb881b84863a5c149db7c3d41b7242d15579
Erko Risthein <erko@risthein.ee>
2020-09-11 19:14:40 +0300

    Fix integration test

--- src/test/groovy/ee/tuleva/onboarding/user/UserCreationIntegrationSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/user/UserCreationIntegrationSpec.groovy
@@ -1,11 +1,16 @@
 package ee.tuleva.onboarding.user
 
-import ee.tuleva.onboarding.auth.UserFixture
+import ee.tuleva.onboarding.epis.EpisService
 import ee.tuleva.onboarding.user.command.CreateUserCommand
 import ee.tuleva.onboarding.user.response.UserResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import spock.lang.Specification
+
+import static ee.tuleva.onboarding.auth.UserFixture.sampleUserNonMember
+import static ee.tuleva.onboarding.epis.contact.ContactDetailsFixture.contactDetailsFixture
+import static org.mockito.Mockito.when
 
 @SpringBootTest
 public class UserCreationIntegrationSpec extends Specification {
@@ -14,18 +19,21 @@ public class UserCreationIntegrationSpec extends Specification {
     UserService userService
     @Autowired
     UserController userController
+    @MockBean
+    EpisService episService
 
     def "handles non member user re-registering"() {
         given:
-        User userToCreateFirst = UserFixture.sampleUserNonMember()
-                .build()
+        User userToCreateFirst = sampleUserNonMember().build()
+        when(episService.getContactDetails(userToCreateFirst)).thenReturn(contactDetailsFixture())
 
         when:
         User newUser = userService.createNewUser(userToCreateFirst)
-        CreateUserCommand createUserCommand = new CreateUserCommand()
-        createUserCommand.setEmail(userToCreateFirst.email)
-        createUserCommand.setPersonalCode(userToCreateFirst.personalCode)
-        createUserCommand.setPhoneNumber(userToCreateFirst.phoneNumber)
+        CreateUserCommand createUserCommand = new CreateUserCommand(
+            email: userToCreateFirst.email,
+            personalCode: userToCreateFirst.personalCode,
+            phoneNumber: userToCreateFirst.phoneNumber
+        )
         UserResponse userResponse = userController.createUser(createUserCommand, null)
 
         then:

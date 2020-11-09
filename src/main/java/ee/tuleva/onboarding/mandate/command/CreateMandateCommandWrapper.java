df77b2f9cd3e601ed5b0289fb3f37625edbb8b4b
Erko Risthein <erko@risthein.ee>
2020-11-09 16:50:41 +0200

    Add missing files

---
+++ src/main/java/ee/tuleva/onboarding/mandate/command/CreateMandateCommandWrapper.java
@@ -0,0 +1,16 @@
+package ee.tuleva.onboarding.mandate.command;
+
+import ee.tuleva.onboarding.conversion.ConversionResponse;
+import ee.tuleva.onboarding.epis.contact.UserPreferences;
+import ee.tuleva.onboarding.user.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class CreateMandateCommandWrapper {
+    private final CreateMandateCommand createMandateCommand;
+    private final User user;
+    private final ConversionResponse conversion;
+    private final UserPreferences contactDetails;
+}

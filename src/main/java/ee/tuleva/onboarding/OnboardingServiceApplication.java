cab10a3c76797e5ca7f68ed92e2420e5355ec310
Erko Risthein <erko@risthein.ee>
2020-11-15 22:58:18 +0200

    Upgrade dependencies

--- src/main/java/ee/tuleva/onboarding/OnboardingServiceApplication.java
+++ src/main/java/ee/tuleva/onboarding/OnboardingServiceApplication.java
@@ -1,16 +1,12 @@
 package ee.tuleva.onboarding;
 
-import io.sentry.Sentry;
-import java.security.Security;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.info.GitProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Profiles;
+
+import java.security.Security;
 
 @SpringBootApplication
 @Slf4j
@@ -21,26 +17,12 @@ public class OnboardingServiceApplication {
     System.setProperty("http.agent", "HTTPie/1.0.2");
     Security.addProvider(new BouncyCastleProvider());
     val context = SpringApplication.run(OnboardingServiceApplication.class, args);
-    trackRelease(context);
     validateFileEncoding();
   }
 
   private static void validateFileEncoding() {
     if (!System.getProperty("file.encoding", "").toLowerCase().equals("utf-8")) {
       log.error("Unsupported file encoding {}!", System.getProperty("file.encoding"));
-    }
-  }
-
-  private static void trackRelease(ApplicationContext context) {
-    try {
-      val gitProperties = context.getBean(GitProperties.class);
-      Sentry.getStoredClient().setRelease(gitProperties.getShortCommitId());
-      val isProduction = context.getEnvironment().acceptsProfiles(Profiles.of("production"));
-      if (isProduction) {
-        Sentry.getStoredClient().setEnvironment("production");
-      }
-    } catch (NoSuchBeanDefinitionException e) {
-      log.info("No git properties found, continuing without release info");
     }
   }
 }

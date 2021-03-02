b4304ebe1a88d524865a6e38baf5fc46032c7d4e
Erko Risthein <erko@risthein.ee>
2021-03-02 16:50:36 +0200

    Configure Docker deployment

--- gradle/packaging.gradle.kts
+++ gradle/packaging.gradle.kts
@@ -1,18 +1,45 @@
+val gitProps: Map<String, String> by extra
+
 tasks {
-
-    val unpack by creating(Copy::class) {
-        dependsOn(named("bootJar"))
-        from(zipTree(named("bootJar").get().outputs.files.singleFile)) {
-            into("dependency")
-        }
-        from("$rootDir/etc/docker/entrypoint.sh") {
-            into("dependency")
-        }
-        from("$rootDir/etc/docker/Dockerfile")
-        into("build/docker")
+  val collectFiles by creating(Copy::class) {
+    dependsOn("jar", "generateGitProperties")
+    from("$rootDir/etc/eb/.ebextensions") {
+      into(".ebextensions")
     }
-
-    named("assemble") {
-        dependsOn(unpack)
+    from("$rootDir/etc/eb/Procfile")
+    val jar = tasks.getByName<Jar>("jar")
+    from(jar.outputs.files.singleFile) {
+      rename { "application.jar" }
     }
+    destinationDir = file("$buildDir/zip")
+    doLast {
+      copy {
+        from("$rootDir/etc/eb/docker-compose.yml") {
+          expand("hash" to gitProps["git.commit.id"]?.substring(0, 8))
+        }
+        into("$buildDir/zip")
+      }
+    }
+  }
+
+  val zipWithExtensions by creating(Zip::class) {
+    dependsOn(collectFiles)
+    from(collectFiles)
+  }
+
+  val unpack by creating(Copy::class) {
+    dependsOn(named("bootJar"))
+    from(zipTree(named("bootJar").get().outputs.files.singleFile)) {
+      into("dependency")
+    }
+    from("$rootDir/etc/docker/entrypoint.sh") {
+      into("dependency")
+    }
+    from("$rootDir/etc/docker/Dockerfile")
+    into("build/docker")
+  }
+
+  named("assemble") {
+    dependsOn(unpack, zipWithExtensions)
+  }
 }

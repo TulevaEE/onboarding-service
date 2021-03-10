dae6a286b5f05a472db43cd4f00adc0fc131d70f
Erko Risthein <erko@risthein.ee>
2021-03-10 14:54:45 +0200

    Hotfix

--- src/main/java/ee/tuleva/onboarding/epis/mandate/ApplicationResponseDTO.java
+++ src/main/java/ee/tuleva/onboarding/epis/mandate/ApplicationResponseDTO.java
@@ -5,11 +5,14 @@ import static java.util.Collections.singletonList;
 import ee.tuleva.onboarding.epis.application.ApplicationResponse;
 import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ApplicationResponseDTO {
+  
   private List<ApplicationResponse> mandateResponses;
 
   public ApplicationResponseDTO(ApplicationResponse applicationResponse) {

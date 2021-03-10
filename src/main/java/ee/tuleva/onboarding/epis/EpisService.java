55322f0ca57c500fd4ae4b5d4063dab7fcdaa851
Erko Risthein <erko@risthein.ee>
2021-03-10 14:32:01 +0200

    Fix import

--- src/main/java/ee/tuleva/onboarding/epis/EpisService.java
+++ src/main/java/ee/tuleva/onboarding/epis/EpisService.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpMethod.GET;
 
 import ee.tuleva.onboarding.auth.principal.Person;
 import ee.tuleva.onboarding.epis.account.FundBalanceDto;
+import ee.tuleva.onboarding.epis.application.ApplicationResponse;
 import ee.tuleva.onboarding.epis.cancellation.CancellationDto;
 import ee.tuleva.onboarding.epis.cashflows.CashFlowStatement;
 import ee.tuleva.onboarding.epis.contact.UserPreferences;

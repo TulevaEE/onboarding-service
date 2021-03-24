a8bf683775fd064c4f851cbfda49033e0d617b01
Erko Risthein <erko@risthein.ee>
2021-03-24 22:42:11 +0200

    Fix translations, add tests

--- src/test/groovy/ee/tuleva/onboarding/mandate/email/MandateEmailContentServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/email/MandateEmailContentServiceSpec.groovy
@@ -1,11 +1,13 @@
 package ee.tuleva.onboarding.mandate.email
 
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
 import static ee.tuleva.onboarding.auth.UserFixture.sampleUser
 import static ee.tuleva.onboarding.auth.UserFixture.sampleUserNonMember
+import static ee.tuleva.onboarding.mandate.MandateFixture.sampleMandate
 import static java.util.Locale.ENGLISH
 
 @SpringBootTest
@@ -105,4 +107,27 @@ class MandateEmailContentServiceSpec extends Specification {
         html.contains('You are now saving for your pension alongside me and other Tuleva members.')
         html.contains('test_account_1')
     }
+
+  def "renders 2nd pillar transfer cancellation email"() {
+    given:
+    def user = sampleUserNonMember().build()
+    def mandate = sampleMandate()
+    when:
+    String html = emailContentService.getSecondPillarTransferCancellationHtml(user, mandate, ENGLISH)
+
+    then:
+    html.contains('You have submitted a cancellation application through Tuleva.')
+    html.contains(mandate.getFundTransferExchanges().get(0).getSourceFundIsin())
+  }
+
+  def "renders 2nd pillar withdrawal cancellation email"() {
+    given:
+    def user = sampleUserNonMember().build()
+    def mandate = sampleMandate()
+    when:
+    String html = emailContentService.getSecondPillarWithdrawalCancellationHtml(user, ENGLISH)
+
+    then:
+    html.contains('You have cancelled your 2nd pillar withdrawal application.')
+  }
 }

76cee962a77257a81a68c3efafce353e059516fe
Maido Kaara <maido@producement.com>
2021-06-06 11:07:25 +0300

    Logout controller

---
+++ src/main/java/ee/tuleva/onboarding/auth/LogoutController.java
@@ -0,0 +1,27 @@
+package ee.tuleva.onboarding.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class LogoutController {
+
+  private final TokenStore tokenStore;
+
+  @GetMapping(value = "/logout")
+  @ResponseStatus(HttpStatus.OK)
+  public void logout(@RequestHeader(value = "Authorization") String authHeader) {
+    String tokenValue = authHeader.replace("Bearer", "").trim();
+    OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+    tokenStore.removeAccessToken(accessToken);
+  }
+}

3478fb2d8ec4a19d7176af66be2a336cb72ae542
Erko Risthein <erko@risthein.ee>
2021-07-02 15:03:55 +0300

    Add proper phone number validation

--- src/main/java/ee/tuleva/onboarding/auth/command/AuthenticateCommand.java
+++ src/main/java/ee/tuleva/onboarding/auth/command/AuthenticateCommand.java
@@ -1,13 +1,26 @@
 package ee.tuleva.onboarding.auth.command;
 
 import ee.tuleva.onboarding.user.personalcode.ValidPersonalCode;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
+@Builder
 public class AuthenticateCommand {
+
+  @NotBlank
+  @Length(min = 7, max = 30)
+  @Pattern(regexp = "^\\+?\\d{7,30}$")
   private String phoneNumber;
+
   @ValidPersonalCode private String personalCode;
+
+  @NotNull
   private AuthenticationType type;
 }

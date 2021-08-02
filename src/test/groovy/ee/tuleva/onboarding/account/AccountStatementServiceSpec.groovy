df4515385f7d78e9fd7a0cf2e74f2df0005c22d1
Mats Johanson <mats@johanson.ee>
2021-08-02 17:50:05 +0300

    Format groovy spec to match style guide

--- src/test/groovy/ee/tuleva/onboarding/account/AccountStatementServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/account/AccountStatementServiceSpec.groovy
@@ -13,84 +13,84 @@ import static java.math.BigDecimal.ZERO
 
 class AccountStatementServiceSpec extends Specification {
 
-    def episService = Mock(EpisService)
-    def fundBalanceConverter = Mock(FundBalanceDtoToFundBalanceConverter)
+  def episService = Mock(EpisService)
+  def fundBalanceConverter = Mock(FundBalanceDtoToFundBalanceConverter)
 
-    def service = new AccountStatementService(episService, fundBalanceConverter)
+  def service = new AccountStatementService(episService, fundBalanceConverter)
 
-    def "returns an account statement"() {
-        given:
-        def person = samplePerson()
-        def fundBalanceDto = FundBalanceDto.builder().isin("someIsin").build()
-        def fundBalance = activeTuleva2ndPillarFundBalance.first()
+  def "returns an account statement"() {
+    given:
+    def person = samplePerson()
+    def fundBalanceDto = FundBalanceDto.builder().isin("someIsin").build()
+    def fundBalance = activeTuleva2ndPillarFundBalance.first()
 
-        episService.getAccountStatement(person) >> [fundBalanceDto]
-        fundBalanceConverter.convert(fundBalanceDto, person) >> fundBalance
+    episService.getAccountStatement(person) >> [fundBalanceDto]
+    fundBalanceConverter.convert(fundBalanceDto, person) >> fundBalance
 
-        when:
-        List<FundBalance> accountStatement = service.getAccountStatement(person)
+    when:
+    List<FundBalance> accountStatement = service.getAccountStatement(person)
 
-        then:
-        accountStatement == [fundBalance]
+    then:
+    accountStatement == [fundBalance]
+  }
+
+  def "fundBalanceDto with no Isin code are filtered out and will not try to convert"() {
+    given:
+    def person = samplePerson()
+    def fundBalanceDto = FundBalanceDto.builder().isin(null).build()
+    episService.getAccountStatement(person) >> [fundBalanceDto]
+
+    when:
+    List<FundBalance> accountStatement = service.getAccountStatement(person)
+
+    then:
+    accountStatement.isEmpty()
+    0 * fundBalanceConverter.convert(fundBalanceDto, person)
+  }
+
+  def "filters non-active out zero balance funds"() {
+    given:
+    def person = samplePerson()
+    def nonActiveZeroFund = FundBalanceDto.builder().isin("1").value(ZERO).activeContributions(false).build()
+    def nonActiveNonZeroFund = FundBalanceDto.builder().isin("2").value(ONE).activeContributions(false).build()
+    def activeZeroFund = FundBalanceDto.builder().isin("3").value(ZERO).activeContributions(true).build()
+    def activeNonZeroFund = FundBalanceDto.builder().isin("4").value(ONE).activeContributions(true).build()
+
+    episService.getAccountStatement(person) >> [nonActiveZeroFund, nonActiveNonZeroFund, activeZeroFund, activeNonZeroFund]
+    fundBalanceConverter.convert(_, person) >> { FundBalanceDto fundBalanceDto, _ ->
+      FundBalance.builder().fund(Fund.builder().isin(fundBalanceDto.isin).build()).build()
     }
 
-    def "fundBalanceDto with no Isin code are filtered out and will not try to convert"() {
-        given:
-            def person = samplePerson()
-            def fundBalanceDto = FundBalanceDto.builder().isin(null).build()
-            episService.getAccountStatement(person) >> [fundBalanceDto]
+    when:
+    List<FundBalance> accountStatement = service.getAccountStatement(person)
 
-        when:
-            List<FundBalance> accountStatement = service.getAccountStatement(person)
+    then:
+    with(accountStatement.get(0)) {
+      isin == nonActiveNonZeroFund.isin
+    }
+    with(accountStatement.get(1)) {
+      isin == activeZeroFund.isin
+    }
+    with(accountStatement.get(2)) {
+      isin == activeNonZeroFund.isin
+    }
+    accountStatement.size() == 3
+  }
 
-        then:
-            accountStatement.isEmpty()
-            0 * fundBalanceConverter.convert(fundBalanceDto, person)
+  def "handles fundBalanceDto to fundBalance conversion exceptions"() {
+    given:
+    def person = samplePerson()
+    def fundBalanceDto = FundBalanceDto.builder().isin("someIsin").build()
+
+    episService.getAccountStatement(person) >> [fundBalanceDto]
+    fundBalanceConverter.convert(fundBalanceDto, person) >> {
+      throw new IllegalArgumentException()
     }
 
-    def "filters non-active out zero balance funds"() {
-        given:
-        def person = samplePerson()
-        def nonActiveZeroFund = FundBalanceDto.builder().isin("1").value(ZERO).activeContributions(false).build()
-        def nonActiveNonZeroFund = FundBalanceDto.builder().isin("2").value(ONE).activeContributions(false).build()
-        def activeZeroFund = FundBalanceDto.builder().isin("3").value(ZERO).activeContributions(true).build()
-        def activeNonZeroFund = FundBalanceDto.builder().isin("4").value(ONE).activeContributions(true).build()
+    when:
+    service.getAccountStatement(person)
 
-        episService.getAccountStatement(person) >> [nonActiveZeroFund, nonActiveNonZeroFund, activeZeroFund, activeNonZeroFund]
-        fundBalanceConverter.convert(_, person) >> { FundBalanceDto fundBalanceDto, _ ->
-            FundBalance.builder().fund(Fund.builder().isin(fundBalanceDto.isin).build()).build()
-        }
-
-        when:
-        List<FundBalance> accountStatement = service.getAccountStatement(person)
-
-        then:
-        with(accountStatement.get(0)) {
-            isin == nonActiveNonZeroFund.isin
-        }
-        with(accountStatement.get(1)) {
-            isin == activeZeroFund.isin
-        }
-        with(accountStatement.get(2)) {
-            isin == activeNonZeroFund.isin
-        }
-        accountStatement.size() == 3
-    }
-
-    def "handles fundBalanceDto to fundBalance conversion exceptions"() {
-        given:
-        def person = samplePerson()
-        def fundBalanceDto = FundBalanceDto.builder().isin("someIsin").build()
-
-        episService.getAccountStatement(person) >> [fundBalanceDto]
-        fundBalanceConverter.convert(fundBalanceDto, person) >> {
-            throw new IllegalArgumentException()
-        }
-
-        when:
-        service.getAccountStatement(person)
-
-        then:
-        thrown(IllegalStateException)
-    }
+    then:
+    thrown(IllegalStateException)
+  }
 }

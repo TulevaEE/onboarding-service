497e1d46c36f11ead288afc26bd61f59e0e2f9c6
Mats Johanson <mats@johanson.ee>
2021-09-16 13:12:58 +0300

    Add serial version uid

--- src/main/java/ee/tuleva/onboarding/mandate/signature/SignatureFile.java
+++ src/main/java/ee/tuleva/onboarding/mandate/signature/SignatureFile.java
@@ -1,5 +1,6 @@
 package ee.tuleva.onboarding.mandate.signature;
 
+import java.io.Serial;
 import java.io.Serializable;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class SignatureFile implements Serializable {
+
+  @Serial private static final long serialVersionUID = -2405222829412049325L;
 
   private final String name;
   private final String mimeType;

4ba42ca8ca9d7440dd041a781b27a90bd3d194d7
Mats Johanson <mats@johanson.ee>
2021-09-21 21:00:00 +0300

    Add English translations and fix specs

--- src/test/groovy/ee/tuleva/onboarding/mandate/email/MandateEmailContentServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/email/MandateEmailContentServiceSpec.groovy
@@ -13,100 +13,38 @@ import static java.util.Locale.ENGLISH
 @SpringBootTest
 class MandateEmailContentServiceSpec extends Specification {
 
-    @Autowired
-    MandateEmailContentService emailContentService
+  @Autowired
+  MandateEmailContentService emailContentService
 
-    def "second pillar message: suggest third pillar when not fully converted to tuleva and not a member (third pillar active)"() {
-        given:
-        def user = sampleUserNonMember().build()
-        def isThirdPillarActive = true
-        def isFullyConverted = false
-        def pillarSuggestion = new PillarSuggestion(3, isThirdPillarActive, isFullyConverted, user.isMember())
+  def "second pillar message: suggest third pillar when not fully converted to tuleva and not a member (third pillar active)"() {
+    given:
+    def user = sampleUserNonMember().build()
+    def isThirdPillarActive = true
+    def isFullyConverted = false
+    def pillarSuggestion = new PillarSuggestion(3, isThirdPillarActive, isFullyConverted, user.isMember())
 
-        when:
-        String html = emailContentService.getSecondPillarHtml(user, pillarSuggestion, ENGLISH)
+    when:
+    String html = emailContentService.getSecondPillarHtml(user, pillarSuggestion, ENGLISH)
 
-        then:
-        html.contains('You are now saving for your pension alongside me and other Tuleva members.')
-        html.contains('Next, set up your third pillar.')
-    }
+    then:
+    html.contains('You are now saving for your pension alongside me and other Tuleva members.')
+    html.contains('Next, set up your third pillar.')
+  }
 
-    def "second pillar message: suggest membership when fully converted to tuleva and not a member"() {
-        given:
-        def user = sampleUserNonMember().build()
-        def isThirdPillarActive = true
-        def isFullyConverted = true
-        def pillarSuggestion = new PillarSuggestion(3, isThirdPillarActive, isFullyConverted, user.isMember())
+  def "second pillar message: suggest membership when fully converted to tuleva and not a member"() {
+    given:
+    def user = sampleUserNonMember().build()
+    def isThirdPillarActive = true
+    def isFullyConverted = true
+    def pillarSuggestion = new PillarSuggestion(3, isThirdPillarActive, isFullyConverted, user.isMember())
 
-        when:
-        String html = emailContentService.getSecondPillarHtml(user, pillarSuggestion, ENGLISH)
+    when:
+    String html = emailContentService.getSecondPillarHtml(user, pillarSuggestion, ENGLISH)
 
-        then:
-        html.contains('You are now saving for your pension alongside me and other Tuleva members.')
-        html.contains('I would still welcome you to think about becoming a member')
-    }
-
-    def "third pillar message: suggest second pillar when not fully converted to tuleva and not a member (second pillar active)"() {
-        given:
-        def user = sampleUserNonMember().build()
-        def isSecondPillarActive = true
-        def isFullyConverted = false
-        def pillarSuggestion = new PillarSuggestion(2, isSecondPillarActive, isFullyConverted, user.isMember())
-
-        when:
-        String html = emailContentService.getThirdPillarHtml(user, pillarSuggestion, "test_account_1", ENGLISH)
-
-        then:
-        html.contains('You are now saving for your pension alongside me and other Tuleva members.')
-        html.contains('Also, make sure your second pillar is in a fund with low fees.')
-        html.contains('test_account_1')
-    }
-
-    def "third pillar message: suggest membership when fully converted to tuleva and not a member"() {
-        given:
-        def user = sampleUserNonMember().build()
-        def isSecondPillarActive = true
-        def isFullyConverted = true
-        def pillarSuggestion = new PillarSuggestion(2, isSecondPillarActive, isFullyConverted, user.isMember())
-
-        when:
-        String html = emailContentService.getThirdPillarHtml(user, pillarSuggestion, "test_account_1", ENGLISH)
-
-        then:
-        html.contains('You are now saving for your pension alongside me and other Tuleva members.')
-        html.contains('I would still welcome you to think about becoming a member')
-        html.contains('test_account_1')
-    }
-
-    def "third pillar message: only action info when fully converted to tuleva and is a member"() {
-        given:
-        def user = sampleUser().build()
-        def isSecondPillarActive = true
-        def isFullyConverted = true
-        def pillarSuggestion = new PillarSuggestion(2, isSecondPillarActive, isFullyConverted, user.isMember())
-
-        when:
-        String html = emailContentService.getThirdPillarHtml(user, pillarSuggestion, "test_account_1", ENGLISH)
-
-        then:
-        html.contains('You are now saving for your pension alongside me and other Tuleva members.')
-        html.contains('test_account_1')
-    }
-
-    def "third pillar message: only action info when second pillar is not active and is a member"() {
-        given:
-        def user = sampleUser().build()
-        def isSecondPillarActive = false
-        def isFullyConverted = false
-        def pillarSuggestion = new PillarSuggestion(2, isSecondPillarActive, isFullyConverted, user.isMember())
-
-        when:
-        String html = emailContentService.getThirdPillarHtml(user, pillarSuggestion, "test_account_1", ENGLISH)
-
-        then:
-        html.contains('You are now saving for your pension alongside me and other Tuleva members.')
-        html.contains('test_account_1')
-    }
+    then:
+    html.contains('You are now saving for your pension alongside me and other Tuleva members.')
+    html.contains('I would still welcome you to think about becoming a member')
+  }
 
   def "renders 2nd pillar transfer cancellation email"() {
     given:
@@ -129,5 +67,20 @@ class MandateEmailContentServiceSpec extends Specification {
 
     then:
     html.contains('You have cancelled your 2nd pillar withdrawal application.')
+  }
+
+  def "third pillar message: only action info when fully converted to tuleva and is a member"() {
+    given:
+    def user = sampleUser().build()
+    def isSecondPillarActive = true
+    def isFullyConverted = true
+    def pillarSuggestion = new PillarSuggestion(2, isSecondPillarActive, isFullyConverted, user.isMember())
+
+    when:
+    String html = emailContentService.getThirdPillarPaymentDetailsHtml(user, pillarSuggestion, "test_account_1", ENGLISH)
+
+    then:
+    html.contains('Welcome, Jordan.')
+    html.contains('test_account_1')
   }
 }

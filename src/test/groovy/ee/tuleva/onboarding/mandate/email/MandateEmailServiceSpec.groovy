b542c405e29a34b0f30126ef869959783dbbb4b5
Mats Johanson <mats@johanson.ee>
2021-09-22 10:12:14 +0300

    Schedule second pillar suggestion e-mail three days after third pillar payment details

--- src/test/groovy/ee/tuleva/onboarding/mandate/email/MandateEmailServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/email/MandateEmailServiceSpec.groovy
@@ -5,19 +5,26 @@ import ee.tuleva.onboarding.notification.email.EmailService
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.time.Clock
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
 import static com.microtripit.mandrillapp.lutung.view.MandrillMessage.Recipient
 import static ee.tuleva.onboarding.auth.UserFixture.sampleUser
+import static ee.tuleva.onboarding.conversion.ConversionResponseFixture.fullyConverted
 import static ee.tuleva.onboarding.conversion.ConversionResponseFixture.notFullyConverted
 import static ee.tuleva.onboarding.epis.contact.ContactDetailsFixture.contactDetailsFixture
 import static ee.tuleva.onboarding.mandate.MandateFixture.sampleMandate
+import static java.time.ZoneOffset.UTC
 
 class MandateEmailServiceSpec extends Specification {
 
   MandateEmailContentService emailContentService = Mock(MandateEmailContentService)
   EmailService emailService = Mock(EmailService)
-  MandateEmailService mandateEmailService = new MandateEmailService(emailService, emailContentService)
+  def now = Instant.parse("2021-09-01T10:06:01Z")
+  MandateEmailService mandateEmailService = new MandateEmailService(emailService, emailContentService, Clock.fixed(now, UTC))
 
-  def "Send mandate email"() {
+  def "Send second pillar mandate email"() {
     given:
     def user = sampleUser().build()
     def conversion = notFullyConverted()
@@ -39,6 +46,79 @@ class MandateEmailServiceSpec extends Specification {
     then:
     1 * emailService.newMandrillMessage(recipients, subject, html, tags, _) >> message
     1 * emailService.send(user, message)
+  }
+
+  def "Send third pillar payment details email"() {
+    given:
+    def user = sampleUser().build()
+    def conversion = fullyConverted()
+    def contactDetails = contactDetailsFixture()
+    def mandate = sampleMandate()
+    def pillarSuggestion = new PillarSuggestion(2, user, contactDetails, conversion)
+    def recipients = [new Recipient()]
+    def message = new MandrillMessage()
+    def subject = "Sinu 3. samba tähtis info ja avalduse koopia"
+    def html = "payment details html"
+    def tags = ["mandate"]
+    def locale = Locale.ENGLISH
+
+    emailContentService.getThirdPillarPaymentDetailsHtml(user, contactDetails.getPensionAccountNumber(), locale) >> html
+    emailService.getRecipients(user) >> recipients
+
+    when:
+    mandateEmailService.sendMandate(user, mandate, pillarSuggestion, contactDetails, locale)
+
+    then:
+    1 * emailService.newMandrillMessage(recipients, subject, html, tags, _) >> message
+    1 * emailService.send(user, message)
+  }
+
+  @Unroll
+  def "Send third pillar suggest second pillar email"() {
+    given:
+    def user = sampleUser().build()
+    def contactDetails = contactDetailsFixture()
+    def mandate = sampleMandate()
+    def pillarSuggestion = new PillarSuggestion(2, user, contactDetails, conversion)
+    def recipients = [new Recipient()]
+    def message = new MandrillMessage()
+    def subject = "Vaata oma teine sammas üle!"
+    def html = "suggest second html"
+    def tags = ["suggest_2"]
+    def locale = Locale.ENGLISH
+    def sendAt = now.plus(3, ChronoUnit.DAYS)
+
+    emailContentService.getThirdPillarSuggestSecondHtml(user, locale) >> html
+    emailService.getRecipients(user) >> recipients
+
+    when:
+    mandateEmailService.sendMandate(user, mandate, pillarSuggestion, contactDetails, locale)
+
+    then:
+    callCount * emailService.newMandrillMessage(recipients, subject, html, tags, null) >> message
+    callCount * emailService.send(user, message, sendAt)
+
+    where:
+    conversion          | callCount
+    notFullyConverted() | 1
+    fullyConverted()    | 0
+  }
+
+  def "Sends two third pillar emails"() {
+    given:
+    def user = sampleUser().build()
+    def conversion = notFullyConverted()
+    def contactDetails = contactDetailsFixture()
+    def pillarSuggestion = new PillarSuggestion(2, user, contactDetails, conversion)
+    emailContentService.getThirdPillarPaymentDetailsHtml(*_) >> "html"
+    emailService.getRecipients(user) >> [new Recipient()]
+
+    when:
+    mandateEmailService.sendMandate(user, sampleMandate(), pillarSuggestion, contactDetails, Locale.ENGLISH)
+
+    then:
+    2 * emailService.newMandrillMessage(*_) >> new MandrillMessage()
+    2 * emailService.send(*_)
   }
 
   @Unroll

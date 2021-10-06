083d6c3bb1e2b34ab7a7520297a45a744281235e
Mats Johanson <mats@johanson.ee>
2021-10-06 21:05:26 +0300

    Use verbose fixture building for argument clarity

--- src/test/groovy/ee/tuleva/onboarding/mandate/application/ApplicationDtoFixture.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/application/ApplicationDtoFixture.groovy
@@ -8,51 +8,64 @@ import static ee.tuleva.onboarding.epis.mandate.ApplicationStatus.PENDING
 import static ee.tuleva.onboarding.mandate.application.ApplicationType.*
 
 class ApplicationDtoFixture {
+
   static ApplicationDTO sampleTransferApplicationDto() {
     return ApplicationDTO.builder()
-      .date(ClockFixture.now)
-      .type(TRANSFER)
-      .status(PENDING)
-      .id(123L)
-      .currency("EUR")
-      .sourceFundIsin("source")
-      .fundTransferExchanges([
-        new MandateDto.MandateFundsTransferExchangeDTO("processId", 1.0, "source", "target", null)
-      ])
-      .build()
+        .date(ClockFixture.now)
+        .type(TRANSFER)
+        .status(PENDING)
+        .id(123L)
+        .currency("EUR")
+        .sourceFundIsin("source")
+        .fundTransferExchanges([
+            new MandateDto.MandateFundsTransferExchangeDTO(
+                processId: "processId",
+                amount: 1.0,
+                sourceFundIsin: "source",
+                targetFundIsin: "target",
+                targetPik: null
+            )
+        ])
+        .build()
   }
 
   static ApplicationDTO samplePikTransferApplicationDto() {
     return ApplicationDTO.builder()
-      .date(ClockFixture.now)
-      .type(TRANSFER)
-      .status(PENDING)
-      .id(123L)
-      .currency("EUR")
-      .sourceFundIsin("source")
-      .fundTransferExchanges([
-        new MandateDto.MandateFundsTransferExchangeDTO("processId", 1.0, "source", null, "EE801281685311741971")
-      ])
-      .build()
+        .date(ClockFixture.now)
+        .type(TRANSFER)
+        .status(PENDING)
+        .id(123L)
+        .currency("EUR")
+        .sourceFundIsin("source")
+        .fundTransferExchanges([
+            new MandateDto.MandateFundsTransferExchangeDTO(
+                processId: "processId",
+                amount: 1.0,
+                sourceFundIsin: "source",
+                targetFundIsin: null,
+                targetPik: "EE801281685311741971"
+            )
+        ])
+        .build()
   }
 
   static ApplicationDTO sampleWithdrawalApplicationDto() {
     return ApplicationDTO.builder()
-      .date(ClockFixture.now)
-      .type(WITHDRAWAL)
-      .status(PENDING)
-      .id(123L)
-      .bankAccount("IBAN")
-      .build()
+        .date(ClockFixture.now)
+        .type(WITHDRAWAL)
+        .status(PENDING)
+        .id(123L)
+        .bankAccount("IBAN")
+        .build()
   }
 
   static ApplicationDTO sampleEarlyWithdrawalApplicationDto() {
     return ApplicationDTO.builder()
-      .date(ClockFixture.now)
-      .type(EARLY_WITHDRAWAL)
-      .status(PENDING)
-      .id(123L)
-      .bankAccount("IBAN")
-      .build()
+        .date(ClockFixture.now)
+        .type(EARLY_WITHDRAWAL)
+        .status(PENDING)
+        .id(123L)
+        .bankAccount("IBAN")
+        .build()
   }
 }

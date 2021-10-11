a128ee190780d4782820d3e881e9b34e85e3106a
Erko Risthein <erko@risthein.ee>
2021-10-11 15:23:01 +0300

    Replace manual session handling with spring session scoped bean

--- src/main/java/ee/tuleva/onboarding/auth/session/GenericSessionStore.java
+++ src/main/java/ee/tuleva/onboarding/auth/session/GenericSessionStore.java
@@ -1,34 +1,34 @@
 package ee.tuleva.onboarding.auth.session;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
-import javax.servlet.http.HttpSession;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
-public class GenericSessionStore {
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class GenericSessionStore implements Serializable {
+
+  @Serial private static final long serialVersionUID = -648103071415508424L;
+
+  private final Map<String, Object> sessionAttributes = new HashMap<>();
 
   public <T extends Serializable> void save(T sessionAttribute) {
-    session().setAttribute(sessionAttribute.getClass().getName(), sessionAttribute);
+    sessionAttributes.put(sessionAttribute.getClass().getName(), sessionAttribute);
   }
 
   public <T extends Serializable> Optional<T> get(Class clazz) {
     @SuppressWarnings("unchecked")
-    T sessionAttribute = (T) session().getAttribute(clazz.getName());
+    T sessionAttribute = (T) sessionAttributes.get(clazz.getName());
 
     if (sessionAttribute == null) {
       return Optional.empty();
     }
 
     return Optional.of(sessionAttribute);
-  }
-
-  private static HttpSession session() {
-    ServletRequestAttributes attr =
-        (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-    boolean allowCreate = true;
-    return attr.getRequest().getSession(allowCreate);
   }
 }

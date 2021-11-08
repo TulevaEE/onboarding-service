a719632bd8bc724c9a2a78e1a5ff06cdd4370690
Mats Johanson <mats@johanson.ee>
2021-11-08 15:00:07 +0200

    Log principal with MDC
    This allows us to connect Sentry errors with users

---
+++ src/main/java/ee/tuleva/onboarding/config/MDCFilter.java
@@ -0,0 +1,54 @@
+package ee.tuleva.onboarding.config;
+
+import java.io.IOException;
+import java.security.Principal;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+@Slf4j
+@Component
+public class MDCFilter extends GenericFilterBean {
+
+  private final String PERSONAL_ID = "personalId";
+
+  @Override
+  public void destroy() {}
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+
+    boolean successfulRegistration = false;
+
+    HttpServletRequest req = (HttpServletRequest) request;
+    Principal principal = req.getUserPrincipal();
+
+    if (principal != null) {
+      String personalId = principal.getName();
+      successfulRegistration = registerPersonalId(personalId);
+    }
+
+    try {
+      chain.doFilter(request, response);
+    } finally {
+      if (successfulRegistration) {
+        MDC.remove(PERSONAL_ID);
+      }
+    }
+  }
+
+  private boolean registerPersonalId(String personalId) {
+    if (personalId != null && personalId.trim().length() > 0) {
+      MDC.put(PERSONAL_ID, personalId);
+      return true;
+    }
+    return false;
+  }
+}

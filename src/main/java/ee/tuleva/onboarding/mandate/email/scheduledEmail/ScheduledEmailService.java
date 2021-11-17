277aa8238763f0211f8df22dc86169a50a74cfba
Mats Johanson <mats@johanson.ee>
2021-11-17 13:45:18 +0200

    Add method to find and cancel all user's scheduled email of a type

--- src/main/java/ee/tuleva/onboarding/mandate/email/scheduledEmail/ScheduledEmailService.java
+++ src/main/java/ee/tuleva/onboarding/mandate/email/scheduledEmail/ScheduledEmailService.java
@@ -1,6 +1,8 @@
 package ee.tuleva.onboarding.mandate.email.scheduledEmail;
 
+import ee.tuleva.onboarding.notification.email.EmailService;
 import ee.tuleva.onboarding.user.User;
+import java.util.List;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,9 +12,17 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class ScheduledEmailService {
   private final ScheduledEmailRepository scheduledEmailRepository;
+  private final EmailService emailService;
 
   public void create(User user, String messageId, ScheduledEmailType type) {
     ScheduledEmail scheduledEmail = new ScheduledEmail(user.getId(), messageId, type);
     scheduledEmailRepository.save(scheduledEmail);
+  }
+
+  public void cancel(User user, ScheduledEmailType type) {
+    List<ScheduledEmail> emails =
+        scheduledEmailRepository.findAllByUserIdAndType(user.getId(), type);
+    emails.forEach(email -> emailService.cancelScheduledEmail(email.getMandrillMessageId()));
+    scheduledEmailRepository.deleteAll(emails);
   }
 }
--- src/test/groovy/ee/tuleva/onboarding/mandate/email/scheduledEmail/ScheduledEmailServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/email/scheduledEmail/ScheduledEmailServiceSpec.groovy
@@ -1,5 +1,6 @@
 package ee.tuleva.onboarding.mandate.email.scheduledEmail
 
+import ee.tuleva.onboarding.notification.email.EmailService
 import ee.tuleva.onboarding.user.User
 import spock.lang.Specification
 
@@ -8,11 +9,13 @@ import static ee.tuleva.onboarding.mandate.email.scheduledEmail.ScheduledEmailTy
 class ScheduledEmailServiceSpec extends Specification {
 
   ScheduledEmailRepository scheduledEmailRepository = Mock()
-  ScheduledEmailService service = new ScheduledEmailService(scheduledEmailRepository)
+  EmailService emailService = Mock()
+
+  ScheduledEmailService service = new ScheduledEmailService(scheduledEmailRepository, emailService)
 
   def 'creates scheduled email with correct attributes'() {
     given:
-    User user = User.builder().id(13L).build();
+    User user = new User(id: 13);
     String messageId = "12345"
     ScheduledEmailType type = SUGGEST_SECOND_PILLAR
 
@@ -26,5 +29,24 @@ class ScheduledEmailServiceSpec extends Specification {
             email.type == SUGGEST_SECOND_PILLAR &&
             email.mandrillMessageId == messageId
     })
+  }
+
+  def 'calls correct methods when cancelling scheduled emails'() {
+    given:
+    User user = new User(id: 13)
+    ScheduledEmailType type = SUGGEST_SECOND_PILLAR
+    List<ScheduledEmail> emails = [
+        new ScheduledEmail(userId: user.id, mandrillMessageId: "100", type: type),
+        new ScheduledEmail(userId: user.id, mandrillMessageId: "200", type: type)
+    ]
+
+    when:
+    service.cancel(user, type)
+
+    then:
+    1 * scheduledEmailRepository.findAllByUserIdAndType(user.id, type) >> emails
+    1 * emailService.cancelScheduledEmail("100")
+    1 * emailService.cancelScheduledEmail("200")
+    1 * scheduledEmailRepository.deleteAll(emails)
   }
 }

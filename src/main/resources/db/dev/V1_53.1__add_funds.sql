3b0d5991f8bc53e8a74cc9b7fd8ea7e435add48c
Mats Johanson <mats@johanson.ee>
2021-11-25 08:57:31 +0200

    Add new funds from production

---
+++ src/main/resources/db/dev/V1_53.1__add_funds.sql
@@ -0,0 +1,172 @@
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001723', 0.0040670, 1, 0.01010000, 2, 'ACTIVE', 'LHV Pension Fund Green',
+        'LHV Pensionifond Roheline', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019782', 0.0057600, 1, 0.00530000, 2, 'ACTIVE', 'LHV Pension Fund XS', 'LHV Pensionifond XS',
+        0.00000000, 'LXK00');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019717', 0.0049000, 3, 0.00500000, 2, 'ACTIVE', 'SEB Conservative Pension Fund',
+        'SEB Konservatiivne Pensionifond', 0.00000000, 'SEK00');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600098612', 0.0082000, 3, 0.00990000, 2, 'ACTIVE', 'SEB Optimal Pension Fund',
+        'SEB Optimaalne Pensionifond', 0.00000000, 'SEK25');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001699', 0.0100000, 3, 0.00990000, 2, 'ACTIVE', 'SEB Pension Fund 100', 'SEB Pensionifond 100',
+        0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019741', 0.0079000, 2, 0.00660000, 2, 'ACTIVE', 'Swedbank Pension Fund K30',
+        'Swedbank Pensionifond K30', 0.00000000, 'SWK25');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001764', 0.0049000, 1, 0.01030000, 3, 'ACTIVE', 'LHV Pension Fund Green Plus',
+        'LHV Pensionifond Roheline Pluss', 0.00000000, 'LRT100');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600011813', 0.0155000, 1, 0.01550000, 3, 'LIQUIDATED', 'LHV Pension Fund 100 Plus',
+        'LHV Pensionifond 100 Pluss', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019808', 0.0123025, 1, 0.01230250, 2, 'LIQUIDATED', 'LHV Pension Fund 50', 'LHV Pensionifond 50',
+        0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019816', 0.0054285, 1, 0.00542850, 2, 'LIQUIDATED', 'LHV Pension Fund Intress',
+        'LHV Pensionifond Intress', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600100574', 0.0095000, 1, 0.00950000, 3, 'LIQUIDATED', 'LHV Pension Fund Intress Pluss',
+        'LHV Pensionifond Intress Pluss', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019840', 0.0075000, 4, 0.00750000, 2, 'LIQUIDATED', 'Nordea Pensionifond 2P1',
+        'Nordea Pensionifond 2P1', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019857', 0.0125000, 4, 0.01250000, 2, 'LIQUIDATED', 'Nordea Pensionifond 2P2',
+        'Nordea Pensionifond 2P2', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600108676', 0.0150000, 4, 0.01500000, 2, 'LIQUIDATED', 'Nordea Pensionifond 2P3',
+        'Nordea Pensionifond 2P3', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001749', 0.0029000, 2, 0.00400000, 3, 'ACTIVE',
+        'Swedbank Pension Fund V30 index (limited redemption)', 'Swedbank Pensionifond V30 indeks (väljumine piiratud)',
+        0.00000000, ' SWV30');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600099685', 0.0100000, 4, 0.01000000, 3, 'LIQUIDATED', 'Nordea Pensionifond 3P1 osak',
+        'Nordea Pensionifond 3P1 osak', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001731', 0.0029000, 2, 0.00400000, 3, 'ACTIVE',
+        'Swedbank Pension Fund V60 index (limited redemption)', 'Swedbank Pensionifond V60 indeks (väljumine piiratud)',
+        0.00000000, ' SWV60');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600109427', 0.0029000, 3, 0.00360000, 2, 'ACTIVE', 'SEB Pension Fund Index 100',
+        'SEB Pensionifond Indeks 100', 0.00000000, 'SIK75');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001806', 0.0025000, 4, 0.00460000, 2, 'ACTIVE', 'Luminor index pension fund "Sustainable Future"',
+        'Pensionifond Luminor Jätkusuutlik Tulevik, Indeks', 0.00000000, 'NIK100');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001798', 0.0025000, 4, 0.00560000, 3, 'ACTIVE',
+        'Luminor Supplamentary Pension Fund "Sustainable Future", Index',
+        'Luminor Täiendav Pensionifond Jätkusuutlik Tulevik, Indeks', 0.00000000, 'NIT100');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001814', 0.0029000, 2, 0.00330000, 2, 'ACTIVE', 'Swedbank Pension Fund Index',
+        'Swedbank Pensionifond Indeks', 0.00000000, 'SWK100');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600001822', 0.0029000, 2, 0.00290000, 3, 'ACTIVE', 'Swedbank III Pillar Pension Fund Index',
+        'Swedbank III Samba Pensionifond Indeks', 0.00000000, 'SWI100');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600099693', 0.0125000, 4, 0.01250000, 3, 'LIQUIDATED', 'Nordea Pensionifond 3P2 osak',
+        'Nordea Pensionifond 3P2 osak', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600099701', 0.0125000, 4, 0.01250000, 3, 'LIQUIDATED', 'Nordea Pensionifond 3P3 osak',
+        'Nordea Pensionifond 3P3 osak', 0.00000000, '');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019832', 0.0120000, 1, 0.01140000, 2, 'ACTIVE', 'LHV Pension Fund L', 'LHV Pensionifond L',
+        0.00000000, 'LLK50');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019824', 0.0067200, 1, 0.00620000, 2, 'ACTIVE', 'LHV Pension Fund S', 'LHV Pensionifond S',
+        0.00000000, 'LSK00');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019766', 0.0120000, 1, 0.01130000, 2, 'ACTIVE', 'LHV Pension Fund XL', 'LHV Pensionifond XL',
+        0.00000000, 'LXK75');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600098448', 0.0119000, 4, 0.01390000, 2, 'ACTIVE', 'Luminor B Pension Fund', 'Luminor B Pensionifond',
+        0.00000000, 'NPK25');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600098455', 0.0070000, 4, 0.01000000, 2, 'ACTIVE', 'Luminor C Pension Fund', 'Luminor C Pensionifond',
+        0.00000000, 'NPK00');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600019733', 0.0029000, 2, 0.00370000, 2, 'ACTIVE', 'Swedbank Pension Fund K10',
+        'Swedbank Pensionifond K10', 0.00000000, 'SWK00');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600109393', 0.0049000, 2, 0.00330000, 2, 'ACTIVE', 'Swedbank Pension Fund K1990-1999 Index',
+        'Swedbank Pensionifond K1990-1999 indeks', 0.00000000, 'SWK99');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600109476', 0.0120000, 1, 0.01260000, 2, 'LIQUIDATED', 'LHV Pension Fund Eesti ',
+        'LHV Pensionifond Eesti', 0.00000000, 'LEK75');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600098430', 0.0119000, 4, 0.01390000, 2, 'ACTIVE', 'Luminor A Pension Fund', 'Luminor A Pensionifond',
+        0.00000000, 'NPK50');
+
+INSERT INTO public.fund (isin, management_fee_rate, fund_manager_id, ongoing_charges_figure, pillar, status,
+                         name_english, name_estonian, equity_share, short_name)
+VALUES ('EE3600103503', 0.0119000, 4, 0.01450000, 2, 'ACTIVE', 'Luminor A Pluss Pension Fund',
+        'Luminor A Pluss Pensionifond', 0.00000000, 'NPK75');

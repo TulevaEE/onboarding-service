738f62f0b403719dd822eeaa21261df9df0b66e1
Mats Johanson <mats@johanson.ee>
2021-12-01 10:53:52 +0200

    Restore automatic mandate transfer date calculation
    It's now safe to use since we've fixed MandateDeadlines to support dates in december

--- src/main/java/ee/tuleva/onboarding/mandate/email/MandateEmailContentService.java
+++ src/main/java/ee/tuleva/onboarding/mandate/email/MandateEmailContentService.java
@@ -1,9 +1,13 @@
 package ee.tuleva.onboarding.mandate.email;
 
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+import ee.tuleva.onboarding.deadline.MandateDeadlines;
 import ee.tuleva.onboarding.deadline.MandateDeadlinesService;
 import ee.tuleva.onboarding.mandate.Mandate;
 import ee.tuleva.onboarding.user.User;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Locale;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,11 +30,9 @@ public class MandateEmailContentService {
     ctx.setVariable("firstName", user.getFirstName());
     ctx.setVariable("thirdPillarSuggestion", thirdPillarSuggestion);
 
-    //    DateTimeFormatter formatter =
-    //        DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneId.of("Europe/Tallinn"));
-    //    MandateDeadlines deadlines = mandateDeadlinesService.getDeadlines(mandateDate);
-    //    LocalDate transferDate = deadlines.getTransferMandateFulfillmentDate();
-    //    ctx.setVariable("transferDate", formatter.format(transferDate));
+    MandateDeadlines deadlines = mandateDeadlinesService.getDeadlines(mandateDate);
+    LocalDate transferDate = deadlines.getTransferMandateFulfillmentDate();
+    ctx.setVariable("transferDate", transferDate.format(ofPattern("dd.MM.yyyy")));
 
     return templateEngine.process("second_pillar_mandate", ctx);
   }

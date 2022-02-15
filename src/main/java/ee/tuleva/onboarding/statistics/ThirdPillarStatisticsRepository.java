3f4ee70f97a51c773ebeb3b9faf2e4e61f013ff7
Mats Johanson <mats@johanson.ee>
2022-02-15 13:37:37 +0200

    Remove unused statistics logic

--- src/main/java/ee/tuleva/onboarding/statistics/ThirdPillarStatisticsRepository.java
+++
@@ -1,6 +0,0 @@
-package ee.tuleva.onboarding.statistics;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThirdPillarStatisticsRepository
-    extends JpaRepository<ThirdPillarStatistics, Long> {}

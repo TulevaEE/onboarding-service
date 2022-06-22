e1c68bb2ac98ce4bfd5bd81ad6f9a7a7a7c381b8
Erko Risthein <erko@producement.com>
2022-06-22 12:03:19 +0300

    Fix missing SmartID challenge code

--- src/main/java/ee/tuleva/onboarding/auth/smartid/SmartIdSession.java
+++ src/main/java/ee/tuleva/onboarding/auth/smartid/SmartIdSession.java
@@ -14,7 +14,6 @@ public class SmartIdSession implements Person, Serializable {
   @Serial private static final long serialVersionUID = 6326478770346040900L;
 
   private final String verificationCode;
-  private final SmartIdAuthenticationResponse authenticationResponse;
   private final String personalCode;
   private final AuthenticationHash authenticationHash;
   private String firstName;

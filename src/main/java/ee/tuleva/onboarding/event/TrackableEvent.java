b6aa798f3d972cbc6143fb0e7ca227cd8e785c8f
gnortsmralient <jordan@valdma.com>
2022-08-16 11:26:02 +0300

    Tracking controller
    https://app.asana.com/0/229163390356948/1202735328740062

--- src/main/java/ee/tuleva/onboarding/event/TrackableEvent.java
+++ src/main/java/ee/tuleva/onboarding/event/TrackableEvent.java
@@ -1,10 +1,15 @@
 package ee.tuleva.onboarding.event;
 
+import java.util.Map;
 import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
 
 public class TrackableEvent extends AuditApplicationEvent {
 
   public TrackableEvent(String principal, String type, String... data) {
+    super(principal, type, data);
+  }
+
+  public TrackableEvent(String principal, String type, Map<String, Object> data) {
     super(principal, type, data);
   }
 }
---
+++ src/test/groovy/ee/tuleva/onboarding/event/TrackingControllerSpec.groovy
@@ -0,0 +1,33 @@
+package ee.tuleva.onboarding.event
+
+import ee.tuleva.onboarding.BaseControllerSpec
+import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson
+import org.springframework.http.MediaType
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class TrackingControllerSpec  extends BaseControllerSpec {
+
+  TrackableEventPublisher trackableEventPublisher = Mock()
+  TrackingController trackingController = new TrackingController(trackableEventPublisher)
+
+  def "POST /t adds tracks an event"() {
+    given:
+    def mvc = mockMvcWithAuthenticationPrincipal(sampleAuthenticatedPerson, trackingController)
+    def data = ["1":2]
+    1 * trackableEventPublisher.publish(sampleAuthenticatedPerson.personalCode, TrackableEventType.PAGE_VIEW, data)
+    expect:
+    mvc.perform(post("/v1/t")
+        .content("""{"type": "PAGE_VIEW", "data": {"1": 2}}""")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+  }
+
+  AuthenticatedPerson sampleAuthenticatedPerson = AuthenticatedPerson.builder()
+      .firstName("Jordan")
+      .lastName("Valdma")
+      .personalCode("38501010000")
+      .userId(2L)
+      .build()
+}

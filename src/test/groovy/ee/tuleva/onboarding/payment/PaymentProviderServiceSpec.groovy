edd47f2c975888ecdf03f1f0ec6e2890d5fec049
Jordan <Jordan>
2022-09-23 12:09:13 +0300

    Rebase onto master

--- src/test/groovy/ee/tuleva/onboarding/payment/PaymentProviderServiceSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/payment/PaymentProviderServiceSpec.groovy
@@ -2,19 +2,18 @@ package ee.tuleva.onboarding.payment
 
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson
 import ee.tuleva.onboarding.auth.principal.Person
-import ee.tuleva.onboarding.currency.Currency
 import ee.tuleva.onboarding.epis.EpisService
 import spock.lang.Specification
 
 import java.time.Clock
 import java.time.Instant
 
-import static ee.tuleva.onboarding.currency.Currency.EUR
 import static ee.tuleva.onboarding.epis.contact.ContactDetailsFixture.contactDetailsFixture
-import static ee.tuleva.onboarding.payment.PaymentProviderConfigurationFixture.aPaymentProviderBankConfiguration
+import static ee.tuleva.onboarding.currency.Currency.EUR
 import static ee.tuleva.onboarding.payment.Bank.LHV
 import static PaymentFixture.aPaymentProviderBankConfiguration
 import static PaymentFixture.anInternalReferenceSerialized
+import static PaymentFixture.aSerializedToken
 import static java.time.ZoneOffset.UTC
 
 class PaymentProviderServiceSpec extends Specification {
@@ -29,7 +28,6 @@ class PaymentProviderServiceSpec extends Specification {
   PaymentProviderService paymentLinkService
 
   void setup() {
-    paymentProviderBankConfigurations.put(LHV.getBeanName(), samplePaymentProviderBankConfiguration())
     paymentProviderBankConfigurations.put(LHV.getBeanName(), aPaymentProviderBankConfiguration())
     paymentLinkService = new PaymentProviderService(
         clock,
@@ -46,19 +44,18 @@ class PaymentProviderServiceSpec extends Specification {
     String internalReference = anInternalReferenceSerialized
     PaymentData paymentData = PaymentData.builder()
         .person(sampleAuthenticatedPerson)
-        .currency(Currency.EUR)
+        .currency(EUR)
         .amount(BigDecimal.TEN)
-        .bank(Bank.LHV)
+        .bank(LHV)
         .build()
 
     1 * paymentInternalReferenceService.getPaymentReference(sampleAuthenticatedPerson as Person) >> internalReference
     1 * episService.getContactDetails(sampleAuthenticatedPerson) >> contactDetailsFixture()
     when:
-    String paymentLink = paymentLinkService.getPaymentUrl(paymentData)
+    PaymentLink paymentLink = paymentLinkService.getPaymentLink(paymentData)
 
     then:
-    paymentLink.url() == "https://sandbox-payments.montonio.com?payment_token=eyJhbGciOiJIUzI1NiJ9.eyJtZXJjaGFudF9yZXR1cm5fdXJsIjoiaHR0cHM6Ly9vbmJvYXJkaW5nLXNlcnZpY2UudHVsZXZhLmVlL3YxL3BheW1lbnRzL3N1Y2Nlc3MiLCJhbW91bnQiOjEwLCJwYXltZW50X2luZm9ybWF0aW9uX3Vuc3RydWN0dXJlZCI6IjMwMTAxMTE5ODI4IiwiY2hlY2tvdXRfZmlyc3RfbmFtZSI6IkpvcmRhbiIsIm1lcmNoYW50X25vdGlmaWNhdGlvbl91cmwiOiJodHRwczovL29uYm9hcmRpbmctc2VydmljZS50dWxldmEuZWUvdjEvcGF5bWVudHMvbm90aWZpY2F0aW9uIiwicHJlc2VsZWN0ZWRfYXNwc3AiOiJleGFtcGxlQXNwc3AiLCJtZXJjaGFudF9yZWZlcmVuY2UiOiJ7XCJwZXJzb25hbENvZGVcIjogXCIxMjM0NDM0MzRcIiwgXCJ1dWlkXCI6IFwiMjMzMlwifSIsImFjY2Vzc19rZXkiOiJleGFtcGxlQWNjZXNzS2V5IiwicGF5bWVudF9pbmZvcm1hdGlvbl9zdHJ1Y3R1cmVkIjoiOTkzNDMyNDMyIiwiY3VycmVuY3kiOiJFVVIiLCJleHAiOjE2MDYxMjYyMDAsInByZXNlbGVjdGVkX2xvY2FsZSI6ImV0IiwiY2hlY2tvdXRfbGFzdF9uYW1lIjoiVmFsZG1hIn0.O5zhG_x5Fb6a8jFFaLmPi6bCyH1b9wk5P3EOn08r3Tk"
-    paymentLink == "https://sandbox-payments.montonio.com?payment_token=" + aSerializedToken
+    paymentLink.url() == "https://sandbox-payments.montonio.com?payment_token=" + aSerializedToken
   }
 
   AuthenticatedPerson sampleAuthenticatedPerson = AuthenticatedPerson.builder()

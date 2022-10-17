909d3f50094f83964d141c0629c6e712a7f1b82d
Maido Käära <maido@producement.com>
2022-10-17 14:26:56 +0300

    Don't load cacheable values multiple times

--- src/test/groovy/ee/tuleva/onboarding/epis/EpisServiceIntSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/epis/EpisServiceIntSpec.groovy
@@ -9,11 +9,13 @@ import ee.tuleva.onboarding.epis.mandate.MandateDto
 import org.mockserver.client.MockServerClient
 import org.mockserver.model.MediaType
 import org.mockserver.springtest.MockServerTest
+import org.mockserver.verify.VerificationTimes
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes
 import org.springframework.boot.web.servlet.error.ErrorAttributes
+import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.security.authentication.TestingAuthenticationToken
@@ -24,6 +26,10 @@ import org.springframework.test.context.TestPropertySource
 import spock.lang.Specification
 
 import java.time.Instant
+import java.time.LocalDate
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 import static ee.tuleva.onboarding.mandate.application.ApplicationType.TRANSFER
 import static ee.tuleva.onboarding.user.address.AddressFixture.addressFixture
@@ -49,9 +55,16 @@ class EpisServiceIntSpec extends Specification {
   @Autowired
   private EpisService episService
 
+  @Autowired
+  private CacheManager cacheManager
+
   private MockServerClient mockServerClient
 
   def setup() {
+    setUpSecurityContext()
+  }
+
+  private void setUpSecurityContext() {
     SecurityContext sc = SecurityContextHolder.createEmptyContext()
     TestingAuthenticationToken authentication = new TestingAuthenticationToken("test", "password")
     OAuth2AuthenticationDetails details = Mock(OAuth2AuthenticationDetails)
@@ -63,16 +76,17 @@ class EpisServiceIntSpec extends Specification {
 
   def cleanup() {
     SecurityContextHolder.clearContext()
+    cacheManager.cacheNames.stream().forEach(cache -> cacheManager.getCache(cache).clear())
   }
 
   def "getApplications"() {
     given:
     mockServerClient
-      .when(request("/applications")
-        .withHeader("Authorization", "Bearer dummy"))
-      .respond(response()
-        .withContentType(MediaType.APPLICATION_JSON)
-        .withBody("""
+        .when(request("/applications")
+            .withHeader("Authorization", "Bearer dummy"))
+        .respond(response()
+            .withContentType(MediaType.APPLICATION_JSON)
+            .withBody("""
                         [{
                          "currency": "EUR",
                          "amount": 100.0,
@@ -92,6 +106,75 @@ class EpisServiceIntSpec extends Specification {
     applications.first().documentNumber == "123456"
   }
 
+  def "getApplications - only one request per person is allowed to run at any time"() {
+    given:
+    mockServerClient
+        .when(request("/applications")
+            .withHeader("Authorization", "Bearer dummy"))
+        .respond(response()
+            .withDelay(TimeUnit.MILLISECONDS, 500)
+            .withContentType(MediaType.APPLICATION_JSON)
+            .withBody("""
+                        [{
+                         "currency": "EUR",
+                         "amount": 100.0,
+                         "status": "PENDING",
+                         "type": "TRANSFER",
+                         "id": 123,
+                         "documentNumber": "123456"
+                        }]
+                        """))
+    when:
+    ExecutorService executor = Executors.newFixedThreadPool(2)
+    with(executor) {
+      submit({
+        setUpSecurityContext()
+        episService.getApplications(PersonFixture.samplePerson())
+        SecurityContextHolder.clearContext()
+      })
+      submit({
+        setUpSecurityContext()
+        episService.getApplications(PersonFixture.samplePerson())
+        SecurityContextHolder.clearContext()
+      })
+      shutdown()
+      awaitTermination(5, TimeUnit.SECONDS)
+    }
+    then:
+    mockServerClient.verify(request().withPath("/applications"), VerificationTimes.exactly(1))
+  }
+
+  def "getCashFlowStatement - only one request per person is allowed to run at any time"() {
+    given:
+    mockServerClient
+        .when(request("/account-cash-flow-statement")
+            .withHeader("Authorization", "Bearer dummy"))
+        .respond(response()
+            .withDelay(TimeUnit.MILLISECONDS, 500)
+            .withContentType(MediaType.APPLICATION_JSON)
+            .withBody("""
+                        {"startBalance": {}, "endBalance": {}, "transactions": []}
+                        """))
+    when:
+    ExecutorService executor = Executors.newFixedThreadPool(2)
+    with(executor) {
+      submit({
+        setUpSecurityContext()
+        episService.getCashFlowStatement(PersonFixture.samplePerson(), LocalDate.EPOCH, LocalDate.EPOCH)
+        SecurityContextHolder.clearContext()
+      })
+      submit({
+        setUpSecurityContext()
+        episService.getCashFlowStatement(PersonFixture.samplePerson(), LocalDate.EPOCH, LocalDate.EPOCH)
+        SecurityContextHolder.clearContext()
+      })
+      shutdown()
+      awaitTermination(5, TimeUnit.SECONDS)
+    }
+    then:
+    mockServerClient.verify(request().withPath("/account-cash-flow-statement"), VerificationTimes.exactly(1))
+  }
+
   def "clear cache does not fail"() {
     when:
     episService.clearCache(PersonFixture.samplePerson())
@@ -105,29 +188,29 @@ class EpisServiceIntSpec extends Specification {
         new MandateDto.MandateFundsTransferExchangeDTO("transferProcessId", 1.0, "EE123", "EE234", null)
     def address = addressFixture().build()
     def mandate = MandateDto.builder()
-      .id(111L)
-      .fundTransferExchanges([fundsTransferExchange])
-      .processId("selectionProcessId")
-      .createdDate(Instant.parse("2021-03-10T10:00:00Z"))
-      .futureContributionFundIsin("EE345")
-      .pillar(2)
-      .address(address)
-      .build()
+        .id(111L)
+        .fundTransferExchanges([fundsTransferExchange])
+        .processId("selectionProcessId")
+        .createdDate(Instant.parse("2021-03-10T10:00:00Z"))
+        .futureContributionFundIsin("EE345")
+        .pillar(2)
+        .address(address)
+        .build()
 
     ApplicationResponseDTO expectedResponse = new ApplicationResponseDTO(
-      ApplicationResponse.builder()
-        .processId("responseProcessId")
-        .applicationType(TRANSFER)
-        .successful(true)
-        .build()
+        ApplicationResponse.builder()
+            .processId("responseProcessId")
+            .applicationType(TRANSFER)
+            .successful(true)
+            .build()
     )
 
     mockServerClient
-      .when(
-        request()
-          .withMethod("POST")
-          .withPath("/mandates")
-          .withBody(json("""
+        .when(
+            request()
+                .withMethod("POST")
+                .withPath("/mandates")
+                .withBody(json("""
             {
               "id": ${mandate.id},
               "processId": "${mandate.processId}",
@@ -150,12 +233,12 @@ class EpisServiceIntSpec extends Specification {
               }
             }
           """))
-      )
-      .respond(
-        response()
-          .withContentType(MediaType.APPLICATION_JSON)
-          .withBody(json(expectedResponse))
-      )
+        )
+        .respond(
+            response()
+                .withContentType(MediaType.APPLICATION_JSON)
+                .withBody(json(expectedResponse))
+        )
 
     when:
     ApplicationResponseDTO response = episService.sendMandate(mandate)

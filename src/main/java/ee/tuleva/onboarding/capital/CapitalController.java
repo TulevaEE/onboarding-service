f596d1bcf7c24899c933b9f018029b3818b086ce
Maido Käära <maido@producement.com>
2022-11-17 11:14:42 +0200

    Create metaannotation for authentication principal

--- src/main/java/ee/tuleva/onboarding/capital/CapitalController.java
+++ src/main/java/ee/tuleva/onboarding/capital/CapitalController.java
@@ -1,11 +1,11 @@
 package ee.tuleva.onboarding.capital;
 
+import ee.tuleva.onboarding.auth.AuthenticatedPersonPrincipal;
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson;
 import ee.tuleva.onboarding.user.User;
 import ee.tuleva.onboarding.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +22,7 @@ public class CapitalController {
   @Operation(summary = "Get info about current user initial capital")
   @GetMapping(CAPITAL_URI)
   public CapitalStatement capitalStatement(
-      @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson) {
+      @AuthenticatedPersonPrincipal AuthenticatedPerson authenticatedPerson) {
     Long userId = authenticatedPerson.getUserId();
     User user = userService.getById(userId);
     return user.getMember()
--- src/main/java/ee/tuleva/onboarding/mandate/application/ApplicationController.java
+++ src/main/java/ee/tuleva/onboarding/mandate/application/ApplicationController.java
@@ -2,13 +2,13 @@ package ee.tuleva.onboarding.mandate.application;
 
 import static ee.tuleva.onboarding.mandate.application.ApplicationController.APPLICATIONS_URI;
 
+import ee.tuleva.onboarding.auth.AuthenticatedPersonPrincipal;
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson;
 import ee.tuleva.onboarding.epis.mandate.ApplicationStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,14 +30,15 @@ class ApplicationController {
   @Operation(summary = "Get application")
   @GetMapping("/{id}")
   public Application<?> getApplication(
-      @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson, @PathVariable Long id) {
+      @AuthenticatedPersonPrincipal AuthenticatedPerson authenticatedPerson,
+      @PathVariable Long id) {
     return applicationService.getApplication(id, authenticatedPerson);
   }
 
   @Operation(summary = "Get applications")
   @GetMapping
   public List<Application<?>> getApplications(
-      @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson,
+      @AuthenticatedPersonPrincipal AuthenticatedPerson authenticatedPerson,
       @RequestParam("status") ApplicationStatus status) {
     return applicationService.getApplications(status, authenticatedPerson);
   }
@@ -45,7 +46,7 @@ class ApplicationController {
   @Operation(summary = "Cancel an application")
   @PostMapping("/{id}/cancellations")
   public ApplicationCancellationResponse cancel(
-      @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson,
+      @AuthenticatedPersonPrincipal AuthenticatedPerson authenticatedPerson,
       @PathVariable("id") Long applicationId) {
     log.info("Cancelling application {}", applicationId);
     return applicationCancellationService.createCancellationMandate(

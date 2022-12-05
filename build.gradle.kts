a18b25e621c8e9fe05e9464d5333f7beb217a80c
Erko Risthein <erko@producement.com>
2022-12-05 20:46:20 +0800

    Upgrade dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jsoup:jsoup:1.15.3")
-    implementation("commons-net:commons-net:3.8.0")
+    implementation("commons-net:commons-net:3.9.0")
     implementation("org.apache.commons:commons-lang3")
     implementation("net.sf.ehcache:ehcache:2.10.9.2")
     implementation("org.decampo:xirr:1.2")

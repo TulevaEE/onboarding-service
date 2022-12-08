87fadff388ff8202c07a3c16f7c7115a246f648a
Erko Risthein <erko@producement.com>
2022-12-08 23:22:50 +0800

    Update fixture

--- src/test/groovy/ee/tuleva/onboarding/conversion/ConversionResponseFixture.groovy
+++ src/test/groovy/ee/tuleva/onboarding/conversion/ConversionResponseFixture.groovy
@@ -2,30 +2,38 @@ package ee.tuleva.onboarding.conversion
 
 class ConversionResponseFixture {
 
-    static ConversionResponse notFullyConverted() {
-        return ConversionResponse.builder()
-            .secondPillar(ConversionResponse.Conversion.builder()
-                .transfersComplete(true)
-                .selectionComplete(false)
-                .build()
-            ).thirdPillar(ConversionResponse.Conversion.builder()
+  static ConversionResponse notFullyConverted() {
+    return ConversionResponse.builder()
+        .secondPillar(ConversionResponse.Conversion.builder()
             .transfersComplete(true)
+            .transfersPartial(true)
             .selectionComplete(false)
+            .selectionPartial(false)
             .build()
-        ).build()
-    }
+        ).thirdPillar(ConversionResponse.Conversion.builder()
+        .transfersComplete(true)
+        .transfersPartial(true)
+        .selectionComplete(false)
+        .selectionPartial(false)
+        .build()
+    ).build()
+  }
 
-    static ConversionResponse fullyConverted() {
-        return ConversionResponse.builder()
-            .secondPillar(ConversionResponse.Conversion.builder()
-                .transfersComplete(true)
-                .selectionComplete(true)
-                .build()
-            ).thirdPillar(ConversionResponse.Conversion.builder()
+  static ConversionResponse fullyConverted() {
+    return ConversionResponse.builder()
+        .secondPillar(ConversionResponse.Conversion.builder()
             .transfersComplete(true)
+            .transfersPartial(true)
             .selectionComplete(true)
+            .selectionPartial(true)
             .build()
-        ).build()
-    }
+        ).thirdPillar(ConversionResponse.Conversion.builder()
+        .transfersComplete(true)
+        .transfersPartial(true)
+        .selectionComplete(true)
+        .selectionPartial(true)
+        .build()
+    ).build()
+  }
 
 }

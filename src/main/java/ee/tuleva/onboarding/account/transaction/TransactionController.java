f51ebcb1141c0d6c7d6906a3ac8753eb46defc6d
Erko Risthein <erko@producement.com>
2023-01-24 22:17:58 +0100

    Fix missing annotation

--- src/main/java/ee/tuleva/onboarding/account/transaction/TransactionController.java
+++ src/main/java/ee/tuleva/onboarding/account/transaction/TransactionController.java
@@ -6,6 +6,7 @@ import ee.tuleva.onboarding.epis.cashflows.CashFlowStatement;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,8 @@ public class TransactionController {
 
   @GetMapping("/transactions")
   @Operation(summary = "Get a list of transactions")
-  public List<Transaction> getTransactions(AuthenticatedPerson authenticatedPerson) {
+  public List<Transaction> getTransactions(
+      @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson) {
     CashFlowStatement cashFlowStatement = cashFlowService.getCashFlowStatement(authenticatedPerson);
     return cashFlowStatement.getTransactions().stream()
         .filter(cashFlow -> cashFlow.isContribution() || cashFlow.isSubtraction())

a33bf5a00962fc36a8d676c85a87ad5c6f5d50b9
Erko Risthein <erko@producement.com>
2023-01-26 11:41:35 +0100

    Fix build

--- src/main/java/ee/tuleva/onboarding/package-info.java
+++ src/main/java/ee/tuleva/onboarding/package-info.java
@@ -1,5 +1,5 @@
 @TypeDef(name = "jsonb", typeClass = JsonType.class)
 package ee.tuleva.onboarding;
 
-import io.hypersistence.utils.hibernate.type.json.JsonType;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import org.hibernate.annotations.TypeDef;

c589ce89de439508b0c892d4857dfa2431366c2f
Erko Risthein <erko@producement.com>
2023-04-27 15:10:04 +0300

    Spotless fix

--- src/main/java/ee/tuleva/onboarding/contribution/ContributionController.java
+++ src/main/java/ee/tuleva/onboarding/contribution/ContributionController.java
@@ -3,14 +3,13 @@ package ee.tuleva.onboarding.contribution;
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson;
 import ee.tuleva.onboarding.epis.EpisService;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -22,8 +21,7 @@ class ContributionController {
 
   @Operation(summary = "Get Contributions")
   @GetMapping("/contributions")
-  public List<Contribution> getContributions(
-      @AuthenticationPrincipal AuthenticatedPerson person) {
+  public List<Contribution> getContributions(@AuthenticationPrincipal AuthenticatedPerson person) {
     return episService.getContributions(person);
   }
 }

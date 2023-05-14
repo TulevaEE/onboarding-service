9e817e54c00b1460e44909b3276bee060bab705e
gnortsmralient <deskpro_belchertown@simplelogin.com>
2023-05-14 18:09:45 +0300

    WIP Calculate pending exchanges
    PIK transfers not implemented

--- src/main/java/ee/tuleva/onboarding/conversion/WeightedAverageFeeCalculator.java
+++ src/main/java/ee/tuleva/onboarding/conversion/WeightedAverageFeeCalculator.java
@@ -1,6 +1,7 @@
 package ee.tuleva.onboarding.conversion;
 
 import ee.tuleva.onboarding.account.FundBalance;
+import ee.tuleva.onboarding.mandate.application.Exchange;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
@@ -11,7 +12,8 @@ public class WeightedAverageFeeCalculator {
     return funds.stream().map(FundBalance::getTotalValue).reduce(BigDecimal.ZERO, BigDecimal::add);
   }
 
-  public BigDecimal getWeightedAverageFee(List<FundBalance> funds) {
+  public BigDecimal getWeightedAverageFee(
+      List<FundBalance> funds, List<Exchange> pendingExchanges) {
 
     if (funds.size() == 0) {
       return BigDecimal.ZERO;
@@ -19,20 +21,57 @@ public class WeightedAverageFeeCalculator {
 
     BigDecimal valueSum = getValueSum(funds);
     if (valueSum.compareTo(BigDecimal.ZERO) == 0) {
-      return funds.stream()
-          .map(fund -> fund.getFund().getOngoingChargesFigure())
-          .reduce(BigDecimal.ZERO, BigDecimal::add)
-          .divide(BigDecimal.valueOf(funds.size()), RoundingMode.HALF_UP);
+      var fromFundIsins =
+          pendingExchanges.stream().map(exchange -> exchange.getSourceFund().getIsin()).toList();
+      var fundsThatDoNotIncludeLeavingFunds =
+          funds.stream()
+              .filter(fundBalance -> !fromFundIsins.contains(fundBalance.getFund().getIsin()))
+              .toList();
+
+      var weightedValue =
+          fundsThatDoNotIncludeLeavingFunds.stream()
+              .map(fund -> fund.getFund().getOngoingChargesFigure())
+              .reduce(BigDecimal.ZERO, BigDecimal::add)
+              .divide(BigDecimal.valueOf(funds.size()), RoundingMode.HALF_UP);
+
+      var toFundWeightedValue =
+          pendingExchanges.stream()
+              .map(exchange -> exchange.getTargetFund().getOngoingChargesFigure())
+              .reduce(BigDecimal.ZERO, BigDecimal::add)
+              .divide(BigDecimal.valueOf(funds.size()), RoundingMode.HALF_UP);
+
+      return weightedValue.add(toFundWeightedValue);
     }
 
     BigDecimal weightedArithmeticMean = BigDecimal.ZERO;
     for (FundBalance fund : funds) {
       BigDecimal fundTotalValue = fund.getTotalValue();
+      var exchangesFromCurrentFund =
+          pendingExchanges.stream()
+              .filter(exchange -> exchange.getSourceFund().getIsin().equals(fund.getIsin()))
+              .toList();
+
+      var amountThatLeavesThisFund =
+          exchangesFromCurrentFund.stream()
+              .map(Exchange::getAmount)
+              .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+      var weightedValueOfExchanges =
+          exchangesFromCurrentFund.stream()
+              .map(
+                  exchange ->
+                      exchange
+                          .getAmount()
+                          .multiply(exchange.getTargetFund().getOngoingChargesFigure())
+                          .divide(valueSum, 4, RoundingMode.HALF_UP))
+              .reduce(BigDecimal.ZERO, BigDecimal::add);
+
       BigDecimal weightedValue =
-          fundTotalValue
+          (fundTotalValue.subtract(amountThatLeavesThisFund))
               .multiply(fund.getFund().getOngoingChargesFigure())
               .divide(valueSum, 4, RoundingMode.HALF_UP);
-      weightedArithmeticMean = weightedArithmeticMean.add(weightedValue);
+      weightedArithmeticMean =
+          weightedArithmeticMean.add(weightedValue).add(weightedValueOfExchanges);
     }
     return weightedArithmeticMean;
   }

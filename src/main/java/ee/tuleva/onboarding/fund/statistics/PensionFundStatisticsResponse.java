b955c295d1840a5adba62c062ce45a39bd74fc4f
Erko Risthein <erko@producement.com>
2023-07-13 18:27:22 +0300

    Spotless fix

--- src/main/java/ee/tuleva/onboarding/fund/statistics/PensionFundStatisticsResponse.java
+++ src/main/java/ee/tuleva/onboarding/fund/statistics/PensionFundStatisticsResponse.java
@@ -1,7 +1,6 @@
 package ee.tuleva.onboarding.fund.statistics;
 
 import java.util.List;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;

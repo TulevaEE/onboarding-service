e8ef4c256c0b28c0a8763e945ada4a52af5729c8
Erko Risthein <erko@producement.com>
2023-07-13 18:20:28 +0300

    Fix FundNavRetriever test

--- src/test/groovy/ee/tuleva/onboarding/comparisons/fundvalue/retrieval/FundNavRetrieverSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/comparisons/fundvalue/retrieval/FundNavRetrieverSpec.groovy
@@ -3,10 +3,11 @@ package ee.tuleva.onboarding.comparisons.fundvalue.retrieval
 import ee.tuleva.onboarding.comparisons.fundvalue.FundValue
 import ee.tuleva.onboarding.epis.EpisService
 import ee.tuleva.onboarding.epis.fund.NavDto
-import ee.tuleva.onboarding.error.exception.ErrorsResponseException
-import ee.tuleva.onboarding.error.response.ErrorsResponse
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.HttpClientErrorException
 import spock.lang.Specification
 
+import static java.nio.charset.StandardCharsets.UTF_8
 import static java.time.LocalDate.parse
 
 class FundNavRetrieverSpec extends Specification {
@@ -28,7 +29,9 @@ class FundNavRetrieverSpec extends Specification {
         def startDate = parse("2019-08-19")
         def endDate = parse("2019-08-22")
         episService.getNav(isin, parse("2019-08-19")) >> new NavDto(isin, parse("2019-08-19"), 19.0)
-        episService.getNav(isin, parse("2019-08-20")) >> { throw new ErrorsResponseException(new ErrorsResponse()) }
+        episService.getNav(isin, parse("2019-08-20")) >> {
+          throw HttpClientErrorException.create(HttpStatus.NOT_FOUND, "message", null, new byte[0], UTF_8)
+        }
         episService.getNav(isin, parse("2019-08-21")) >> new NavDto(isin, parse("2019-08-21"), 21.0)
         episService.getNav(isin, parse("2019-08-22")) >> new NavDto(isin, parse("2019-08-22"), 22.0)
         when:

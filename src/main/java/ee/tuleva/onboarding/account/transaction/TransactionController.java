6e7158c619ea5c3503073f8e84b08862719876a6
Erko Risthein <erko@producement.com>
2023-07-13 18:21:07 +0300

    Spotless fix

--- src/main/java/ee/tuleva/onboarding/account/transaction/TransactionController.java
+++ src/main/java/ee/tuleva/onboarding/account/transaction/TransactionController.java
@@ -7,11 +7,8 @@ import ee.tuleva.onboarding.auth.AuthenticatedPersonPrincipal;
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson;
 import ee.tuleva.onboarding.epis.cashflows.CashFlowStatement;
 import io.swagger.v3.oas.annotations.Operation;
-
 import java.util.List;
-
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

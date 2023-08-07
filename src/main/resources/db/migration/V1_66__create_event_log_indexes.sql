5a0ad53247f5b5d285cb02dfacfd289c85354564
Erko Risthein <erko@producement.com>
2023-08-07 19:14:12 +0300

    Create event_log indexes

---
+++ src/main/resources/db/migration/V1_66__create_event_log_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX event_log_type_index ON event_log (type);
+CREATE INDEX event_log_principal_index ON event_log (principal);
+CREATE INDEX event_log_timestamp_index ON event_log (timestamp);

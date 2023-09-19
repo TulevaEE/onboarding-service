6b06dd176dd25edc2af81ce5311b6fae579985d2
gnortsmralient <deskpro_belchertown@simplelogin.com>
2023-09-19 13:28:29 +0300

    Keep old data in payment type migration

--- src/main/resources/db/migration/V1_68__payment_type.sql
+++ src/main/resources/db/migration/V1_68__payment_type.sql
@@ -1,2 +1,9 @@
-DELETE FROM payment;
-ALTER TABLE payment ADD COLUMN payment_type VARCHAR(255) NOT NULL;
+ALTER TABLE payment
+  ADD COLUMN payment_type VARCHAR(255);
+
+UPDATE payment
+  SET payment_type = 'SINGLE';
+
+ALTER TABLE payment
+  ALTER COLUMN payment_type SET NOT NULL;
+

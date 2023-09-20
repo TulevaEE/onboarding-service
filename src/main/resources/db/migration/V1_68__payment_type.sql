113c9a792fc7f291db86b8534232a6f6a0763703
gnortsmralient <deskpro_belchertown@simplelogin.com>
2023-09-20 14:05:30 +0300

    Update payment types to "SINGLE", unless it's a GIFT

--- src/main/resources/db/migration/V1_68__payment_type.sql
+++ src/main/resources/db/migration/V1_68__payment_type.sql
@@ -1,9 +1,22 @@
 ALTER TABLE payment
-  ADD COLUMN payment_type VARCHAR(255);
+  ADD COLUMN payment_type VARCHAR(255) DEFAULT 'SINGLE';
 
 UPDATE payment
-  SET payment_type = 'SINGLE';
+SET payment_type = (
+  SELECT CASE
+           WHEN u.personal_code != payment.recipient_personal_code THEN 'GIFT'
+           ELSE 'SINGLE'
+           END
+  FROM users AS u
+  WHERE payment.user_id = u.id
+);
+
+UPDATE payment
+SET payment_type = 'SINGLE'
+WHERE payment_type IS NULL;
 
 ALTER TABLE payment
   ALTER COLUMN payment_type SET NOT NULL;
 
+ALTER TABLE payment
+  ALTER COLUMN payment_type DROP DEFAULT;

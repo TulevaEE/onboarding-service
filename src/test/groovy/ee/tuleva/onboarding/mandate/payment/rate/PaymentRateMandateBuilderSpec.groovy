9da7361becdc965ed3445448fc1cd6e155c2328c
Erko Risthein <erko@risthein.ee>
2023-12-21 10:50:54 +0200

    Fix some tests

--- src/test/groovy/ee/tuleva/onboarding/mandate/payment/rate/PaymentRateMandateBuilderSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/payment/rate/PaymentRateMandateBuilderSpec.groovy
@@ -3,6 +3,8 @@ package ee.tuleva.onboarding.mandate.payment.rate
 import ee.tuleva.onboarding.mandate.Mandate
 import ee.tuleva.onboarding.mandate.builder.ConversionDecorator
 import spock.lang.Specification
+
+import static ee.tuleva.onboarding.auth.AuthenticatedPersonFixture.sampleAuthenticatedPersonAndMember
 import static ee.tuleva.onboarding.auth.UserFixture.sampleUser
 import static ee.tuleva.onboarding.conversion.ConversionResponseFixture.fullyConverted
 import static ee.tuleva.onboarding.epis.contact.ContactDetailsFixture.contactDetailsFixture
@@ -15,17 +17,18 @@ class PaymentRateMandateBuilderSpec extends Specification {
   def "build creates mandate with given payment rate"() {
     given:
     BigDecimal paymentRate = new BigDecimal("2.0")
+    def authenticatedPerson = sampleAuthenticatedPersonAndMember().build()
     def user = sampleUser().build()
     def conversion = fullyConverted()
     def contactDetails = contactDetailsFixture()
 
     when:
-    Mandate mandate = paymentRateMandateBuilder.build(paymentRate, user, conversion, contactDetails)
+    Mandate mandate = paymentRateMandateBuilder.build(paymentRate, authenticatedPerson, user, conversion, contactDetails)
 
     then:
     mandate.paymentRate == paymentRate
     mandate.user == user
     mandate.address == contactDetails.address
-    1 * conversionDecorator.addConversionMetadata(_, conversion, contactDetails)
+    1 * conversionDecorator.addConversionMetadata(_, conversion, contactDetails, authenticatedPerson)
   }
 }

b170fbd3d77608ca4d3ab8b59dd8d932798dc4bf
Erko Risthein <erko@risthein.ee>
2023-12-28 13:04:30 +0200

    Fix rebase issues

--- src/main/java/ee/tuleva/onboarding/mandate/builder/CreateMandateCommandToMandateConverter.java
+++ src/main/java/ee/tuleva/onboarding/mandate/builder/CreateMandateCommandToMandateConverter.java
@@ -45,7 +45,6 @@ public class CreateMandateCommandToMandateConverter
     mandate.setUser(user);
     mandate.setPillar(getPillar(createMandateCommand));
     mandate.setAddress(createMandateCommand.getAddress());
-    mandate.setPaymentRate(createMandateCommand.getPaymentRate());
     conversionDecorator.addConversionMetadata(
         mandate.getMetadata(), conversion, contactDetails, authenticatedPerson);
 

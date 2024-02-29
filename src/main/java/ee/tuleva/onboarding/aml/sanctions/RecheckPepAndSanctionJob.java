515746473ba2463a66283505f9ba7debd4c412ba
Erko Risthein <erko@risthein.ee>
2024-02-29 15:16:43 +0200

    Fix bug

--- src/main/java/ee/tuleva/onboarding/aml/sanctions/RecheckPepAndSanctionJob.java
+++ src/main/java/ee/tuleva/onboarding/aml/sanctions/RecheckPepAndSanctionJob.java
@@ -2,17 +2,19 @@ package ee.tuleva.onboarding.aml.sanctions;
 
 import ee.tuleva.onboarding.aml.AmlService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Profile("!dev")
 public class RecheckPepAndSanctionJob {
 
-  AmlService amlService;
+  private final AmlService amlService;
 
-  // once per week on Thursday at 15:15
-  @Scheduled(cron = "0 15 15 * * THU", zone = "Europe/Tallinn")
+  // once per week on Thursday at 15:45
+  @Scheduled(cron = "0 30 15 * * THU", zone = "Europe/Tallinn")
   public void run() {
     amlService.recheckAllPepAndSanctionChecks();
   }

ec50e76d5329d79351c3120328738260fdc5121a
Erko Risthein <erko@risthein.ee>
2024-04-17 10:00:23 +0300

    Disable security config debug

--- src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/config/SecurityConfiguration.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfiguration {
 
   @Bean

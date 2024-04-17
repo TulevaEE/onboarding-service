ed577bf9c2909e6db4ab8b9f50c09b1f9111ad5a
Erko Risthein <erko@risthein.ee>
2024-04-17 08:45:32 +0300

    Disable DigiDocTslRefreshJob in tests

--- src/main/java/ee/tuleva/onboarding/config/DigiDocTslRefreshJob.java
+++ src/main/java/ee/tuleva/onboarding/config/DigiDocTslRefreshJob.java
@@ -3,12 +3,14 @@ package ee.tuleva.onboarding.config;
 import lombok.RequiredArgsConstructor;
 import org.digidoc4j.Configuration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Profile("!test")
 public class DigiDocTslRefreshJob {
 
   private final Configuration digiDocConfiguration;

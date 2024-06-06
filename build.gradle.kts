6ff387e791338a835946dc4a0ae1c8b87f220371
Erko Risthein <erko@risthein.ee>
2024-06-06 13:09:20 +0300

    Spotless fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STARTED
 
 buildscript {
     repositories {

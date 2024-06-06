5c82f923c2f8241f7399e257cd8301646e4ccdde
Erko Risthein <erko@risthein.ee>
2024-06-06 13:08:06 +0300

    Hotfix flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,6 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
-    implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")

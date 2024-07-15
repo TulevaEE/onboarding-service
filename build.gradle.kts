56abef70220825eb64a459dba4b6d1aabb0b479d
Erko Risthein <erko@risthein.ee>
2024-07-15 11:13:58 +0300

    Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -195,3 +195,11 @@ tasks.withType<JavaCompile> {
     options.compilerArgs.add("-parameters")
     options.compilerArgs.add("--enable-preview")
 }
+
+tasks.withType<JavaExec> {
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<Test> {
+    jvmArgs("--enable-preview")
+}

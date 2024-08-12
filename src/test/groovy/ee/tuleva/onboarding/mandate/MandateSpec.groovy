e4e1d409c39c61227b1818f263b40f495069d0ad
Sander Nemvalts <snemvalts@gmail.com>
2024-08-12 10:40:34 +0300

    Add more tests, improve coverage

--- src/test/groovy/ee/tuleva/onboarding/mandate/MandateSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/mandate/MandateSpec.groovy
@@ -1,16 +1,16 @@
 package ee.tuleva.onboarding.mandate
 
-import ee.tuleva.onboarding.mandate.application.ApplicationType
+import ee.tuleva.onboarding.epis.mandate.GenericMandateDto
+import ee.tuleva.onboarding.epis.mandate.details.EarlyWithdrawalCancellationMandateDetails
+import ee.tuleva.onboarding.epis.mandate.details.TransferCancellationMandateDetails
+import ee.tuleva.onboarding.epis.mandate.details.WithdrawalCancellationMandateDetails
+import jakarta.validation.ConstraintViolation
+import jakarta.validation.Validation
+import jakarta.validation.Validator
 import spock.lang.Specification
 import spock.lang.Unroll
 
-import jakarta.validation.ConstraintViolation
-
-import static ee.tuleva.onboarding.mandate.application.ApplicationType.CANCELLATION
-import static ee.tuleva.onboarding.mandate.application.ApplicationType.EARLY_WITHDRAWAL
-import static ee.tuleva.onboarding.mandate.application.ApplicationType.WITHDRAWAL
-import jakarta.validation.Validation
-import jakarta.validation.Validator
+import static ee.tuleva.onboarding.mandate.MandateFixture.*
 
 class MandateSpec extends Specification {
 
@@ -59,6 +59,52 @@ class MandateSpec extends Specification {
     "4.0"   | true
     "6.0"   | true
     null    | true
+  }
+
+
+  def "can build generic mandate dto for withdrawal cancellation"() {
+    when:
+    Mandate mandate = sampleWithdrawalCancellationMandate()
+
+    GenericMandateDto dto = mandate.getGenericMandateDto()
+    then:
+    dto.id == mandate.id
+    dto.createdDate == mandate.createdDate
+    dto.address == mandate.address
+    dto.email == mandate.email
+    dto.phoneNumber == mandate.phoneNumber
+    dto.details instanceof WithdrawalCancellationMandateDetails
+  }
+
+
+  def "can build generic mandate dto for early withdrawal cancellation"() {
+    when:
+    Mandate mandate = sampleEarlyWithdrawalCancellationMandate()
+
+    GenericMandateDto dto = mandate.getGenericMandateDto()
+    then:
+    dto.id == mandate.id
+    dto.createdDate == mandate.createdDate
+    dto.address == mandate.address
+    dto.email == mandate.email
+    dto.phoneNumber == mandate.phoneNumber
+    dto.details instanceof EarlyWithdrawalCancellationMandateDetails
+  }
+
+  def "can build generic mandate dto for transfer cancellation"() {
+    when:
+    Mandate mandate = sampleTransferCancellationMandate()
+
+    GenericMandateDto dto = mandate.getGenericMandateDto()
+    then:
+    dto.id == mandate.id
+    dto.createdDate == mandate.createdDate
+    dto.address == mandate.address
+    dto.email == mandate.email
+    dto.phoneNumber == mandate.phoneNumber
+    dto.details instanceof TransferCancellationMandateDetails
+    ((TransferCancellationMandateDetails) dto.details).getSourceFundIsinOfTransferToCancel() == mandate.fundTransferExchanges.first.sourceFundIsin
+    ((TransferCancellationMandateDetails) dto.details).getPillar() == 2
   }
 
 }

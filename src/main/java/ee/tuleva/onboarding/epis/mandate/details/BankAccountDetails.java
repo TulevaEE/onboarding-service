ffcc285399fd58616e288ef3e8bb038a95f3a4a7
Sander Nemvalts <snemvalts@gmail.com>
2024-09-02 13:17:45 +0300

    Add more required fields to templates, add fund pension file generator

--- src/main/java/ee/tuleva/onboarding/epis/mandate/details/BankAccountDetails.java
+++ src/main/java/ee/tuleva/onboarding/epis/mandate/details/BankAccountDetails.java
@@ -1,6 +1,7 @@
 package ee.tuleva.onboarding.epis.mandate.details;
 
 public record BankAccountDetails(BankAccountType type, String accountIban) {
+  // TODO bank name missing, required in form
   public enum BankAccountType {
     ESTONIAN,
     // EUROPEAN(E) and OTHER(V) not supported
---
+++ src/main/java/ee/tuleva/onboarding/mandate/content/FundPensionOpeningMandateFileCreator.java
@@ -0,0 +1,39 @@
+package ee.tuleva.onboarding.mandate.content;
+
+import static ee.tuleva.onboarding.mandate.MandateType.FUND_PENSION_OPENING;
+
+import ee.tuleva.onboarding.epis.contact.ContactDetails;
+import ee.tuleva.onboarding.mandate.Mandate;
+import ee.tuleva.onboarding.mandate.MandateType;
+import ee.tuleva.onboarding.user.User;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FundPensionOpeningMandateFileCreator implements MandateFileCreator {
+
+  private final MandateContentService mandateContentService;
+
+  @Override
+  public List<MandateContentFile> getContentFiles(
+      User user, Mandate mandate, ContactDetails contactDetails) {
+
+    String htmlContent =
+        mandateContentService.getFundPensionOpeningHtml(user, mandate, contactDetails);
+    String documentNumber = mandate.getId().toString();
+
+    return List.of(
+        MandateContentFile.builder()
+            .name("fondipensioni_avamise_avaldus_" + documentNumber + ".html")
+            .mimeType("text/html")
+            .content(htmlContent.getBytes())
+            .build());
+  }
+
+  @Override
+  public boolean supports(MandateType mandateType) {
+    return mandateType == FUND_PENSION_OPENING;
+  }
+}

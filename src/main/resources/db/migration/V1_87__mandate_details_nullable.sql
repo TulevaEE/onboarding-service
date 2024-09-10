c6f09398f7598a72cefb6f6fec0ef6076738eeb4
Sander Nemvalts <snemvalts@gmail.com>
2024-09-10 12:08:25 +0300

    Update migration to add missing transfer cancellation fields to database

--- src/main/resources/db/migration/V1_87__mandate_details_nullable.sql
+++ src/main/resources/db/migration/V1_87__mandate_details_nullable.sql
@@ -13,5 +13,18 @@ SET mandate_type = 'EARLY_WITHDRAWAL_CANCELLATION', details = '{"mandateType":"E
 WHERE mandate_type = '1';
 
 UPDATE mandate
-SET mandate_type = 'TRANSFER_CANCELLATION', details = '{"mandateType":"TRANSFER_CANCELLATION"}'::jsonb
-WHERE mandate_type = '2';
+SET mandate_type = 'TRANSFER_CANCELLATION',
+    details = jsonb_set(
+        jsonb_set(
+            '{"mandateType":"TRANSFER_CANCELLATION"}'::jsonb,
+            '{pillar}',
+            to_jsonb((CASE WHEN mandate.pillar = 2 THEN 'SECOND' ELSE 'THIRD' END)::text)
+        ),
+        '{sourceFundIsinOfTransferToCancel}',
+        to_jsonb(fund_transfer_exchange.source_fund_isin)
+    )
+  FROM fund_transfer_exchange
+WHERE mandate_type = '2'
+  AND fund_transfer_exchange.mandate_id = mandate.id
+  AND fund_transfer_exchange.target_fund_isin IS NULL
+  AND fund_transfer_exchange.amount IS NULL;

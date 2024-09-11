b2e69d28be8987ac0a0e251012944b9e91647faa
Sander Nemvalts <snemvalts@gmail.com>
2024-09-11 12:15:14 +0300

    Change migration, improve README

--- src/main/resources/db/migration/V1_87__mandate_details_nullable.sql
+++ src/main/resources/db/migration/V1_87__mandate_details_nullable.sql
@@ -2,19 +2,22 @@ ALTER TABLE mandate ALTER COLUMN details DROP NOT NULL;
 
 UPDATE mandate
 SET details = null
+WHERE mandate_type = null;
+
+UPDATE mandate
+SET details = null
 WHERE details = '{}' AND mandate_type = 'UNKNOWN';
 
 UPDATE mandate
-SET mandate_type = 'WITHDRAWAL_CANCELLATION', details = '{"mandateType":"WITHDRAWAL_CANCELLATION"}'::jsonb
-WHERE mandate_type = '0';
+SET details = '{"mandateType":"WITHDRAWAL_CANCELLATION"}'::jsonb
+WHERE mandate_type = 'WITHDRAWAL_CANCELLATION';
 
 UPDATE mandate
-SET mandate_type = 'EARLY_WITHDRAWAL_CANCELLATION', details = '{"mandateType":"EARLY_WITHDRAWAL_CANCELLATION"}'::jsonb
-WHERE mandate_type = '1';
+SET details = '{"mandateType":"EARLY_WITHDRAWAL_CANCELLATION"}'::jsonb
+WHERE mandate_type = 'EARLY_WITHDRAWAL_CANCELLATION';
 
 UPDATE mandate
-SET mandate_type = 'TRANSFER_CANCELLATION',
-    details = jsonb_set(
+SET details = jsonb_set(
         jsonb_set(
             '{"mandateType":"TRANSFER_CANCELLATION"}'::jsonb,
             '{pillar}',
@@ -24,7 +27,7 @@ SET mandate_type = 'TRANSFER_CANCELLATION',
         to_jsonb(fund_transfer_exchange.source_fund_isin)
     )
   FROM fund_transfer_exchange
-WHERE mandate_type = '2'
+WHERE mandate_type = 'TRANSFER_CANCELLATION'
   AND fund_transfer_exchange.mandate_id = mandate.id
   AND fund_transfer_exchange.target_fund_isin IS NULL
   AND fund_transfer_exchange.amount IS NULL;

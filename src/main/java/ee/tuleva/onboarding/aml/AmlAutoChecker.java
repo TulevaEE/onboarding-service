10a148fc75e8d8a7c2bcbb79fc6ac2c71bfe40ed
Sander Nemvalts <snemvalts@gmail.com>
2024-09-23 10:28:24 +0300

    Fix merge oddity, run spotless

--- src/main/java/ee/tuleva/onboarding/aml/AmlAutoChecker.java
+++ src/main/java/ee/tuleva/onboarding/aml/AmlAutoChecker.java
@@ -13,7 +13,6 @@ import ee.tuleva.onboarding.user.address.Address;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j

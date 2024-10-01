b9d20d703a3c5c029d9cc11411d04b0bf84f208b
Sander Nemvalts <snemvalts@gmail.com>
2024-10-01 11:00:22 +0300

    Add mandatebatch integration test, improve coverage

--- src/main/java/ee/tuleva/onboarding/mandate/batch/MandateBatchController.java
+++ src/main/java/ee/tuleva/onboarding/mandate/batch/MandateBatchController.java
@@ -3,13 +3,12 @@ package ee.tuleva.onboarding.mandate.batch;
 import static ee.tuleva.onboarding.mandate.batch.MandateBatchController.MANDATE_BATCHES_URI;
 
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -20,6 +19,8 @@ public class MandateBatchController {
 
   private final MandateBatchService mandateBatchService;
 
+  @Operation(summary = "Create mandate batch")
+  @PostMapping()
   public MandateBatchDto createMandateBatch(
       @AuthenticationPrincipal AuthenticatedPerson authenticatedPerson,
       @Valid @RequestBody MandateBatchDto mandateBatchDto) {
--- src/main/java/ee/tuleva/onboarding/mandate/generic/GenericMandateService.java
+++ src/main/java/ee/tuleva/onboarding/mandate/generic/GenericMandateService.java
@@ -3,6 +3,7 @@ package ee.tuleva.onboarding.mandate.generic;
 import ee.tuleva.onboarding.auth.principal.AuthenticatedPerson;
 import ee.tuleva.onboarding.epis.mandate.details.MandateDetails;
 import ee.tuleva.onboarding.mandate.*;
+import ee.tuleva.onboarding.mandate.batch.MandateBatch;
 import ee.tuleva.onboarding.user.User;
 import ee.tuleva.onboarding.user.UserService;
 import java.util.List;
@@ -30,6 +31,29 @@ public class GenericMandateService {
             .createMandate(authenticatedPerson, (MandateDto<T>) mandateCreationDto);
 
     User user = userService.getById(authenticatedPerson.getUserId());
+    mandateService.save(user, mandate);
+
+    return mandate;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T extends MandateDetails> Mandate createGenericMandate(
+      AuthenticatedPerson authenticatedPerson,
+      MandateDto<?> mandateCreationDto,
+      MandateBatch batch) {
+    MandateType mandateType = mandateCreationDto.getDetails().getMandateType();
+
+    Mandate mandate =
+        mandateFactories.stream()
+            .filter(mandateFactory -> mandateFactory.supports(mandateType))
+            .map(mandateFactory -> (MandateFactory<T>) mandateFactory)
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("Unsupported mandateType: " + mandateType))
+            .createMandate(authenticatedPerson, (MandateDto<T>) mandateCreationDto);
+
+    User user = userService.getById(authenticatedPerson.getUserId());
+
+    mandate.setMandateBatch(batch);
     mandateService.save(user, mandate);
 
     return mandate;

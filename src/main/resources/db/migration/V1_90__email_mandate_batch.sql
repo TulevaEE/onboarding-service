333ba952e15c552bbeaba3624b8e39ebaf0c91de
Sander Nemvalts <snemvalts@gmail.com>
2024-11-14 09:08:05 +0100

    Fix FK name

--- src/main/resources/db/migration/V1_90__email_mandate_batch.sql
+++ src/main/resources/db/migration/V1_90__email_mandate_batch.sql
@@ -2,6 +2,6 @@ ALTER TABLE email
   ADD COLUMN mandate_batch_id BIGINT;
 
 ALTER TABLE email
-  ADD CONSTRAINT fk_mandate_batch
+  ADD CONSTRAINT fk_mandate_batch_email
     FOREIGN KEY (mandate_batch_id) REFERENCES mandate_batch(id)
       ON DELETE SET NULL;

cfb2c9d41791aa6537c17762cfd5d461a769ef36
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2024-11-27 13:32:52 +0700

    Spotless

--- src/main/java/ee/tuleva/onboarding/aml/AmlService.java
+++ src/main/java/ee/tuleva/onboarding/aml/AmlService.java
@@ -26,7 +26,6 @@ import java.util.stream.StreamSupport;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 

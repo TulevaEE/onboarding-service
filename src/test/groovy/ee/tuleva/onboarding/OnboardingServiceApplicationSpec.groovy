368e376e9715d658cb12f815cede01407a5b823b
Erko Risthein <erko@risthein.ee>
2025-01-25 09:59:18 +0200

    Improve our Smoke Test, so that it would try to initialize the Application Context in the dev profile – which means that it would use Postgres instead of H2 and detect any possible compatibility issues there

--- src/test/groovy/ee/tuleva/onboarding/OnboardingServiceApplicationSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/OnboardingServiceApplicationSpec.groovy
@@ -2,10 +2,12 @@ package ee.tuleva.onboarding
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class OnboardingServiceApplicationSpec extends Specification {
 
     @Autowired

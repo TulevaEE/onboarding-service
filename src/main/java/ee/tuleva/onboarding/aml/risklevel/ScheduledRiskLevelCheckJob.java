9062b8d120801088fac221504118f099cf304f74
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2025-01-25 10:42:19 +0700

    update cron

--- src/main/java/ee/tuleva/onboarding/aml/risklevel/ScheduledRiskLevelCheckJob.java
+++ src/main/java/ee/tuleva/onboarding/aml/risklevel/ScheduledRiskLevelCheckJob.java
@@ -15,7 +15,7 @@ public class ScheduledRiskLevelCheckJob {
   private final RiskLevelService riskLevelService;
 
   // Runs on the 25th of January (Europe/Tallinn) - repeats annually
-  @Scheduled(cron = "0 40 05 24 1 ?", zone = "Europe/Tallinn")
+  @Scheduled(cron = "0 00 06 25 1 ?", zone = "Europe/Tallinn")
   public void run() {
     log.info("Starting AML risk level check job");
     riskLevelService.runRiskLevelCheck();

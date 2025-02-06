897e5561c7e698f33d3e1b4e0a6af008cca6fe29
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2025-02-06 14:43:46 +0700

    Schedule aml risk level today

--- src/main/java/ee/tuleva/onboarding/aml/risklevel/ScheduledRiskLevelCheckJob.java
+++ src/main/java/ee/tuleva/onboarding/aml/risklevel/ScheduledRiskLevelCheckJob.java
@@ -14,8 +14,10 @@ public class ScheduledRiskLevelCheckJob {
 
   private final RiskLevelService riskLevelService;
 
-  //  Runs on the 1st day of each month at 01:00 (Europe/Tallinn)
-  @Scheduled(cron = "0 0 1 1 * ?", zone = "Europe/Tallinn")
+  //  Runs on the 1st day of each month at 01:00 (Europe/Tallinn)...
+  //  @Scheduled(cron = "0 0 1 1 * ?", zone = "Europe/Tallinn")
+  //  Runs once (Europe/Tallinn) - repeats annually
+  @Scheduled(cron = "0 05 10 06 2 ?", zone = "Europe/Tallinn")
   public void run() {
     log.info("Starting AML risk level check job");
     riskLevelService.runRiskLevelCheck();

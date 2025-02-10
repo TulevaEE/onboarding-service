aa878fb1fc36fb71b63fc305e38e63cc6dfaa909
Sander Nemvalts <snemvalts@gmail.com>
2025-02-10 10:48:06 +0200

    Add back tests for signature service start methods

--- src/test/groovy/ee/tuleva/onboarding/mandate/batch/MandateBatchSignatureServiceTest.java
+++ src/test/groovy/ee/tuleva/onboarding/mandate/batch/MandateBatchSignatureServiceTest.java
@@ -1,6 +1,9 @@
 package ee.tuleva.onboarding.mandate.batch;
 
+import static ee.tuleva.onboarding.auth.AuthenticatedPersonFixture.authenticatedPersonFromUser;
 import static ee.tuleva.onboarding.auth.AuthenticatedPersonFixture.sampleAuthenticatedPersonAndMember;
+import static ee.tuleva.onboarding.auth.UserFixture.sampleUser;
+import static ee.tuleva.onboarding.auth.mobileid.MobileIDSession.PHONE_NUMBER;
 import static ee.tuleva.onboarding.mandate.response.MandateSignatureStatus.SIGNATURE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -11,11 +14,17 @@ import static org.mockito.Mockito.when;
 import ee.tuleva.onboarding.auth.session.GenericSessionStore;
 import ee.tuleva.onboarding.locale.LocaleService;
 import ee.tuleva.onboarding.mandate.MandateFixture;
+import ee.tuleva.onboarding.mandate.signature.SignatureService;
 import ee.tuleva.onboarding.mandate.signature.idcard.IdCardSignatureSession;
 import ee.tuleva.onboarding.mandate.signature.mobileid.MobileIdSignatureSession;
 import ee.tuleva.onboarding.mandate.signature.smartid.SmartIdSignatureSession;
+
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
+
+import ee.tuleva.onboarding.user.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -33,11 +42,38 @@ class MandateBatchSignatureServiceTest {
 
   @Mock private LocaleService localeService;
 
+  @Mock private SignatureService signService;
+  @Mock private UserService userService;
+
+
   @InjectMocks private MandateBatchSignatureService mandateBatchSignatureService;
 
   @Nested
   @DisplayName("mobile id")
   class MobileIdTests {
+
+    @Test
+    @DisplayName("start mobile id signature returns the mobile ID challenge code")
+    void startMobileIdSignatureReturnsChallengeCode() {
+      var mandateBatchId = 1L;
+      var phoneNumber = "+372 555 5555";
+      var mockSession = MobileIdSignatureSession.builder().verificationCode("1234").build();
+      var user = sampleUser().build();
+      var authenticatedPerson =
+          authenticatedPersonFromUser(user)
+              .attributes(Map.of(PHONE_NUMBER, phoneNumber))
+              .build();
+
+      when(userService.getById(eq(authenticatedPerson.getUserId()))).thenReturn(user);
+      when(mandateBatchService.getMandateBatchContentFiles(eq(mandateBatchId), eq(user))).thenReturn(List.of());
+      when(signService.startMobileIdSign(any(), eq(authenticatedPerson.getPersonalCode()), eq(phoneNumber)))
+          .thenReturn(mockSession);
+
+      var result = mandateBatchSignatureService.startMobileIdSignature(mandateBatchId, authenticatedPerson);
+
+      assertThat(result.getChallengeCode()).isEqualTo("1234");
+      verify(sessionStore, times(1)).save(mockSession);
+    }
 
     @Test
     @DisplayName("get mobile id signature status returns the status and challenge code")
@@ -64,6 +100,26 @@ class MandateBatchSignatureServiceTest {
   class SmartIdTests {
 
     @Test
+    @DisplayName("start smart id signature returns null challenge code")
+    void startSmartIdSignatureReturnsNullChallengeCode() {
+      var mandateBatchId = 1L;
+      var mockSession = new SmartIdSignatureSession("certSessionId", "personalCode", null);
+      mockSession.setVerificationCode(null);
+      var user = sampleUser().build();
+      var authenticatedPerson =
+          authenticatedPersonFromUser(user).build();
+
+      when(userService.getById(eq(authenticatedPerson.getUserId()))).thenReturn(user);
+      when(mandateBatchService.getMandateBatchContentFiles(eq(mandateBatchId), eq(user))).thenReturn(List.of());
+      when(signService.startSmartIdSign(any(), eq(user.getPersonalCode()))).thenReturn(mockSession);
+
+      var result = mandateBatchSignatureService.startSmartIdSignature(mandateBatchId, authenticatedPerson);
+
+      assertThat(result.getChallengeCode()).isNull();
+      verify(sessionStore, times(1)).save(mockSession);
+    }
+
+    @Test
     @DisplayName("get smart id signature status returns the status and challenge code")
     void getSmartIdSignatureStatusReturnsStatusAndChallengeCode() {
       var mandateBatchId = 1L;
@@ -87,6 +143,29 @@ class MandateBatchSignatureServiceTest {
   @Nested
   @DisplayName("id card")
   class IdCardTests {
+
+    @Test
+    @DisplayName("start id card signature returns the hash to be signed by the client")
+    void startIdCardSignatureReturnsHash() {
+      var mandateBatchId = 1L;
+      var clientCertificate = "clientCertificate";
+      var startCommand = MandateFixture.sampleStartIdCardSignCommand(clientCertificate);
+      var mockSession = IdCardSignatureSession.builder().hashToSignInHex("asdfg").build();
+
+      var user = sampleUser().build();
+      var authenticatedPerson =
+          authenticatedPersonFromUser(user).build();
+
+      when(userService.getById(eq(authenticatedPerson.getUserId()))).thenReturn(user);
+      when(mandateBatchService.getMandateBatchContentFiles(eq(mandateBatchId), eq(user))).thenReturn(List.of());
+      when(signService.startIdCardSign(any(), eq(clientCertificate)))
+          .thenReturn(mockSession);
+
+      var result = mandateBatchSignatureService.startIdCardSign(mandateBatchId, authenticatedPerson, startCommand);
+
+      assertThat(result.getHash()).isEqualTo("asdfg");
+      verify(sessionStore, times(1)).save(mockSession);
+    }
 
     @Test
     @DisplayName("persistIdCardAndGetProcessingStatus returns finished status code")

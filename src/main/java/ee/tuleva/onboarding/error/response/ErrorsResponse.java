584d1d98cef26bd178ff84befff89c9d5bbd2091
Erko Risthein <erko@risthein.ee>
2025-02-19 17:32:22 +0200

    Spotless fix

--- src/main/java/ee/tuleva/onboarding/error/response/ErrorsResponse.java
+++ src/main/java/ee/tuleva/onboarding/error/response/ErrorsResponse.java
@@ -1,7 +1,6 @@
 package ee.tuleva.onboarding.error.response;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,8 +18,7 @@ public class ErrorsResponse {
   private List<ErrorResponse> errors = new ArrayList<>();
 
   public static ErrorsResponse ofSingleError(String code, String message) {
-    return new ErrorsResponse(
-        List.of(ErrorResponse.builder().code(code).message(message).build()));
+    return new ErrorsResponse(List.of(ErrorResponse.builder().code(code).message(message).build()));
   }
 
   public static ErrorsResponse ofSingleError(ErrorResponse error) {

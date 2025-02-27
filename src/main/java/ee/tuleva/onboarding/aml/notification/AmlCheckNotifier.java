58128ecaec458811893bfe544da21909b86770c4
Sander Nemvalts <snemvalts@gmail.com>
2025-02-27 12:42:56 +0200

    WIP multiple webhook logic

--- src/main/java/ee/tuleva/onboarding/aml/notification/AmlCheckNotifier.java
+++ src/main/java/ee/tuleva/onboarding/aml/notification/AmlCheckNotifier.java
@@ -1,9 +1,12 @@
 package ee.tuleva.onboarding.aml.notification;
 
 import static ee.tuleva.onboarding.aml.AmlCheckType.*;
+import static ee.tuleva.onboarding.notification.slack.SlackService.SlackChannel.AML;
 
-import ee.tuleva.onboarding.notification.slack.AmlSlackService;
+import ee.tuleva.onboarding.notification.slack.SlackService;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -14,27 +17,27 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AmlCheckNotifier {
 
-  private final AmlSlackService slackService;
+  private final SlackService slackService;
 
   @EventListener
   public void onAmlCheckCreated(AmlCheckCreatedEvent event) {
     if (List.of(POLITICALLY_EXPOSED_PERSON_AUTO, SANCTION, RISK_LEVEL)
-            .contains(event.getAmlCheckType())
+        .contains(event.getAmlCheckType())
         && event.isFailed()) {
-      slackService.sendMessage("AML check failed: type=%s".formatted(event.getAmlCheckType()));
+      slackService.sendMessage("AML check failed: type=%s".formatted(event.getAmlCheckType()), AML);
     }
   }
 
   @EventListener
   public void onScheduledAmlCheckJobRun(AmlChecksRunEvent event) {
     slackService.sendMessage(
-        "Running AML checks job: numberOfRecords=%d".formatted(event.getNumberOfRecords()));
+        "Running AML checks job: numberOfRecords=%d".formatted(event.getNumberOfRecords()), AML);
   }
 
   @EventListener
   public void onAmlRiskLevelJobRun(AmlRiskLevelJobRunEvent event) {
     slackService.sendMessage(
         "Ran AML Risk Level job: highRiskRecordCount=%d, amlChecksCreatedCount=%d"
-            .formatted(event.getHighRiskRowCount(), event.getAmlChecksCreatedCount()));
+            .formatted(event.getHighRiskRowCount(), event.getAmlChecksCreatedCount()), AML);
   }
 }

36ae8a85d4312b3a24c555483d54f7b17483e288
Sander Nemvalts <snemvalts@gmail.com>
2025-03-04 10:52:03 +0200

    Fix autobind magic, undo test logic

--- src/main/java/ee/tuleva/onboarding/notification/slack/SlackWebhookConfiguration.java
+++ src/main/java/ee/tuleva/onboarding/notification/slack/SlackWebhookConfiguration.java
@@ -1,19 +1,20 @@
 package ee.tuleva.onboarding.notification.slack;
 
 import ee.tuleva.onboarding.notification.slack.SlackService.SlackChannel;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
 @Configuration
-@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "slack")
-class SlackWebhookConfiguration {
-  
-  private final Map<String, String> webhooks;
+@Getter
+@Setter
+public class SlackWebhookConfiguration {
 
+  private Map<String, String> webhooks;
 
   public String getWebhookUrl(SlackChannel channel) {
     return webhooks.getOrDefault(channel.getConfigurationKey(), null);

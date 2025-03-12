dfdbb56e1b7aba1544e7b5b47a2ea1a1ac4e8156
Sander Nemvalts <snemvalts@gmail.com>
2025-03-12 12:36:53 +0200

    Add test, improve coverage

--- src/test/groovy/ee/tuleva/onboarding/mandate/generic/SelectionMandateFactoryTest.java
+++ src/test/groovy/ee/tuleva/onboarding/mandate/generic/SelectionMandateFactoryTest.java
@@ -1,10 +1,9 @@
 package ee.tuleva.onboarding.mandate.generic;
 
 import static ee.tuleva.onboarding.auth.UserFixture.sampleUser;
-import static ee.tuleva.onboarding.conversion.ConversionResponseFixture.*;
+import static ee.tuleva.onboarding.conversion.ConversionResponseFixture.fullyConverted;
 import static ee.tuleva.onboarding.epis.contact.ContactDetailsFixture.contactDetailsFixture;
-import static ee.tuleva.onboarding.epis.mandate.details.PaymentRateChangeMandateDetails.PaymentRate.SIX;
-import static ee.tuleva.onboarding.mandate.MandateFixture.*;
+import static ee.tuleva.onboarding.mandate.MandateFixture.sampleMandateCreationDto;
 import static ee.tuleva.onboarding.mandate.MandateType.*;
 import static ee.tuleva.onboarding.pillar.Pillar.SECOND;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -14,7 +13,7 @@ import static org.mockito.Mockito.*;
 import ee.tuleva.onboarding.auth.AuthenticatedPersonFixture;
 import ee.tuleva.onboarding.conversion.UserConversionService;
 import ee.tuleva.onboarding.epis.EpisService;
-import ee.tuleva.onboarding.epis.mandate.details.PaymentRateChangeMandateDetails;
+import ee.tuleva.onboarding.epis.mandate.details.SelectionMandateDetails;
 import ee.tuleva.onboarding.mandate.Mandate;
 import ee.tuleva.onboarding.mandate.builder.ConversionDecorator;
 import ee.tuleva.onboarding.user.UserService;
@@ -27,7 +26,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class PaymentRateChangeMandateFactoryTest {
+public class SelectionMandateFactoryTest {
 
   @Mock private EpisService episService;
 
@@ -37,22 +36,23 @@ public class PaymentRateChangeMandateFactoryTest {
 
   @Mock private ConversionDecorator conversionDecorator;
 
-  @InjectMocks private PaymentRateChangeMandateFactory paymentRateChangeMandateFactory;
+  @InjectMocks private SelectionMandateFactory selectionMandateFactory;
 
   @Test
   @DisplayName("delegates mandate creation to mandate factory")
   void testDelegateToMandateFactory() {
     var anUser = sampleUser().build();
     var aContactDetails = contactDetailsFixture();
+    var futureContributionIsin = "EE_TEST_ISIN";
 
-    var anDto = sampleMandateCreationDto(new PaymentRateChangeMandateDetails(SIX));
+    var anDto = sampleMandateCreationDto(new SelectionMandateDetails(futureContributionIsin));
 
     when(userService.getById(any())).thenReturn(anUser);
     when(conversionService.getConversion(any())).thenReturn(fullyConverted());
     when(episService.getContactDetails(any())).thenReturn(aContactDetails);
 
     Mandate genericMandate =
-        paymentRateChangeMandateFactory.createMandate(
+        selectionMandateFactory.createMandate(
             AuthenticatedPersonFixture.authenticatedPersonFromUser(anUser).build(), anDto);
 
     assertThat(genericMandate.getUser()).isEqualTo(anUser);
@@ -60,19 +60,19 @@ public class PaymentRateChangeMandateFactoryTest {
     assertThat(genericMandate.getFundTransferExchanges()).isEqualTo(List.of());
     verify(conversionDecorator, times(1)).addConversionMetadata(any(), any(), any(), any());
 
-    assertThat(genericMandate.getDetails()).isInstanceOf(PaymentRateChangeMandateDetails.class);
-    assertThat(((PaymentRateChangeMandateDetails) genericMandate.getDetails()).getPaymentRate())
-        .isEqualTo(SIX);
+    assertThat(genericMandate.getDetails()).isInstanceOf(SelectionMandateDetails.class);
+    assertThat(
+            ((SelectionMandateDetails) genericMandate.getDetails()).getFutureContributionFundIsin())
+        .isEqualTo(futureContributionIsin);
     assertThat(genericMandate.getPillar()).isEqualTo(SECOND.toInt());
 
-    assertThat(genericMandate.getGenericMandateDto().getMandateType())
-        .isEqualTo(PAYMENT_RATE_CHANGE);
+    assertThat(genericMandate.getGenericMandateDto().getMandateType()).isEqualTo(SELECTION);
   }
 
   @Test
-  @DisplayName("supports PAYMENT_RATE_CHANGE mandates")
+  @DisplayName("supports SELECTION mandates")
   void testSupports() {
-    assertThat(paymentRateChangeMandateFactory.supports(PAYMENT_RATE_CHANGE)).isTrue();
-    assertThat(paymentRateChangeMandateFactory.supports(WITHDRAWAL_CANCELLATION)).isFalse();
+    assertThat(selectionMandateFactory.supports(SELECTION)).isTrue();
+    assertThat(selectionMandateFactory.supports(WITHDRAWAL_CANCELLATION)).isFalse();
   }
 }

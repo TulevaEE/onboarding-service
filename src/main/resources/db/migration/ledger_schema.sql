39b4efb33d7f32c68c29a7df8c1638d2e7eeecd1
Sander Nemvalts <snemvalts@gmail.com>
2025-04-23 08:38:23 +0300

    Move LedgerAccount to correct package, iterate on SQL schema of ledger

--- src/main/resources/db/migration/ledger_schema.sql
+++ src/main/resources/db/migration/ledger_schema.sql
@@ -1,39 +1,64 @@
 CREATE SCHEMA ledger;
 /* TODO permissions */
 
-CREATE TYPE currency AS ENUM ('EUR', 'EE_TEST_FUND_ISIN'); /* rename from isin? */
-CREATE TYPE ledger_type AS ENUM ('ASSET', 'LIABILITY', 'INCOME', 'EXPENSE');
-CREATE TYPE managed_account_type AS ENUM ('COMPANY', 'CHILD');
+CREATE TYPE ledger.account_type AS ENUM ('ASSET', 'LIABILITY', 'INCOME', 'EXPENSE');
+CREATE TYPE ledger.service_account_type AS ENUM ('DEPOSIT_EUR'); /*TODO add/rename here */
+CREATE TYPE ledger.asset_category AS ENUM ('EUR', 'UNIT');
+CREATE TYPE ledger.party_type AS ENUM ('USER', 'LEGAL_ENTITY');
+
+CREATE TABLE ledger.asset_type(
+    code VARCHAR(255) PRIMARY KEY,
+    name TEXT NOT NULL,
+    precision INTEGER NOT NULL,
+    category asset_category NOT NULL
+);
+
+
+CREATE TABLE ledger.transaction_type(
+    code VARCHAR(255) PRIMARY KEY,
+    description TEXT NOT NULL
+);
+
+
+CREATE TABLE ledger.party(
+  id UUID PRIMARY KEY,
+  type ledger.party_type NOT NULL,
+  name TEXT NOT NULL,
+  details JSONB NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
 
 CREATE TABLE ledger.account(
     id UUID PRIMARY KEY,
-    user_id INTEGER REFERENCES public.users,
-    service_account BOOLEAN, /* TODO use specific enum types here, or only two potential values. service_account = FALSE is a valid type currently but shouldn't */
-    managed_account managed_account_type,
-    managee_id TEXT, /* child's personal code, or company's registration code */
     name TEXT NOT NULL,
-    currency currency NOT NULL,
-    type ledger_type NOT NULL,
-    created_time TIMESTAMPTZ DEFAULT NOW(),
-    /* cannot be owned by user and a service account at the same time */
+    service_account_type ledger.service_account_type,
+    type ledger.account_type NOT NULL,
+    owner_party_id UUID REFERENCES ledger.party,
+    asset_type_code VARCHAR(255) NOT NULL references ledger.asset_type,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    /* cannot be owned by party and a service account at the same time */
     CHECK (
-      (user_id IS NOT NULL AND service_account IS NOT NULL)
-      OR (user_id IS NULL and service_account IS NULL)
+      (owner_party_id IS NOT NULL AND service_account_type IS NOT NULL)
+      OR (owner_party_id IS NULL and service_account_type IS NULL)
     ),
     /* cannot be a service account and a managed account at the same time */
     CHECK (
-      (service_account IS NOT NULL AND managed_account IS NULL)
-      OR (managed_account IS NULL AND managee_id IS NULL)
-    ),
-    UNIQUE (user_id, managee_id) /*can only have 1 account per managee */
+      (service_account_type IS NOT NULL AND owner_party_id IS NULL)
+      OR (owner_party_id IS NULL AND owner_party_id IS NULL)
+    )
     /*UNIQUE (service_account, type) TODO can only have 1 service account of type*/
 );
 
 
 CREATE TABLE ledger.transaction(
     id UUID PRIMARY KEY,
+    description TEXT NOT NULL,
+    transaction_type_id VARCHAR(255) NOT NULL REFERENCES ledger.transaction_type,
+    transaction_date TIMESTAMPTZ NOT NULL,
     metadata JSONB NOT NULL, /* TODO lock this down – or keep this a JSONB here with very heavy validations in application layer */
-    created_time TIMESTAMPTZ DEFAULT NOW()
+    event_log_id INTEGER NOT NULL references public.event_log,
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE ledger.entry(
@@ -41,7 +66,7 @@ CREATE TABLE ledger.entry(
     account_id UUID NOT NULL references ledger.account,
     transaction_id UUID NOT NULL references ledger.transaction,
     amount NUMERIC NOT NULL,
-    created_time TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 

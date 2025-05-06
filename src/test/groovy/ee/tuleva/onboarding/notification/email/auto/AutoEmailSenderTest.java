bd2ee8cc601b8f274b9d6bef928299c358873d4b
Sander Nemvalts <snemvalts@gmail.com>
2025-05-06 15:51:27 +0300

    Make auto email threshold rejection more sensitive by accounting for emails that match but already got an email of this type in the last 4 months

--- src/test/groovy/ee/tuleva/onboarding/notification/email/auto/AutoEmailSenderTest.java
+++ src/test/groovy/ee/tuleva/onboarding/notification/email/auto/AutoEmailSenderTest.java
@@ -7,7 +7,6 @@ import static ee.tuleva.onboarding.mandate.email.persistence.EmailType.SECOND_PI
 import static ee.tuleva.onboarding.mandate.email.persistence.EmailType.SECOND_PILLAR_LEAVERS;
 import static ee.tuleva.onboarding.notification.email.auto.EmailEvent.NEW_EARLY_WITHDRAWAL;
 import static ee.tuleva.onboarding.notification.email.auto.EmailEvent.NEW_LEAVER;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -16,6 +15,7 @@ import static org.mockito.quality.Strictness.*;
 
 import ee.tuleva.onboarding.analytics.earlywithdrawals.AnalyticsEarlyWithdrawalsRepository;
 import ee.tuleva.onboarding.analytics.leavers.ExchangeTransactionLeaver;
+import ee.tuleva.onboarding.analytics.leavers.ExchangeTransactionLeaverFixture;
 import ee.tuleva.onboarding.analytics.leavers.ExchangeTransactionLeaversRepository;
 import ee.tuleva.onboarding.mandate.email.persistence.EmailPersistenceService;
 import ee.tuleva.onboarding.notification.email.provider.MailchimpService;
@@ -24,9 +24,9 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -85,6 +85,23 @@ class AutoEmailSenderTest {
     // Then
     verify(mailchimpService, times(1)).sendEvent(leaver.email(), NEW_LEAVER);
     verify(emailPersistenceService, times(1)).save(leaver, SECOND_PILLAR_LEAVERS, SCHEDULED);
+  }
+
+  @Test
+  @DisplayName("Does not send leaver emails over threshold")
+  void rejectsLeaverEmailsOverThreshold() {
+    List<ExchangeTransactionLeaver> leavers =
+        Stream.generate(ExchangeTransactionLeaverFixture::leaverFixture).limit(101).toList();
+
+    var leaver = leaverFixture();
+    when(leaversRepository.fetch(any(LocalDate.class), any(LocalDate.class))).thenReturn(leavers);
+    when(emailPersistenceService.getLastEmailSendDate(eq(leaver), eq(SECOND_PILLAR_LEAVERS)))
+        .thenReturn(Optional.empty());
+
+    assertThrows(
+        IllegalStateException.class,
+        () -> autoEmailSender.sendMonthlyEmails(),
+        "Too many people for monthly emails: emailType=SECOND_PILLAR_LEAVERS, estimatedSendCount=101");
   }
 
   @Test
@@ -228,22 +245,5 @@ class AutoEmailSenderTest {
     verify(mailchimpService, times(2)).sendEvent(anyString(), eq(NEW_LEAVER));
     verify(emailPersistenceService, times(2))
         .save(any(ExchangeTransactionLeaver.class), eq(SECOND_PILLAR_LEAVERS), eq(SCHEDULED));
-  }
-
-  @Test
-  @DisplayName("Does not send more than 100 emails at once. throws IllegalStateException")
-  void doesNotSendMoreThan100Emails() {
-    // Given
-    when(leaversRepository.fetch(any(LocalDate.class), any(LocalDate.class)))
-        .thenReturn(Collections.nCopies(101, leaverFixture()));
-
-    // When
-    Exception exception =
-        assertThrows(IllegalStateException.class, () -> autoEmailSender.sendMonthlyEmails());
-
-    // Then
-    assertEquals(
-        "Sanity check – too many emailable people found. Please debug and make sure there is no bug: emailType=SECOND_PILLAR_LEAVERS, emailablePeople=101",
-        exception.getMessage());
   }
 }

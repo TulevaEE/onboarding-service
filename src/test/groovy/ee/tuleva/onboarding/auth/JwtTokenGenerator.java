f6f97f6473efec4c92c942af828171c6d9f00973
Erko Risthein <erko@risthein.ee>
2025-05-15 20:28:06 +0700

    Add test for Security Config

--- src/test/groovy/ee/tuleva/onboarding/auth/JwtTokenGenerator.java
+++ src/test/groovy/ee/tuleva/onboarding/auth/JwtTokenGenerator.java
@@ -1,27 +1,28 @@
 package ee.tuleva.onboarding.auth;
 
-import static ee.tuleva.onboarding.auth.PersonFixture.samplePerson;
-import static ee.tuleva.onboarding.auth.jwt.CustomClaims.*;
-import static ee.tuleva.onboarding.auth.jwt.CustomClaims.TOKEN_TYPE;
-import static ee.tuleva.onboarding.auth.mobileid.MobileIDSession.PHONE_NUMBER;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
-import ee.tuleva.onboarding.auth.authority.Authority;
 import ee.tuleva.onboarding.auth.jwt.TokenType;
 import ee.tuleva.onboarding.auth.principal.Person;
 import io.jsonwebtoken.Jwts;
+import org.springframework.http.HttpHeaders;
+
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import org.springframework.http.HttpHeaders;
+
+import static ee.tuleva.onboarding.auth.PersonFixture.samplePerson;
+import static ee.tuleva.onboarding.auth.authority.Authority.USER;
+import static ee.tuleva.onboarding.auth.jwt.CustomClaims.*;
+import static ee.tuleva.onboarding.auth.jwt.TokenType.ACCESS;
+import static ee.tuleva.onboarding.auth.mobileid.MobileIDSession.PHONE_NUMBER;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 public class JwtTokenGenerator {
 
   public static HttpHeaders getHeaders() {
-    var jwtToken = JwtTokenGenerator.generateDefaultJwtToken(samplePerson());
+    var jwtToken = generateDefaultJwtToken(samplePerson());
 
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(APPLICATION_JSON);
@@ -31,7 +32,7 @@ public class JwtTokenGenerator {
   }
 
   public static HttpHeaders getHeaders(Person person) {
-    var jwtToken = JwtTokenGenerator.generateDefaultJwtToken(person);
+    var jwtToken = generateDefaultJwtToken(person);
 
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(APPLICATION_JSON);
@@ -41,11 +42,16 @@ public class JwtTokenGenerator {
   }
 
   public static String generateDefaultJwtToken(Person person) {
-    return JwtTokenGenerator.generateToken(
-        List.of("ROLE_USER"), "test-jwt-keystore.p12", "Kalamaja123", "jwt", "Kalamaja123", person);
+    return generateJwtToken(person, ACCESS, List.of(USER));
+  }
+
+  public static String generateJwtToken(Person person, TokenType tokenType, List<String> authorities) {
+    return generateToken(
+        tokenType, authorities, "test-jwt-keystore.p12", "Kalamaja123", "jwt", "Kalamaja123", person);
   }
 
   public static String generateToken(
+      TokenType tokenType,
       List<String> authorities,
       String keystorePath,
       String keystorePassword,
@@ -71,12 +77,11 @@ public class JwtTokenGenerator {
 
       return Jwts.builder()
           .subject(person.getPersonalCode())
-          .claim("authorities", authorities)
-          .claim(TOKEN_TYPE.getValue(), TokenType.ACCESS)
+          .claim(TOKEN_TYPE.getValue(), tokenType)
           .claim(FIRST_NAME.getValue(), person.getFirstName())
           .claim(LAST_NAME.getValue(), person.getLastName())
           .claim(ATTRIBUTES.getValue(), Map.of(PHONE_NUMBER, "+372 555 5555"))
-          .claim(AUTHORITIES.getValue(), List.of(Authority.USER))
+          .claim(AUTHORITIES.getValue(), authorities)
           .issuedAt(new Date())
           .expiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour expiration
           .signWith(privateKey, Jwts.SIG.RS256)

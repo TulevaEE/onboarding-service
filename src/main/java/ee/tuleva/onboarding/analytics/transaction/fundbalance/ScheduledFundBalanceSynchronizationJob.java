332b0cdba9780c32b34176e938b94de9a74ad654
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2025-05-16 17:52:32 +0700

    Remove ScheduledFundBalanceSynchronizationJob historical sync code

--- src/main/java/ee/tuleva/onboarding/analytics/transaction/fundbalance/ScheduledFundBalanceSynchronizationJob.java
+++ src/main/java/ee/tuleva/onboarding/analytics/transaction/fundbalance/ScheduledFundBalanceSynchronizationJob.java
@@ -2,7 +2,6 @@ package ee.tuleva.onboarding.analytics.transaction.fundbalance;
 
 import ee.tuleva.onboarding.time.ClockHolder;
 import java.time.LocalDate;
-import java.time.Month;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
@@ -33,29 +32,5 @@ public class ScheduledFundBalanceSynchronizationJob {
           e.getMessage(),
           e);
     }
-  }
-
-  @Scheduled(cron = "0 20 10 16 5 ?", zone = "Europe/Tallinn")
-  public void runHistoricalFundBalanceSync() {
-    LocalDate startDate = LocalDate.of(2025, Month.APRIL, 22);
-    LocalDate endDate = LocalDate.of(2025, Month.MAY, 16);
-    log.info(
-        "Starting historical fund balance synchronization job from {} to {}.", startDate, endDate);
-
-    for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
-      try {
-        log.debug("Synchronizing fund balances for date {}.", date);
-        fundBalanceSynchronizer.sync(date);
-        log.debug("Successfully synchronized fund balances for date {}.", date);
-      } catch (Exception e) {
-        log.error(
-            "Historical fund balance synchronization failed during execution for date {}: {}",
-            date,
-            e.getMessage(),
-            e);
-      }
-    }
-    log.info(
-        "Finished historical fund balance synchronization job from {} to {}.", startDate, endDate);
   }
 }

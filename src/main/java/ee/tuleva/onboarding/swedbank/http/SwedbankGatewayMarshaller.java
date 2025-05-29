13163a194386c3cec492d5d5ee8f03688c89c846
Sander Nemvalts <snemvalts@gmail.com>
2025-05-29 12:10:56 +0300

    Remove dynamic class scanning to prevent CI timeouts during integration tests, add classes manually

--- src/main/java/ee/tuleva/onboarding/swedbank/http/SwedbankGatewayMarshaller.java
+++ src/main/java/ee/tuleva/onboarding/swedbank/http/SwedbankGatewayMarshaller.java
@@ -1,12 +1,13 @@
 package ee.tuleva.onboarding.swedbank.http;
 
-import static java.util.stream.Collectors.toSet;
-
+import ee.swedbank.gateway.request.AccountStatement;
+import ee.swedbank.gateway.request.GetBalance;
+import ee.swedbank.gateway.request.Ping;
+import ee.swedbank.gateway.response.B4B;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 import java.io.*;
-import java.util.Set;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
@@ -30,32 +31,17 @@ class SwedbankGatewayMarshaller {
 
   @SneakyThrows
   public SwedbankGatewayMarshaller() {
-    var requestClasses =
-        findAllClassesUsingClassLoader("ee.swedbank.gateway.request").stream()
-            .toArray(Class[]::new);
+    Class[] requestClasses = {
+      AccountStatement.class, GetBalance.class, Ping.class,
+    };
+
     JAXBContext marshalContext = JAXBContext.newInstance(requestClasses);
     this.marshaller = marshalContext.createMarshaller();
 
-    var responseClasses =
-        findAllClassesUsingClassLoader("ee.swedbank.gateway.response").stream()
-            .toArray(Class[]::new);
+    Class[] responseClasses = {
+      B4B.class,
+    };
     JAXBContext unMarshalContext = JAXBContext.newInstance(responseClasses);
     this.unmarshaller = unMarshalContext.createUnmarshaller();
-  }
-
-  private static Set<Class> findAllClassesUsingClassLoader(String packageName) {
-    InputStream stream =
-        ClassLoader.getSystemClassLoader().getResourceAsStream(packageName.replaceAll("[.]", "/"));
-    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-    return reader
-        .lines()
-        .filter(line -> line.endsWith(".class"))
-        .map(line -> getClass(line, packageName))
-        .collect(toSet());
-  }
-
-  @SneakyThrows
-  private static Class getClass(String className, String packageName) {
-    return Class.forName(packageName + "." + className.substring(0, className.lastIndexOf('.')));
   }
 }

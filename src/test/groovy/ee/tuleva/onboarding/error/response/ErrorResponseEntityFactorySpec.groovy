8d0ac6a4ed9ffc330d4e32a5f1c2d6f0f9b7bd35
Erko Risthein <erko@risthein.ee>
2025-06-03 16:33:37 +0300

    Add /listings API, fix DI problems with @WebMvcTest, fix tests

--- src/test/groovy/ee/tuleva/onboarding/error/response/ErrorResponseEntityFactorySpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/error/response/ErrorResponseEntityFactorySpec.groovy
@@ -1,6 +1,6 @@
 package ee.tuleva.onboarding.error.response
 
-import ee.tuleva.onboarding.error.converter.InputErrorsConverter
+
 import ee.tuleva.onboarding.error.converter.TestCommand
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.Errors
@@ -10,13 +10,11 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST
 
 class ErrorResponseEntityFactorySpec extends Specification {
 
-    InputErrorsConverter inputErrorsConverter = Mock(InputErrorsConverter)
-    ErrorResponseEntityFactory errorResponseEntityFactory = new ErrorResponseEntityFactory(inputErrorsConverter)
+    ErrorResponseEntityFactory errorResponseEntityFactory = new ErrorResponseEntityFactory()
 
     def "OfErrors: Creates errors response from errors"() {
         given:
         Errors errors = ErrorFactory.manufactureErrors(new TestCommand())
-        1 * inputErrorsConverter.convert(errors) >> new ErrorsResponse()
         when:
         ResponseEntity responseEntity = errorResponseEntityFactory.fromErrors(errors)
 

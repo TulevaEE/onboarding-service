eb3d1d6f3bc02ee33f3a7147529145b639a76416
Erko Risthein <erko@risthein.ee>
2025-06-10 17:01:30 +0300

    Create Deal domain

--- src/main/resources/db/migration/V1_100__listing_schema.sql
+++ src/main/resources/db/migration/V1_100__listing_schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE listing
   price_per_unit NUMERIC(14, 2)     NOT NULL,
   currency       VARCHAR(3)         NOT NULL,
   state          VARCHAR(20)        NOT NULL,
+  iban           VARCHAR(34),
   expiry_time    TIMESTAMP          NOT NULL,
   created_time   TIMESTAMP          NOT NULL DEFAULT NOW(),
   cancelled_time TIMESTAMP,
@@ -14,3 +15,17 @@ CREATE TABLE listing
 );
 
 CREATE INDEX listings_member_id_index ON listing (member_id);
+
+CREATE TABLE deal
+(
+  id                  SERIAL PRIMARY KEY NOT NULL,
+  listing_id          BIGINT             NOT NULL REFERENCES listing (id),
+  member_id           BIGINT             NOT NULL REFERENCES member (id),
+  state               VARCHAR(20)        NOT NULL,
+  created_time        TIMESTAMP          NOT NULL DEFAULT NOW(),
+  buyer_confirmed_time  TIMESTAMP,
+  seller_confirmed_time TIMESTAMP
+);
+
+CREATE INDEX deal_listing_id_index ON deal (listing_id);
+CREATE INDEX deal_member_id_index ON deal (member_id);

3efc7db22f92d8c739c337fe2c54805e99d87279
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2025-06-11 12:57:03 +0700

    Adds TrackableEvent integration test

---
+++ src/test/groovy/ee/tuleva/onboarding/event/TrackableEventLoggerIntegrationTest.java
@@ -0,0 +1,75 @@
+package ee.tuleva.onboarding.event;
+
+import static ee.tuleva.onboarding.auth.PersonFixture.samplePerson;
+import static ee.tuleva.onboarding.auth.PersonFixture.sampleRetirementAgePerson;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ee.tuleva.onboarding.auth.principal.Person;
+import java.time.Instant;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class TrackableEventLoggerIntegrationTest {
+
+  @Autowired private ApplicationEventPublisher eventPublisher;
+
+  @Autowired private EventLogRepository eventLogRepository;
+
+  @BeforeEach
+  void setUp() {
+    eventLogRepository.deleteAll();
+  }
+
+  @Test
+  @Transactional
+  @Commit
+  void logsTrackableEvent() {
+    // Given
+    Person person = samplePerson;
+    TrackableEventType eventType = TrackableEventType.LOGIN;
+    Map<String, Object> eventData = Map.of("key", "value", "anotherKey", 123);
+    TrackableEvent eventToPublish = new TrackableEvent(person, eventType, eventData);
+
+    // When
+    eventPublisher.publishEvent(eventToPublish);
+
+    // Then
+    Iterable<EventLog> eventLogs = eventLogRepository.findAll();
+    assertThat(eventLogs).hasSize(1);
+
+    EventLog savedLog = eventLogs.iterator().next();
+    assertThat(savedLog.getPrincipal()).isEqualTo(person.getPersonalCode());
+    assertThat(savedLog.getType()).isEqualTo(eventType.toString());
+    assertThat(savedLog.getData()).isEqualTo(eventData);
+    assertThat(savedLog.getTimestamp()).isNotNull().isBeforeOrEqualTo(Instant.now());
+  }
+
+  @Test
+  @Transactional
+  @Commit
+  void logsTrackableEventWithNoData() {
+    // Given
+    Person person = sampleRetirementAgePerson;
+    TrackableEventType eventType = TrackableEventType.MANDATE_SUCCESSFUL;
+    TrackableEvent eventToPublish = new TrackableEvent(person, eventType);
+
+    // When
+    eventPublisher.publishEvent(eventToPublish);
+
+    // Then
+    Iterable<EventLog> eventLogs = eventLogRepository.findAll();
+    assertThat(eventLogs).hasSize(1);
+
+    EventLog savedLog = eventLogs.iterator().next();
+    assertThat(savedLog.getPrincipal()).isEqualTo(person.getPersonalCode());
+    assertThat(savedLog.getType()).isEqualTo(eventType.toString());
+    assertThat(savedLog.getData()).isEmpty();
+  }
+}

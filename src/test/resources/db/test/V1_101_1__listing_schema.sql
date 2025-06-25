38708d6cc534cb2a7bebb0546eb0e786bed230d0
Erko Risthein <erko@risthein.ee>
2025-06-25 14:24:30 +0300

    Remove deal domain

--- src/test/resources/db/test/V1_101_1__listing_schema.sql
+++ src/test/resources/db/test/V1_101_1__listing_schema.sql
@@ -1,3 +1,5 @@
+-- TODO: move to src/main/resources/db/migration once finished
+
 CREATE TABLE listing
 (
   id             SERIAL PRIMARY KEY NOT NULL,
@@ -8,6 +10,7 @@ CREATE TABLE listing
   currency       VARCHAR(3)         NOT NULL,
   state          VARCHAR(20)        NOT NULL,
   iban           VARCHAR(34),
+  language       VARCHAR(3),
   expiry_time    TIMESTAMP          NOT NULL,
   created_time   TIMESTAMP          NOT NULL DEFAULT NOW(),
   cancelled_time TIMESTAMP,
@@ -15,17 +18,3 @@ CREATE TABLE listing
 );
 
 CREATE INDEX listings_member_id_index ON listing (member_id);
-
-CREATE TABLE deal
-(
-  id                  SERIAL PRIMARY KEY NOT NULL,
-  listing_id          BIGINT             NOT NULL REFERENCES listing (id),
-  member_id           BIGINT             NOT NULL REFERENCES member (id),
-  state               VARCHAR(20)        NOT NULL,
-  created_time        TIMESTAMP          NOT NULL DEFAULT NOW(),
-  buyer_confirmed_time  TIMESTAMP,
-  seller_confirmed_time TIMESTAMP
-);
-
-CREATE INDEX deal_listing_id_index ON deal (listing_id);
-CREATE INDEX deal_member_id_index ON deal (member_id);

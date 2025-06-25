db2ed3fa72232548f7d059598521264b8f31ff37
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2025-06-25 18:32:00 +0700

    Remove index migration, non h2 compatible

--- src/main/resources/db/migration/V1_101_aml_check_indexes.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_aml_check_metadata_level
-  ON public.aml_check (((metadata ->> 'level')::integer));
-
-CREATE INDEX IF NOT EXISTS idx_aml_check_type_personal_code
-  ON public.aml_check (type, personal_code);

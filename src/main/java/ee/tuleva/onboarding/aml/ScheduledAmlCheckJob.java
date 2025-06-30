8915de86b89679476d3b1ae99a6f307920464366
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2025-06-30 11:01:22 +0700

    Removes live test scheduled job

--- src/main/java/ee/tuleva/onboarding/aml/ScheduledAmlCheckJob.java
+++ src/main/java/ee/tuleva/onboarding/aml/ScheduledAmlCheckJob.java
@@ -3,7 +3,6 @@ package ee.tuleva.onboarding.aml;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,12 +13,9 @@ public class ScheduledAmlCheckJob {
   private final AmlService amlService;
 
   // once per week on Sunday at 19:10
-  @Schedules({
-    @Scheduled(
-        cron = "${aml.jobs.third-pillar.cron:0 10 19 * * SUN}",
-        zone = "${aml.jobs.third-pillar.zone:Europe/Tallinn}"),
-    @Scheduled(cron = "0 50 6 30 6 MON", zone = "Europe/Tallinn")
-  })
+  @Scheduled(
+      cron = "${aml.jobs.third-pillar.cron:0 10 19 * * SUN}",
+      zone = "${aml.jobs.third-pillar.zone:Europe/Tallinn}")
   public void run() {
     amlService.runAmlChecksOnThirdPillarCustomers();
   }

665b2306529a81612325d3572ca3715941ed3e30
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2025-07-08 12:27:33 +0700

    WIP Capital Transfer Contract, getMemberByPersonalCode

--- src/main/java/ee/tuleva/onboarding/user/member/MemberRepository.java
+++ src/main/java/ee/tuleva/onboarding/user/member/MemberRepository.java
@@ -1,5 +1,6 @@
 package ee.tuleva.onboarding.user.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -7,4 +8,6 @@ public interface MemberRepository extends CrudRepository<Member, Long> {
 
   @Query("select max(m.memberNumber) + 1 from Member m")
   Integer getNextMemberNumber();
+
+  Optional<Member> findByUserPersonalCode(String personalCode);
 }

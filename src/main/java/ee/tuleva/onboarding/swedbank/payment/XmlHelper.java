be8c728a721e832ccc390ada8375de7287365d7a
Erik Jõgi <erik@codeborne.com>
2025-09-29 15:37:04 +0300

    Swedbank payment XML generator

---
+++ src/main/java/ee/tuleva/onboarding/swedbank/payment/XmlHelper.java
@@ -0,0 +1,97 @@
+package ee.tuleva.onboarding.swedbank.payment;
+
+import static java.math.RoundingMode.HALF_UP;
+
+import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import lombok.SneakyThrows;
+
+public class XmlHelper {
+
+  public static Document createDocument() {
+    try {
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+      factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+      factory.setXIncludeAware(false);
+      factory.setExpandEntityReferences(false);
+      return factory.newDocumentBuilder().newDocument();
+    }
+    catch (ParserConfigurationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static Element add(Element parent, String name, String value) {
+    Element element = add(parent, name);
+    element.setTextContent(value);
+    return element;
+  }
+
+  public static Element add(Element parent, String name, int value) {
+    return add(parent, name, Integer.toString(value));
+  }
+
+  public static Element add(Element parent, String name, long value) {
+    return add(parent, name, Long.toString(value));
+  }
+
+  public static Element add(Node parent, String name) {
+    Element element = (parent instanceof Document ? (Document)parent : parent.getOwnerDocument()).createElement(name);
+    parent.appendChild(element);
+    return element;
+  }
+
+  public static Element add(Element parent, String name, String value, int maxLength) {
+    if (value.length() > maxLength) value = value.substring(0, maxLength);
+    return add(parent, name, value);
+  }
+
+  public static Element add(Element parent, String name, Date date) {
+    return add(parent, name, new SimpleDateFormat("yyyy-MM-dd").format(date));
+  }
+
+  public static Element add(Element parent, String name, BigDecimal value) {
+    return add(parent, name, value.setScale(2, HALF_UP).toPlainString());
+  }
+
+  @SneakyThrows
+  public static String asString(Document document) {
+    var transformer = transformerFactory().newTransformer();
+    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+    var stringWriter = new StringWriter();
+    transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+    return stringWriter.toString();
+  }
+
+  public static TransformerFactory transformerFactory() {
+    TransformerFactory factory = TransformerFactory.newInstance();
+    try {
+      factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+    }
+    catch (Exception ignored) {
+    }
+    try {
+      factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+    }
+    catch (Exception ignored) {
+    }
+    return factory;
+  }
+}
---
+++ src/test/java/ee/tuleva/onboarding/swedbank/payment/PaymentMessageGeneratorTest.java
@@ -0,0 +1,121 @@
+package ee.tuleva.onboarding.swedbank.payment;
+
+import static java.time.ZoneOffset.UTC;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+class PaymentMessageGeneratorTest {
+
+  Clock clock = Clock.fixed(Instant.parse("2020-01-01T14:13:15Z"), UTC);
+  PaymentMessageGenerator generator = new PaymentMessageGenerator(clock);
+
+  @Test
+  void generate() throws IOException, SAXException {
+    var paymentRequest = PaymentRequest.builder()
+        .remitterName("Tuleva AS")
+        .remitterId("14118923")
+        .remitterIban("EE121283519985595614")
+        .remitterBic("HABAEE2X")
+        .beneficiaryName("John Doe")
+        .beneficiaryIban("EE461277288334943840")
+        .amount(new BigDecimal("111.03"))
+        .description("money for nothing")
+        .ourId("111")
+        .endToEndId("123ABC")
+        .build();
+
+    var result = generator.generatePaymentMessage(paymentRequest);
+
+    assertThat(result).isEqualToIgnoringWhitespace(xml);
+
+    var schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+        .newSchema(new StreamSource(getClass().getResourceAsStream("/swedbank/pain.001.001.09.xsd")));
+    var validator = schema.newValidator();
+    validator.validate(new StreamSource(new StringReader(result)));
+  }
+
+  //language=xml
+  private static final String xml = """
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<Document xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.09" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <CstmrCdtTrfInitn>
+    <GrpHdr>
+      <MsgId>1577887995</MsgId>
+      <CreDtTm>2020-01-01T14:13:15Z</CreDtTm>
+      <NbOfTxs>1</NbOfTxs>
+      <CtrlSum>111.03</CtrlSum>
+      <InitgPty>
+        <Nm>Tuleva AS</Nm>
+      </InitgPty>
+    </GrpHdr>
+    <PmtInf>
+      <PmtInfId>1577887995</PmtInfId>
+      <PmtMtd>TRF</PmtMtd>
+      <NbOfTxs>1</NbOfTxs>
+      <CtrlSum>111.03</CtrlSum>
+      <PmtTpInf>
+        <SvcLvl>
+          <Cd>SEPA</Cd>
+        </SvcLvl>
+      </PmtTpInf>
+      <ReqdExctnDt>
+        <Dt>2020-01-01</Dt>
+      </ReqdExctnDt>
+      <Dbtr>
+        <Nm>Tuleva AS</Nm>
+        <Id>
+          <OrgId>
+            <Othr>
+              <Id>14118923</Id>
+            </Othr>
+          </OrgId>
+        </Id>
+      </Dbtr>
+      <DbtrAcct>
+        <Id>
+          <IBAN>EE121283519985595614</IBAN>
+        </Id>
+      </DbtrAcct>
+      <DbtrAgt>
+        <FinInstnId>
+          <BICFI>HABAEE2X</BICFI>
+        </FinInstnId>
+      </DbtrAgt>
+      <CdtTrfTxInf>
+        <PmtId>
+          <InstrId>111</InstrId>
+          <EndToEndId>123ABC</EndToEndId>
+        </PmtId>
+        <Amt>
+          <InstdAmt Ccy="EUR">111.03</InstdAmt>
+        </Amt>
+        <Cdtr>
+          <Nm>John Doe</Nm>
+        </Cdtr>
+        <CdtrAcct>
+          <Id>
+            <IBAN>EE461277288334943840</IBAN>
+          </Id>
+        </CdtrAcct>
+        <RmtInf>
+          <Ustrd>money for nothing</Ustrd>
+        </RmtInf>
+      </CdtTrfTxInf>
+    </PmtInf>
+  </CstmrCdtTrfInitn>
+</Document>
+""";
+
+}

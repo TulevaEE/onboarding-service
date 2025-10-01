09b23656142046afc980d6c5af82471b282416e3
Erik Jõgi <erik@codeborne.com>
2025-10-01 11:47:07 +0300

    First sketch of SavingFundPaymentRepository

--- src/main/java/ee/tuleva/onboarding/savings/fund/SavingFundPaymentRepository.java
+++ src/main/java/ee/tuleva/onboarding/savings/fund/SavingFundPaymentRepository.java
@@ -1,10 +1,23 @@
 package ee.tuleva.onboarding.savings.fund;
 
+import java.util.List;
 import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SavingFundPaymentRepository extends CrudRepository<SavingFundPayment, UUID> {
   boolean existsByRemitterIdCodeAndDescription(String remitterIdCode, String description);
+
+  List<SavingFundPayment> findRecentPayments(String description);
+  void savePaymentData(SavingFundPayment payment); // always status CREATED
+  void updatePaymentData(SavingFundPayment payment); // only Swed, does not change status
+
+  void changeStatus(UUID paymentId, SavingFundPayment.Status newStatus);
+
+  void attachUser(UUID paymentId, Long userId);
+  List<SavingFundPayment> findPaymentsWithStatus(SavingFundPayment.Status status);
+
+  void addReturnReason(UUID paymentId, String reason);
 }

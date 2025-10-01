02bbdc6ec025e7b6ec78b836877750bf6c3c046d
Erik Jõgi <erik@codeborne.com>
2025-10-01 15:16:28 +0300

    Update change IDs

--- src/main/resources/db/migration/V1_116__saving_fund_payment_status.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE saving_fund_payment ADD COLUMN status TEXT;
-UPDATE saving_fund_payment SET status='CREATED' WHERE status IS NULL;
-ALTER TABLE saving_fund_payment ALTER COLUMN status SET NOT NULL;
-ALTER TABLE saving_fund_payment ADD COLUMN status_changed_at TIMESTAMPTZ NOT NULL DEFAULT NOW();

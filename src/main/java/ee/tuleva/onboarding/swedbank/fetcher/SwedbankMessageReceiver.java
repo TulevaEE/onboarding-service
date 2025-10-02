d6d5d891c630120e4d9e3e603cc199e54604ae24
Erko Risthein <erko@risthein.ee>
2025-10-02 15:18:00 +0300

    Fix staging

--- src/main/java/ee/tuleva/onboarding/swedbank/fetcher/SwedbankMessageReceiver.java
+++ src/main/java/ee/tuleva/onboarding/swedbank/fetcher/SwedbankMessageReceiver.java
@@ -6,11 +6,9 @@ import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Profile({"!staging"})
 @RequiredArgsConstructor
 @Slf4j
 @Service

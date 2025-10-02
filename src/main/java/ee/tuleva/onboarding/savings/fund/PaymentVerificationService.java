7b1a4329ed7c447b03c26962d94393a09a6cb01a
Erko Risthein <erko@risthein.ee>
2025-10-02 09:48:54 +0300

    Spotless fix

--- src/main/java/ee/tuleva/onboarding/savings/fund/PaymentVerificationService.java
+++ src/main/java/ee/tuleva/onboarding/savings/fund/PaymentVerificationService.java
@@ -12,7 +12,6 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 

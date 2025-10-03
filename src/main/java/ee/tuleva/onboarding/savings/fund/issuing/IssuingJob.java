912a8626392d5a47989bcd8a2ee707e992ae5cc5
Sander Nemvalts <snemvalts@gmail.com>
2025-10-03 13:09:35 +0300

    Remove profile dev xd

--- src/main/java/ee/tuleva/onboarding/savings/fund/issuing/IssuingJob.java
+++ src/main/java/ee/tuleva/onboarding/savings/fund/issuing/IssuingJob.java
@@ -11,11 +11,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Profile("dev")
 @Service
 @Slf4j
 @RequiredArgsConstructor

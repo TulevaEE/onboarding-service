e873d90d7ae08ea5ce635c91fbe647c509260ee8
Erko Risthein <erko@risthein.ee>
2025-10-08 11:50:42 +0300

    Fix test

--- src/test/groovy/ee/tuleva/onboarding/swedbank/fetcher/SwedbankStatementFetcherTest.java
+++ src/test/groovy/ee/tuleva/onboarding/swedbank/fetcher/SwedbankStatementFetcherTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.*;
 
 import ee.swedbank.gateway.iso.request.Document;
 import ee.tuleva.onboarding.swedbank.http.SwedbankGatewayClient;
-import ee.tuleva.onboarding.time.TestClockHolder;
 import jakarta.xml.bind.JAXBElement;
 import java.util.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,24 +18,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class SwedbankStatementFetcherTest {
 
   private SwedbankGatewayClient swedbankGatewayClient;
-
-  private SwedbankMessageRepository fetchJobRepository;
-
   private SwedbankStatementFetcher fetcher;
-
-  private SwedbankAccountConfiguration configuration;
 
   private static final String testIban = "EE_TEST_IBAN";
 
   @BeforeEach
   void setup() {
     swedbankGatewayClient = mock(SwedbankGatewayClient.class);
-    fetchJobRepository = mock(SwedbankMessageRepository.class);
-    configuration = mock(SwedbankAccountConfiguration.class);
+    var configuration = mock(SwedbankAccountConfiguration.class);
 
-    fetcher =
-        new SwedbankStatementFetcher(
-            TestClockHolder.clock, fetchJobRepository, swedbankGatewayClient, configuration);
+    fetcher = new SwedbankStatementFetcher(swedbankGatewayClient, configuration);
 
     when(configuration.getAccountIban(DEPOSIT_EUR)).thenReturn(Optional.of(testIban));
   }

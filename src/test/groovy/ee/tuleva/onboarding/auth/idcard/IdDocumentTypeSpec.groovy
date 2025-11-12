ee25f041d66cc913c8b5df4f21452483b9de43b4
Erko Risthein <erko@risthein.ee>
2025-11-12 15:29:15 +0200

    Support new 2025 ID-cards

--- src/test/groovy/ee/tuleva/onboarding/auth/idcard/IdDocumentTypeSpec.groovy
+++ src/test/groovy/ee/tuleva/onboarding/auth/idcard/IdDocumentTypeSpec.groovy
@@ -24,4 +24,33 @@ class IdDocumentTypeSpec extends Specification {
         DIGITAL_ID_CARD                               | null
         OLD_DIGITAL_ID_CARD                           | null
     }
+
+    @Unroll
+    def "findByIdentifier maps OID '#oid' to #expectedType"() {
+        when:
+        def result = IdDocumentType.findByIdentifier(oid)
+
+        then:
+        result == expectedType
+
+        where:
+        oid                            | expectedType
+        "1.3.6.1.4.1.51361.1.1.1"     | ESTONIAN_CITIZEN_ID_CARD
+        "1.3.6.1.4.1.51361.2.1.1"     | ESTONIAN_CITIZEN_ID_CARD
+        "1.3.6.1.4.1.51361.1.1.2"     | EUROPEAN_CITIZEN_ID_CARD
+        "1.3.6.1.4.1.51361.2.1.2"     | EUROPEAN_CITIZEN_ID_CARD
+        "1.3.6.1.4.1.51361.1.1.3"     | DIGITAL_ID_CARD
+        "1.3.6.1.4.1.51361.1.1.4"     | E_RESIDENT_DIGITAL_ID_CARD
+        "1.3.6.1.4.1.51361.2.1.6"     | E_RESIDENT_DIGITAL_ID_CARD
+        "1.3.6.1.4.1.51361.1.1.5"     | LONG_TERM_RESIDENCE_CARD
+        "1.3.6.1.4.1.51361.2.1.3"     | LONG_TERM_RESIDENCE_CARD
+        "1.3.6.1.4.1.51361.1.1.6"     | TEMPORARY_RESIDENCE_CARD
+        "1.3.6.1.4.1.51361.2.1.4"     | TEMPORARY_RESIDENCE_CARD
+        "1.3.6.1.4.1.51361.1.1.7"     | EUROPEAN_CITIZEN_FAMILY_MEMBER_RESIDENCE_CARD
+        "1.3.6.1.4.1.51361.2.1.5"     | EUROPEAN_CITIZEN_FAMILY_MEMBER_RESIDENCE_CARD
+        "1.3.6.1.4.1.10015.1.1"       | OLD_ID_CARD
+        "1.3.6.1.4.1.10015.1.2"       | OLD_DIGITAL_ID_CARD
+        "1.3.6.1.4.1.51455.1.1.1"     | DIPLOMATIC_ID_CARD
+        "1.3.6.1.4.1.51455.2.1.1"     | DIPLOMATIC_ID_CARD
+    }
 }

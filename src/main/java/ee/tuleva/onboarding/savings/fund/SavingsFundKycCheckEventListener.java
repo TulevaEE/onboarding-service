35b1c1c87de8ff6d4e06c4aeed20b4fabb2a90d5
Vootele Rotov <vootele.rotov@gmail.com>
2025-12-02 15:45:30 +0200

    Add event logging for savings fund onboarding status change

--- src/main/java/ee/tuleva/onboarding/savings/fund/SavingsFundKycCheckEventListener.java
+++ src/main/java/ee/tuleva/onboarding/savings/fund/SavingsFundKycCheckEventListener.java
@@ -22,6 +22,6 @@ public class SavingsFundKycCheckEventListener {
         .ifPresent(
             user ->
                 savingsFundOnboardingService.updateOnboardingStatusIfNeeded(
-                    user.getId(), event.getKycCheck()));
+                    user, event.getKycCheck()));
   }
 }

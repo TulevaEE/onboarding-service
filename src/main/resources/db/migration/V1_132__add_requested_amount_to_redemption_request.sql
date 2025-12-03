6e1bf5294ba27ceae7b793425f4c06890e839369
Samuel Pitko <samuel.pitko@gmail.com>
2025-12-03 14:19:08 +0200

    update migration to backfill existing rows

--- src/main/resources/db/migration/V1_132__add_requested_amount_to_redemption_request.sql
+++ src/main/resources/db/migration/V1_132__add_requested_amount_to_redemption_request.sql
@@ -2,4 +2,10 @@ ALTER TABLE redemption_request
 DROP COLUMN created_at;
 
 ALTER TABLE redemption_request
-ADD COLUMN requested_amount DECIMAL(15, 2) NOT NULL;
+ADD COLUMN requested_amount DECIMAL(15, 2);
+
+UPDATE redemption_request
+SET requested_amount = CAST(fund_units AS DECIMAL(15, 2));
+
+ALTER TABLE redemption_request
+ALTER COLUMN requested_amount SET NOT NULL;

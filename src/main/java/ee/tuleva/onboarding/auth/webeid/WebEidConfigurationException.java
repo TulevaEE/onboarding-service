6a61d5182d76bc5365f34724ffe6e35535c8879a
Erko Risthein <erko@risthein.ee>
2025-12-30 22:31:19 +0200

    Missing file

---
+++ src/main/java/ee/tuleva/onboarding/auth/webeid/WebEidConfigurationException.java
@@ -0,0 +1,7 @@
+package ee.tuleva.onboarding.auth.webeid;
+
+public class WebEidConfigurationException extends RuntimeException {
+  public WebEidConfigurationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}

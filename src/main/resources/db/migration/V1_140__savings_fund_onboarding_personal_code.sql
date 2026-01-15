4c52ade7ce0720279fbc7d03017243e11cfdf147
Erko Risthein <erko@risthein.ee>
2026-01-15 10:26:38 +0200

    Fix migration

--- src/main/resources/db/migration/V1_140__savings_fund_onboarding_personal_code.sql
+++ src/main/resources/db/migration/V1_140__savings_fund_onboarding_personal_code.sql
@@ -3,7 +3,7 @@ CREATE TABLE savings_fund_onboarding_new
     personal_code char(11) not null,
     created_at    timestamptz not null default now(),
     status        text not null,
-    constraint savings_fund_onboarding_pkey primary key (personal_code)
+    constraint savings_fund_onboarding_new_pkey primary key (personal_code)
 );
 
 INSERT INTO savings_fund_onboarding_new (personal_code, created_at, status)
@@ -14,3 +14,5 @@ FROM savings_fund_onboarding
 DROP TABLE savings_fund_onboarding;
 
 ALTER TABLE savings_fund_onboarding_new RENAME TO savings_fund_onboarding;
+
+ALTER INDEX savings_fund_onboarding_new_pkey RENAME TO savings_fund_onboarding_pkey;

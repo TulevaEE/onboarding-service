9615fde698d1a3ea967447cbcc238e27517a073c
Erko Risthein <erko@risthein.ee>
2026-02-19 15:04:53 +0200

    Fix master

--- src/main/resources/db/migration/V1_158__replace_transfer_with_specific_transaction_types.sql
+++
@@ -1,3 +0,0 @@
-ALTER TYPE ledger.transaction_type ADD VALUE 'POSITION_UPDATE';
-ALTER TYPE ledger.transaction_type ADD VALUE 'FEE_ACCRUAL';
-ALTER TYPE ledger.transaction_type ADD VALUE 'FEE_SETTLEMENT';

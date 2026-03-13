c888dd4b4727f08bb2401bd885d50e8d64bb158d
Jordan Valdma <deskpro_belchertown@simplelogin.com>
2026-03-13 09:00:34 +0000

    LimitCheckBackfillJob and bug fix

--- src/main/java/ee/tuleva/onboarding/investment/check/limit/LimitCheckEventRepository.java
+++ src/main/java/ee/tuleva/onboarding/investment/check/limit/LimitCheckEventRepository.java
@@ -4,11 +4,15 @@ import ee.tuleva.onboarding.fund.TulevaFund;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 interface LimitCheckEventRepository extends JpaRepository<LimitCheckEvent, Long> {
 
   List<LimitCheckEvent> findByFundAndCheckDate(TulevaFund fund, LocalDate checkDate);
 
+  @Modifying
+  @Transactional
   void deleteByFundAndCheckDateAndCheckType(
       TulevaFund fund, LocalDate checkDate, CheckType checkType);
 }

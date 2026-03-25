30e4a8ae7053188eb3112ef76b78964290180a86
Vootele Rotov <vootele.rotov@gmail.com>
2026-03-25 15:10:37 +0200

    Also fetch NaceCode from Ariregister

--- src/main/java/ee/tuleva/onboarding/ariregister/CompanyDetail.java
+++ src/main/java/ee/tuleva/onboarding/ariregister/CompanyDetail.java
@@ -11,12 +11,18 @@ public class CompanyDetail {
   String name;
   String registryCode;
   @Nullable String status;
+  @Nullable String legalForm;
   @Nullable LocalDate foundingDate;
   @Nullable String address;
   @Nullable String mainActivity;
+  @Nullable String naceCode;
 
   public Optional<String> getStatus() {
     return Optional.ofNullable(status);
+  }
+
+  public Optional<String> getLegalForm() {
+    return Optional.ofNullable(legalForm);
   }
 
   public Optional<LocalDate> getFoundingDate() {
@@ -29,5 +35,9 @@ public class CompanyDetail {
 
   public Optional<String> getMainActivity() {
     return Optional.ofNullable(mainActivity);
+  }
+
+  public Optional<String> getNaceCode() {
+    return Optional.ofNullable(naceCode);
   }
 }

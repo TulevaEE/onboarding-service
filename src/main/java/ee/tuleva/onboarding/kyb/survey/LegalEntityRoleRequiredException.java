123b5575dafa0ea0b912bdfe88498ec0e19b831a
Vootele Rotov <vootele.rotov@gmail.com>
2026-03-26 10:19:35 +0200

    Take registry code in as query paramater
    We have not yet added a role for the company, we are creating it.

--- src/main/java/ee/tuleva/onboarding/kyb/survey/LegalEntityRoleRequiredException.java
+++
@@ -1,8 +0,0 @@
-package ee.tuleva.onboarding.kyb.survey;
-
-class LegalEntityRoleRequiredException extends RuntimeException {
-
-  LegalEntityRoleRequiredException(String personalCode) {
-    super("Legal entity role required: personalCode=" + personalCode);
-  }
-}

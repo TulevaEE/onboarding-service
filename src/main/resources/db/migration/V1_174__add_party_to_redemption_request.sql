b7843c692b534f9944b10145321e7f68937e0d21
Aivar Naaber <aivar@codeborne.com>
2026-03-27 14:37:24 +0200

    Add party_type and party_code columns to redemption_request table with backfill from users.

---
+++ src/main/resources/db/migration/V1_174__add_party_to_redemption_request.sql
@@ -0,0 +1,11 @@
+ALTER TABLE redemption_request ADD COLUMN party_type text;
+ALTER TABLE redemption_request ADD COLUMN party_code text;
+
+UPDATE redemption_request
+SET party_type = 'PERSON',
+    party_code = (SELECT u.personal_code FROM users u WHERE u.id = redemption_request.user_id);
+
+ALTER TABLE redemption_request ALTER COLUMN party_type SET NOT NULL;
+ALTER TABLE redemption_request ALTER COLUMN party_code SET NOT NULL;
+
+CREATE INDEX idx_redemption_request_party ON redemption_request (party_type, party_code);
